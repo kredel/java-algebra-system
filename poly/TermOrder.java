c4b23a9ae7080b630efeea22bfcc0efaeca67bac
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2003-12-28 22:39:12 +0000

    new class for SortedMap polynomials
    git-svn-id: file:///home/SUBVERSION/jas/trunk@83 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ poly/TermOrder.java
@@ -0,0 +1,68 @@
+/*
+ * $Id$
+ */
+
+package edu.jas.poly;
+
+import edu.jas.poly.ExpVector;
+
+import java.util.Comparator;
+
+/**
+ * Term order class for ordered polynomials. 
+ */
+
+public final class TermOrder {
+
+    public final static int DEFAULT_EVORD = ExpVector.DEFAULT_EVORD;
+    //public final static int DEFAULT_EVORD = ExpVector.INVLEX;
+
+    private final int evord;
+
+    private final Comparator horder;  // highest first 
+    private final Comparator lorder;  // lowest first
+
+    private final class EVhorder implements Comparator {
+	   public int compare(Object o1, Object o2) {
+               return -ExpVector.EVCOMP( evord, 
+                                         (ExpVector) o1, 
+                                         (ExpVector) o2 ); 
+           }
+    }
+
+    private final class EVlorder implements Comparator {
+	   public int compare(Object o1, Object o2) {
+               return ExpVector.EVCOMP( evord, 
+                                        (ExpVector) o1, 
+                                        (ExpVector) o2 ); 
+           }
+    }
+
+
+    public TermOrder() {
+	this(DEFAULT_EVORD);
+    }
+
+
+    public TermOrder(int evord) {
+	this.evord = evord;
+        horder = new EVhorder();
+        lorder = new EVlorder();
+    }
+
+
+    public int getEvord() { 
+        return evord; 
+    }
+
+
+    public Comparator getDescendComparator() { 
+        return horder; 
+    }
+
+
+    public Comparator getAscendComparator() { 
+        return lorder; 
+    }
+
+}
