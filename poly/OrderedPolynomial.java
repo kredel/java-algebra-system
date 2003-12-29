fb730656bd3de73d6ed3082d03de963e776ec7c9
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2003-12-29 11:54:26 +0000

    removed extension of UnorderedPolynomial
    git-svn-id: file:///home/SUBVERSION/jas/trunk@88 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- poly/OrderedPolynomial.java
+++ poly/OrderedPolynomial.java
@@ -15,20 +15,62 @@ import java.util.Map;
  * e.g. java.util.TreeMap.
  */
 
-interface OrderedPolynomial extends UnorderedPolynomial {
+interface OrderedPolynomial /* extends UnorderedPolynomial */ {
 
     public TermOrder getTermOrder();
 
 
-    public void reSort();
+    public Map getMap();
 
-    public void reSort(TermOrder to);
+    public int length();
+
+    public String[] getVars();
+
+    public String[] setVars(String[] v);
+
+    public int numberOfVariables(); 
+
+
+    public String toString(); 
+
+    public String toString(String[] vars); 
+
+    public boolean equals( Object B ); 
 
 
     public Map.Entry leadingMonomial();
 
     public ExpVector leadingExpVector();
 
-    public Object leadingBaseCoefficient(); 
+    public Coefficient leadingBaseCoefficient(); 
+
+
+    public OrderedPolynomial getZERO();
+
+    public OrderedPolynomial getZERO(TermOrder t);
+
+    public OrderedPolynomial getONE();
+
+    public OrderedPolynomial getONE(TermOrder t);
+
+    public boolean isZERO();
+
+    public boolean isONE();
+
+
+    public OrderedPolynomial add(OrderedPolynomial B);
+
+    public OrderedPolynomial subtract(OrderedPolynomial B);
+
+    public OrderedPolynomial negate();
+
+
+    public OrderedPolynomial multiply(OrderedPolynomial B);
+
+    public OrderedPolynomial multiply(Coefficient b);
+
+    public OrderedPolynomial multiply(Coefficient b, ExpVector e);
+
+    public OrderedPolynomial multiply(Map.Entry m);
 
 }
