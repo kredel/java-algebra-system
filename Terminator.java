3e240116dd9cdf0162ffffb5b050794fc8ae67e2
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2004-01-10 11:00:42 +0000

    moved from edu.jas.ring to edu.jas
    git-svn-id: file:///home/SUBVERSION/jas/trunk@128 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ Terminator.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ */
+
+package edu.jas;
+
+    /**
+     * terminating helper class
+     * like a barrier with coming and going
+     * @author Heinz Kredel
+     */
+
+public class Terminator {
+
+	private int workers = 0;
+	private int idler = 0;
+	private Semaphore fin = new Semaphore(0);
+
+	public Terminator(int workers) {
+	    this.workers = workers;
+	}
+
+	public synchronized void beIdle() {
+	    idler++;
+	    if ( idler >= workers ) fin.V();
+	}
+
+	public synchronized void allIdle() {
+	    idler = workers;
+	    fin.V();
+	}
+
+	public synchronized void notIdle() {
+	    idler--;
+	}
+
+	public boolean hasJobs() {
+	    return ( idler < workers );
+	}
+
+	public void done() {
+            try { fin.P();
+	    } catch (InterruptedException e) { }
+	}
+
+}
