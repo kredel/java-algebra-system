4876ca34417c226400e0a06c9aab06788267dc0c
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2004-01-11 18:00:20 +0000

    works now for ald and new polynomials
    git-svn-id: file:///home/SUBVERSION/jas/trunk@147 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- poly/PolynomialList.java
+++ poly/PolynomialList.java
@@ -16,12 +16,18 @@ import java.util.Iterator;
 public class PolynomialList {
 
     public final String[] vars;
-    public final int evord;
+    public final TermOrder tord;
     public final ArrayList list;
 
     public PolynomialList( String[] v, int eo, ArrayList l ) {
 	vars = v;
-	evord = eo;
+	tord = new TermOrder(eo);
+	list = l;
+    }
+
+    public PolynomialList( String[] v, TermOrder to, ArrayList l ) {
+	vars = v;
+	tord = to;
 	list = l;
     }
 
@@ -33,20 +39,24 @@ public class PolynomialList {
 	    if ( i < vars.length-1 ) erg.append(","); 
         }
         erg.append(")");
-	switch ( evord ) {
-	case ExpVector.INVLEX: erg.append(" INVLEX "); break ;
-	case ExpVector.IGRLEX: erg.append(" IGRLEX "); break ;
-	case ExpVector.LEX: erg.append(" LEX "); break ;
-	case ExpVector.GRLEX: erg.append(" GRLEX "); break ;
-	}
+        erg.append(" "+tord);
         erg.append("\n");
 
         Polynomial a;
+        OrderedPolynomial oa;
         Iterator it = list.iterator();
         erg.append("(\n");
         while ( it.hasNext() ) {
-	      a = (Polynomial) it.next();
-	      erg.append(a.toString(vars));
+              Object o = it.next();
+	      if ( o instanceof Polynomial ) {
+                a = (Polynomial) o;
+	        erg.append( a.toString(vars) );
+	      } else if ( o instanceof OrderedPolynomial ) {
+                oa = (OrderedPolynomial) o;
+	        erg.append( oa.toString(vars) );
+	      } else {
+	        erg.append( o.toString() );
+	      }
 	      if ( it.hasNext() ) erg.append(",\n");
 	      else erg.append("\n");
         }
