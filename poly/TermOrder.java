6040855d8929b10ffaaa5088629b5101bde5fc9f
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2004-01-17 12:25:42 +0000

    implemended Serializable and moved constants from ExpVector
    git-svn-id: file:///home/SUBVERSION/jas/trunk@163 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- poly/TermOrder.java
+++ poly/TermOrder.java
@@ -4,26 +4,36 @@
 
 package edu.jas.poly;
 
-import edu.jas.poly.ExpVector;
-
 import java.util.Comparator;
+import java.io.Serializable;
+
+import edu.jas.poly.ExpVector;
 
 /**
  * Term order class for ordered polynomials. 
  * @author Heinz Kredel
  */
 
-public final class TermOrder {
+public final class TermOrder implements Serializable {
 
-    public final static int DEFAULT_EVORD = ExpVector.DEFAULT_EVORD;
-    //public final static int DEFAULT_EVORD = ExpVector.INVLEX;
+    public static final int LEX     = 1;
+    public static final int INVLEX  = 2;
+    public static final int GRLEX   = 3;
+    public static final int IGRLEX  = 4;
+    public static final int REVLEX  = 5;
+    public static final int REVILEX = 6;
+    public static final int REVTDEG = 7;
+    public static final int REVITDG = 8;
+
+    public final static int DEFAULT_EVORD = IGRLEX;
+    //public final static int DEFAULT_EVORD = INVLEX;
 
     private final int evord;
 
     private final Comparator horder;  // highest first 
     private final Comparator lorder;  // lowest first
 
-    private final class EVhorder implements Comparator {
+    private final class EVhorder implements Comparator, Serializable {
            public int compare(Object o1, Object o2) {
                return -ExpVector.EVCOMP( evord, 
                                          (ExpVector) o1, 
@@ -31,7 +41,7 @@ public final class TermOrder {
            }
     }
 
-    private final class EVlorder implements Comparator {
+    private final class EVlorder implements Comparator, Serializable {
            public int compare(Object o1, Object o2) {
                return ExpVector.EVCOMP( evord, 
                                         (ExpVector) o1, 
