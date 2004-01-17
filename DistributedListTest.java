d73794b2e363bbec8caa83148674580d76bf8017
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2004-01-17 12:22:51 +0000

    more cases for Coefficients and OrderedPolynomials
    git-svn-id: file:///home/SUBVERSION/jas/trunk@160 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- DistributedListTest.java
+++ DistributedListTest.java
@@ -13,6 +13,17 @@ import junit.framework.TestSuite;
 
 import org.apache.log4j.BasicConfigurator;
 
+import edu.jas.arith.Coefficient;
+import edu.jas.arith.BigRational;
+import edu.jas.arith.BigComplex;
+import edu.jas.arith.BigQuaternion;
+import edu.jas.arith.BigInteger;
+import edu.jas.poly.OrderedPolynomial;
+import edu.jas.poly.IntOrderedMapPolynomial;
+import edu.jas.poly.RatOrderedMapPolynomial;
+import edu.jas.poly.ComplexOrderedMapPolynomial;
+import edu.jas.poly.QuatOrderedMapPolynomial;
+
 /**
  * DistributedList Test using JUnit 
  * @author Heinz Kredel
@@ -51,6 +62,11 @@ public class DistributedListTest extends TestCase {
    private DistributedListServer dls;
    private Thread dlst;
 
+   int rl = 7; 
+   int kl = 10;
+   int ll = 10;
+   int el = 5;
+   float q = 0.5f;
 
    protected void setUp() {
        dls = new DistributedListServer();
@@ -128,15 +144,13 @@ public class DistributedListTest extends TestCase {
      }
      assertTrue("#l1==10", l1.size() == loops );
 
-     while ( l2.size() < loops ) {
+     while ( l2.size() < loops/2 ) {
          try {
 	     //System.out.print(".");
              Thread.currentThread().sleep(100);
          } catch (InterruptedException e) {
          }
      }
-     assertTrue("#l2==10", l2.size() == loops );
-
      Iterator it = null;
      it = l2.iterator();
      i = 0;
@@ -146,7 +160,98 @@ public class DistributedListTest extends TestCase {
 	   //System.out.println("o = " + o + " x = "+ x);
            assertEquals("l2(i)==(i)", x, o );
      }
+     assertTrue("#l2==10", l2.size() == loops );
    }
 
+/**
+ * Tests DistributedLists with BigInteger, BigRational etc.
+ */
+ public void testDistributedList3() {
+     l1 = new DistributedList(host);
+     assertTrue("l1==empty",l1.isEmpty());
+     l2 = new DistributedList(host);
+     assertTrue("l2==empty",l2.isEmpty());
+
+     Coefficient a = BigInteger.IRAND(kl);
+     l1.add( a );
+     assertTrue("#l1==i", l1.size() == 1 );
+
+     Coefficient b = BigRational.RNRAND(kl);
+     l1.add( b );
+     assertTrue("#l1==i", l1.size() == 2 );
+
+     Coefficient c = BigComplex.CRAND(kl);
+     l1.add( c );
+     assertTrue("#l1==i", l1.size() == 3 );
+
+     Coefficient d = BigQuaternion.QRAND(kl);
+     l1.add( d );
+     assertTrue("#l1==i", l1.size() == 4 );
+
+     while ( l2.size() < l1.size() ) {
+         try {
+	     //System.out.print(".");
+             Thread.currentThread().sleep(100);
+         } catch (InterruptedException e) {
+         }
+     }
+     Iterator ita = null;
+     ita = l1.iterator();
+     Iterator itb = null;
+     itb = l2.iterator();
+     while ( ita.hasNext() && itb.hasNext() ) {
+	   Object o = ita.next();
+	   Object p = itb.next();
+	   //System.out.println("o = " + o + " x = "+ x);
+           assertEquals("a==b", o, p );
+     }
+     assertTrue("#l1==#l210", l1.size() == l2.size() );
+ }
+
+
+/**
+ * Tests DistributedLists with Polynomials.
+ */
+ public void testDistributedList4() {
+     l1 = new DistributedList(host);
+     assertTrue("l1==empty",l1.isEmpty());
+     l2 = new DistributedList(host);
+     assertTrue("l2==empty",l2.isEmpty());
+
+     OrderedPolynomial a = IntOrderedMapPolynomial.DIIRAS(rl, kl, ll, el, q);
+     l1.add( a );
+     assertTrue("#l1==i", l1.size() == 1 );
+
+     OrderedPolynomial b = RatOrderedMapPolynomial.DIRRAS(rl, kl, ll, el, q);
+     l1.add( b );
+     assertTrue("#l1==i", l1.size() == 2 );
+
+     OrderedPolynomial c = ComplexOrderedMapPolynomial.DICRAS(rl, kl, ll, el, q);
+     l1.add( c );
+     assertTrue("#l1==i", l1.size() == 3 );
+
+     OrderedPolynomial d = QuatOrderedMapPolynomial.DIQRAS(rl, kl, ll, el, q);
+     l1.add( d );
+     assertTrue("#l1==i", l1.size() == 4 );
+
+     while ( l2.size() < l1.size() ) {
+         try {
+	     //System.out.print(".");
+             Thread.currentThread().sleep(100);
+         } catch (InterruptedException e) {
+         }
+     }
+     Iterator ita = null;
+     ita = l1.iterator();
+     Iterator itb = null;
+     itb = l2.iterator();
+     while ( ita.hasNext() && itb.hasNext() ) {
+	   Object o = ita.next();
+	   Object p = itb.next();
+	   //System.out.println("o = " + o + " x = "+ x);
+           assertEquals("a==b", o, p );
+     }
+     assertTrue("#l1==#l210", l1.size() == l2.size() );
+ }
 
 }
