d25e29b0935de184b5539b2611fea65815e3ec43
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2005-01-10 20:49:51 +0000

    moved to ring package
    git-svn-id: file:///home/SUBVERSION/jas/trunk@231 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- ring/Katsura.java
+++ ring/Katsura.java
@@ -3,6 +3,8 @@
  * $Id$
   */
 
+package edu.jas.ring;
+
 /**
  * Class to produce a system of equations as defined by Katsura
  * 
@@ -11,108 +13,108 @@
   */
 public class Katsura {
 
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("usage: Katsura N <order> <var>");
-			return;
-		}
-		int n = Integer.parseInt(args[0]);
-		Katsura k = null;
-        if ( args.length == 1 ) {		
-		   k = new Katsura(n);
+    public static void main(String[] args) {
+        if ( args.length == 0 ) {
+           System.out.println("usage: Katsura N <order> <var>");
+           return;
         }
-		if ( args.length == 2 ) {		
-		   k = new Katsura("u",n, args[1]);
-		}
-		if ( args.length == 3 ) {		
-			   k = new Katsura(args[2],n, args[1]);
-			}
-     	System.out.println("#Katsura equations for N = " + n + ":");
-		System.out.println("" + k);
-	}
+        int n = Integer.parseInt(args[0]);
+        Katsura k = null;
+        if ( args.length == 1 ) {               
+           k = new Katsura(n);
+        }
+        if ( args.length == 2 ) {               
+           k = new Katsura("u",n, args[1]);
+        }
+        if ( args.length == 3 ) {               
+        k = new Katsura(args[2],n, args[1]);
+                        }
+        System.out.println("#Katsura equations for N = " + n + ":");
+                System.out.println("" + k);
+    }
 
-	final int N;
-	final String var;
-	final String order;
+    final int N;
+    final String var;
+    final String order;
 
-	public Katsura(int n) {
-		this("u", n);
-	}
+    public Katsura(int n) {
+           this("u", n);
+    }
 
-	public Katsura(String v, int n) {
-		this(v, n, "G");
-	}
+    public Katsura(String v, int n) {
+           this(v, n, "G");
+    }
 
-	public Katsura(String var, int n, String order) {
-		this.var = var;
-		this.N = n;
-		this.order = order;
-	}
+    public Katsura(String var, int n, String order) {
+           this.var = var;
+           this.N = n;
+           this.order = order;
+    }
 
-	String sum1() {
-		StringBuffer s = new StringBuffer();
-		for (int i = -N; i <= N; i++) {
-			s.append(variable(i));
-			if (i < N) {
-				s.append(" + ");
-			}
-		}
-		s.append(" - 1");
-		return s.toString();
-	}
+    String sum1() {
+           StringBuffer s = new StringBuffer();
+           for (int i = -N; i <= N; i++) {
+               s.append(variable(i));
+               if (i < N) {
+                   s.append(" + ");
+               }
+           }
+           s.append(" - 1");
+           return s.toString();
+    }
 
-	String sumUm(int m) {
-		StringBuffer s = new StringBuffer();
-		for (int i = -N; i <= N; i++) {
-			s.append(variable(i));
-			s.append("*");
-			s.append(variable(m - i));
-			if (i < N) {
-				s.append(" + ");
-			}
-		}
-		s.append(" - " + variable(m));
-		return s.toString();
-	}
+    String sumUm(int m) {
+           StringBuffer s = new StringBuffer();
+           for (int i = -N; i <= N; i++) {
+               s.append(variable(i));
+               s.append("*");
+               s.append(variable(m - i));
+               if (i < N) {
+                  s.append(" + ");
+               }
+           }
+           s.append(" - " + variable(m));
+           return s.toString();
+    }
 
-	String varList(String order) {
-		StringBuffer s = new StringBuffer();
-		s.append("(");
-		for (int i = 0; i <= N; i++) {
-			s.append(variable(i));
-			if (i < N) {
-				s.append(",");
-			}
-		}
-		s.append(")  ");
-		s.append(order);
-		return s.toString();
-	}
+    String varList(String order) {
+           StringBuffer s = new StringBuffer();
+           s.append("(");
+           for (int i = 0; i <= N; i++) {
+               s.append(variable(i));
+               if (i < N) {
+                  s.append(",");
+               }
+           }
+           s.append(")  ");
+           s.append(order);
+           return s.toString();
+    }
 
-	public String toString() {
-		StringBuffer s = new StringBuffer();
-		s.append(varList(order));
-		s.append("\n");
-		s.append("(\n");
-		//for (int m = -N + 1; m <= N - 1; m++) { doubles polynomials
-	   for (int m = 0; m <= N - 1; m++) {
-			s.append(sumUm(m));
-			s.append(",\n");
-		}
-		s.append(sum1());
-		s.append("\n");
-		s.append(")\n");
-		return s.toString();
-	}
+    public String toString() {
+           StringBuffer s = new StringBuffer();
+           s.append(varList(order));
+           s.append("\n");
+           s.append("(\n");
+           //for (int m = -N + 1; m <= N - 1; m++) { doubles polynomials
+           for (int m = 0; m <= N - 1; m++) {
+               s.append(sumUm(m));
+               s.append(",\n");
+           }
+           s.append(sum1());
+           s.append("\n");
+           s.append(")\n");
+           return s.toString();
+    }
 
-	String variable(int i) {
-		if (i < 0) {
-			return variable(-i);
-		}
-		if (i > N) {
-			return "0";
-		}
-		return var + i;
-	}
+    String variable(int i) {
+           if (i < 0) {
+               return variable(-i);
+           }
+           if (i > N) {
+               return "0";
+           }
+           return var + i;
+    }
 
 }
