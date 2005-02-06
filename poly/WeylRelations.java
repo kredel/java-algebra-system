a96bb18de5a1881eac2757cdf98e83fb90ca6012
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2005-02-06 17:38:40 +0000

    generate for arbitrary solvable polynomials
    git-svn-id: file:///home/SUBVERSION/jas/trunk@289 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- poly/WeylRelations.java
+++ poly/WeylRelations.java
@@ -45,4 +45,29 @@ public class WeylRelations {
         return table;
     }
 
+    public RelationTable generate(int r, SolvableOrderedMapPolynomial ref) {
+        RelationTable table = new RelationTable();
+        if ( r <= 1 || (r % 2) != 0 ) {
+           throw new IllegalArgumentException("WeylRelations, wrong r = "+r);
+        }
+        int m = r / 2;
+        ExpVector z = new ExpVector(r);
+        OrderedPolynomial one = ref.getONE(table);
+        OrderedPolynomial zero = ref.getZERO(table);
+        // Coefficient one = a.leadingBaseCoefficient();
+        for ( int i = m; i < r; i++ ) {
+            ExpVector f = new ExpVector(r,i,1); 
+            int j = i - m;
+            ExpVector e = new ExpVector(r,j,1);
+            ExpVector ef = ExpVector.EVSUM(e,f);
+            OrderedPolynomial b = one.multiply(ef);
+            OrderedPolynomial rel = one.add(b);
+            table.update(e,f,rel);
+        }
+        if ( logger.isDebugEnabled() ) {
+           logger.debug("\nWeyl relations = " + table);
+        }
+        return table;
+    }
+
 }
