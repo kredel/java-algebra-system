11ca155fee06772e2a6431b9b0f8f0bacb79c2ff
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2005-02-07 13:50:12 +0000

    added left methods
    git-svn-id: file:///home/SUBVERSION/jas/trunk@298 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- ring/Reduction.java
+++ ring/Reduction.java
@@ -16,6 +16,7 @@ import org.apache.log4j.Logger;
 import edu.jas.arith.Coefficient;
 import edu.jas.poly.ExpVector;
 import edu.jas.poly.OrderedPolynomial;
+import edu.jas.poly.SolvablePolynomial;
 import edu.jas.util.DistHashTable;
 
 
@@ -28,6 +29,7 @@ import edu.jas.util.DistHashTable;
 public class Reduction  {
 
     private static Logger logger = Logger.getLogger(Reduction.class);
+
 
     /**
      * S-Polynomial
@@ -52,6 +54,43 @@ public class Reduction  {
         ExpVector e = (ExpVector) ma.getKey();
         ExpVector f = (ExpVector) mb.getKey();
 
+        ExpVector g  = ExpVector.EVLCM(e,f);
+        ExpVector e1 = ExpVector.EVDIF(g,e);
+        ExpVector f1 = ExpVector.EVDIF(g,f);
+
+        Coefficient a = (Coefficient) ma.getValue();
+        Coefficient b = (Coefficient) mb.getValue();
+
+        OrderedPolynomial App = Ap.multiply( b, e1 );
+        OrderedPolynomial Bpp = Bp.multiply( a, f1 );
+        OrderedPolynomial Cp = App.subtract(Bpp);
+        return Cp;
+    }
+
+
+    /**
+     * Left S-Polynomial
+     */
+
+    public static SolvablePolynomial leftSPolynomial(SolvablePolynomial Ap, 
+                                                     SolvablePolynomial Bp) {  
+        if ( logger.isInfoEnabled() ) {
+	   if ( Bp == null || Bp.isZERO() ) {
+	      return Ap;
+	   }
+	   if ( Ap == null || Ap.isZERO() ) {
+	      return (SolvablePolynomial)Bp.negate();
+	   }
+           if ( ! Ap.getTermOrder().equals( Bp.getTermOrder() ) ) { 
+              logger.error("term orderings not equal"); 
+           }
+	}
+        Map.Entry ma = Ap.leadingMonomial();
+        Map.Entry mb = Bp.leadingMonomial();
+
+        ExpVector e = (ExpVector) ma.getKey();
+        ExpVector f = (ExpVector) mb.getKey();
+
         ExpVector g = ExpVector.EVLCM(e,f);
         ExpVector e1 = ExpVector.EVDIF(g,e);
         ExpVector f1 = ExpVector.EVDIF(g,f);
@@ -59,10 +98,9 @@ public class Reduction  {
         Coefficient a = (Coefficient) ma.getValue();
         Coefficient b = (Coefficient) mb.getValue();
 
-        OrderedPolynomial App = (OrderedPolynomial) Ap.multiply( b, e1 );
-        OrderedPolynomial Bpp = (OrderedPolynomial) Bp.multiply( a, f1 );
-       
-        OrderedPolynomial Cp = (OrderedPolynomial) App.subtract(Bpp);
+        SolvablePolynomial App = Ap.multiplyLeft( b, e1 );
+        SolvablePolynomial Bpp = Bp.multiplyLeft( a, f1 );
+        SolvablePolynomial Cp = (SolvablePolynomial) App.subtract(Bpp);
         return Cp;
     }
 
@@ -80,6 +118,11 @@ public class Reduction  {
            if ( ! A.getTermOrder().equals( B.getTermOrder() ) ) { 
               logger.error("term orderings not equal"); 
            }
+           if (   A instanceof SolvablePolynomial
+               || B instanceof SolvablePolynomial ) {
+              logger.error("GBCriterion4 not applicabable to SolvablePolynomials"); 
+              return true;
+           }
 	}
         ExpVector ei = A.leadingExpVector();
         ExpVector ej = B.leadingExpVector();
@@ -92,6 +135,11 @@ public class Reduction  {
 
     public static boolean GBCriterion4(OrderedPolynomial A, 
                                        OrderedPolynomial B) {  
+        if (   A instanceof SolvablePolynomial
+            || B instanceof SolvablePolynomial ) {
+            logger.error("GBCriterion4 not applicabable to SolvablePolynomials"); 
+            return true;
+        }
         ExpVector ei = A.leadingExpVector();
         ExpVector ej = B.leadingExpVector();
         ExpVector g = ExpVector.EVSUM(ei,ej);
@@ -107,7 +155,7 @@ public class Reduction  {
      * Normalform.
      */
 
-    public static OrderedPolynomial Normalform(List Pp, 
+    public static OrderedPolynomial normalform(List Pp, 
                                                OrderedPolynomial Ap) {  
         if ( Pp == null ) return Ap;
         if ( Pp.isEmpty() ) return Ap;
@@ -159,8 +207,72 @@ public class Reduction  {
                  //logger.debug("red");
 		 e = ExpVector.EVDIF( e, htl[i] );
                  a = a.divide( lbc[i] );
-                 Q = (OrderedPolynomial) p[i].multiply( a, e );
+                 Q = p[i].multiply( a, e );
                  S = S.subtract( Q );
+              }
+	}
+        return R;
+    }
+
+
+    /**
+     * Left Normalform.
+     */
+
+    public static SolvablePolynomial leftNormalform(List Pp, 
+                                                    SolvablePolynomial Ap) {  
+        if ( Pp == null ) return Ap;
+        if ( Pp.isEmpty() ) return Ap;
+        int i;
+        int l = Pp.size();
+        Map.Entry m;
+        Object[] P;
+        synchronized (Pp) {
+           P = Pp.toArray();
+	}
+        ExpVector[] htl = new ExpVector[ l ];
+        Coefficient[] lbc = new Coefficient[ l ];
+        SolvablePolynomial[] p = new SolvablePolynomial[ l ];
+	int j = 0;
+        for ( i = 0; i < l; i++ ) { 
+            p[i] = (SolvablePolynomial) P[i];
+            m = p[i].leadingMonomial();
+	    if ( m != null ) { 
+               p[j] = p[i];
+               htl[j] = (ExpVector) m.getKey();
+               lbc[j] = (Coefficient) m.getValue();
+	       j++;
+	    }
+	}
+	l = j;
+        ExpVector e;
+        Coefficient a;
+        boolean mt = false;
+        SolvablePolynomial R = Ap.getZERO( Ap.getRelationTable(), Ap.getTermOrder() );
+
+        SolvablePolynomial T = null;
+        SolvablePolynomial Q = null;
+        SolvablePolynomial S = Ap;
+        while ( S.length() > 0 ) { 
+	      m = S.leadingMonomial();
+              e = (ExpVector) m.getKey();
+              a = (Coefficient) m.getValue();
+              for ( i = 0; i < l; i++ ) {
+                  mt = ExpVector.EVMT( e, htl[i] );
+                  if ( mt ) break; 
+	      }
+              if ( ! mt ) { 
+                 //logger.debug("irred");
+                 //T = new OrderedMapPolynomial( a, e );
+                 R = (SolvablePolynomial)R.add( a, e );
+                 S = (SolvablePolynomial)S.subtract( a, e ); 
+		 // System.out.println(" S = " + S);
+	      } else { 
+                 //logger.debug("red");
+		 e = ExpVector.EVDIF( e, htl[i] );
+                 a = a.divide( lbc[i] );
+                 Q = p[i].multiplyLeft( a, e );
+                 S = (SolvablePolynomial)S.subtract( Q );
               }
 	}
         return R;
@@ -171,7 +283,7 @@ public class Reduction  {
      * Normalform. Allows concurrent modification of the list.
      */
 
-    public static OrderedPolynomial NormalformMod(/*Array*/List Pp, 
+    public static OrderedPolynomial normalformMod(/*Array*/List Pp, 
                                                   OrderedPolynomial Ap) {  
         if ( Pp == null ) return Ap;
         if ( Pp.isEmpty() ) return Ap;
@@ -237,7 +349,7 @@ public class Reduction  {
      * Normalform. Allows concurrent modification of the list.
      */
 
-    public static OrderedPolynomial NormalformMod(DistHashTable Pp, 
+    public static OrderedPolynomial normalformMod(DistHashTable Pp, 
                                                   OrderedPolynomial Ap) {  
         if ( Pp == null ) return Ap;
         if ( Pp.isEmpty() ) return Ap;
@@ -303,7 +415,7 @@ public class Reduction  {
      * Irreducible set.
      */
 
-    public static ArrayList IrreducibleSet(List Pp) {  
+    public static ArrayList irreducibleSet(List Pp) {  
         OrderedPolynomial a;
         ArrayList P = new ArrayList();
         ListIterator it = Pp.listIterator();
@@ -326,7 +438,7 @@ public class Reduction  {
 	    a = (OrderedPolynomial) it.next();
             P.remove(0);
             e = a.leadingExpVector();
-            a = Normalform( P, a );
+            a = normalform( P, a );
             logger.debug(String.valueOf(irr));
             if ( a.length() == 0 ) { l--;
 	       if ( l <= 1 ) { return P; }
@@ -343,6 +455,57 @@ public class Reduction  {
                   irr = 0; a = a.monic();
 	       }
                P.add( (Object) a );
+	    }
+	}
+        //System.out.println();
+	return P;
+    }
+
+
+    /**
+     * Left irreducible set.
+     */
+
+    public static ArrayList leftIrreducibleSet(List Pp) {  
+        SolvablePolynomial a;
+        ArrayList P = new ArrayList();
+        ListIterator it = Pp.listIterator();
+        while ( it.hasNext() ) { 
+            a = (SolvablePolynomial) it.next();
+            if ( a.length() != 0 ) {
+               a = (SolvablePolynomial)a.monic();
+               P.add( a );
+	    }
+	}
+        int l = P.size();
+        if ( l <= 1 ) return P;
+
+        int irr = 0;
+        ExpVector e;        
+        ExpVector f;        
+        logger.debug("irr = ");
+        while ( irr != l ) {
+            it = P.listIterator(); 
+	    a = (SolvablePolynomial) it.next();
+            P.remove(0);
+            e = a.leadingExpVector();
+            a = leftNormalform( P, a );
+            logger.debug(String.valueOf(irr));
+            if ( a.length() == 0 ) { l--;
+	       if ( l <= 1 ) { return P; }
+	    } else {
+	       f = a.leadingExpVector();
+               if ( ExpVector.EVSIGN( f ) == 0 ) { 
+		  P = new ArrayList(); 
+                  P.add( (SolvablePolynomial)a.monic() ); 
+	          return P;
+               }    
+               if ( e.equals( f ) ) {
+		  irr++;
+	       } else {
+                  irr = 0; a = (SolvablePolynomial)a.monic();
+	       }
+               P.add( a );
 	    }
 	}
         //System.out.println();
