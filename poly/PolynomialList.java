a05f00b3f1d85fc77053084bdc16061dfd7dc145
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2005-02-27 16:29:52 +0000

    polynomial list with relation table, static sort method and cosmetic
    git-svn-id: file:///home/SUBVERSION/jas/trunk@338 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- poly/PolynomialList.java
+++ poly/PolynomialList.java
@@ -7,10 +7,13 @@ package edu.jas.poly;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 /**
  * list of polynomials
- * mainly for storage and printing/toString
+ * mainly for storage and printing/toString and sorting
  * @author Heinz Kredel
  */
 
@@ -19,25 +22,33 @@ public class PolynomialList {
     public final String[] vars;
     public final TermOrder tord;
     public final List list;
+    public final RelationTable table;
 
     public PolynomialList( String[] v, int eo, List l ) {
-	vars = v;
-	tord = new TermOrder(eo);
-	list = l;
+        this( v, new TermOrder(eo), l);
     }
 
     public PolynomialList( String[] v, TermOrder to, List l ) {
+        this( v, to, l, null);
+    }
+
+    public PolynomialList( String[] v, TermOrder to, 
+                           List l, RelationTable rt ) {
 	vars = v;
 	tord = to;
-	list = l;
+	list = sort(l);
+        table = rt;
     }
+
 
     public String toString() {
 	StringBuffer erg = new StringBuffer();
         erg.append("(");
         for ( int i = 0; i < vars.length; i++ ) {
             erg.append(vars[i]); 
-	    if ( i < vars.length-1 ) erg.append(","); 
+	    if ( i < vars.length-1 ) {
+               erg.append(",");
+            } 
         }
         erg.append(")");
         if ( tord.getWeight() == null ) {
@@ -47,7 +58,10 @@ public class PolynomialList {
         }
         erg.append("\n");
 
-        // Polynomial a;
+        if ( table != null ) {
+           erg.append(""+table+"\n\n");
+        }
+
         OrderedPolynomial oa;
         String sa;
         Iterator it = list.iterator();
@@ -78,4 +92,30 @@ public class PolynomialList {
         erg.append(")");
 	return erg.toString();
     }
+
+
+    /**
+     * Sort a list of polynomials with respect to the ascending order 
+     * of the leading Exponent vectors. 
+     * The term order is taken from the first polynomials TermOrder.
+     */
+
+    public static List sort(List l) {
+        if ( l == null ) {
+            return l;
+        }
+        if ( l.size() <= 1 ) { // nothing to sort
+            return l;
+        }
+        OrderedPolynomial p = (OrderedPolynomial)l.get(0);
+        Map map = new TreeMap( p.getTermOrder().getAscendComparator() );
+        for ( Iterator it = l.iterator(); it.hasNext(); ) {
+            p = (OrderedPolynomial)it.next();
+            map.put( p.leadingExpVector(), p );
+        }
+        List s = new ArrayList( map.values() );
+        return s;
+    }
+
+
 }
