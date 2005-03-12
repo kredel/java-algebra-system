cfaa3c7417e502402c2b9bc62fbb3e03abf42a05
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2005-03-12 15:37:16 +0000

    added module support
    git-svn-id: file:///home/SUBVERSION/jas/trunk@352 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- jas.py
+++ jas.py
@@ -4,10 +4,11 @@
 from java.lang import System
 from java.io import StringReader
 
-from edu.jas.arith import *
-from edu.jas.poly import *
-from edu.jas.ring import *
-from edu.jas.util import *
+from edu.jas.arith  import *
+from edu.jas.poly   import *
+from edu.jas.ring   import *
+from edu.jas.module import *
+from edu.jas.util   import *
 
 
 class Ring:
@@ -119,3 +120,44 @@ class SolvIdeal:
         g = PolynomialList(s.vars,s.tord,G,s.table);
         return g;
 
+
+
+
+class Module:
+
+    def __init__(self,str):
+        sr = StringReader( str );
+        tok = OrderedPolynomialTokenizer(sr);
+        self.mset = tok.nextSubModuleSet();
+        self.vars = self.mset.vars;
+        self.tord = self.mset.tord;
+
+    def __str__(self):
+        return str(self.mset);
+
+
+class SubModule:
+
+    def __init__(self,module,str):
+        self.module = module;
+        sr = StringReader( str );
+        tok = OrderedPolynomialTokenizer(module.mset.vars,module.mset.tord,sr);
+        self.list = tok.nextSubModuleList();
+        self.mset = ModuleList(module.mset.vars,module.mset.tord,self.list);
+        self.cols = self.mset.list[0].size();
+        #print "cols = %s" % self.cols;
+        self.pset = self.mset.getPolynomialList();
+
+    def __str__(self):
+        return str(self.mset) + "\n\n" + str(self.pset);
+
+    def GB(self):
+        s = self.pset;
+        F = s.list;
+        t = System.currentTimeMillis();
+        G = GroebnerBase.DIRPGB(F);
+        t = System.currentTimeMillis() - t;
+        print "executed in %s ms" % t; 
+        self.pset = PolynomialList(s.vars,s.tord,G);
+        self.mset = ModuleList.getModuleList(self.cols,self.pset);
+        return self;
