e5abe1e5a8d8320924da4e1878188bf207649ccd
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2005-03-28 22:07:23 +0000

    solvable syzygy examples
    git-svn-id: file:///home/SUBVERSION/jas/trunk@399 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ syzsolv.py
@@ -0,0 +1,41 @@
+#
+# jython examples for jas.
+
+from jas import SolvRing
+from jas import SolvIdeal
+
+# ? example
+
+rs = """
+     Rat(a,b,e1,e2,e3) L
+     RelationTable
+     (
+       ( e3 ), ( e1 ), ( e1 e3 - e1 ),
+       ( e3 ), ( e2 ), ( e2 e3 - e2 )
+     )
+""";
+
+r = SolvRing( rs );
+print "SolvRing: " + str(r);
+print;
+
+ps = """
+(
+ ( e1 e3^3 + e2^10 - a ),
+ ( e1^3 e2^2 + e3 ),
+ ( e3^3 + e3^2 - b )
+)
+""";
+
+f = SolvIdeal( r, ps );
+print "SolvIdeal: " + str(f);
+print;
+
+from edu.jas.module import SolvableSyzygy;
+
+R = SolvableSyzygy.resolution( f.pset );
+
+for i in range(0,R.size()): 
+   print "\n %s. resolution" % (i+1);
+   print "\n", R[i];
+
---
+++ syzy3.py
@@ -0,0 +1,35 @@
+#
+# jython examples for jas.
+
+from jas import Ring
+from jas import Ideal
+
+# ? example
+
+r = Ring( "Rat(x,y,z) L" );
+print "Ring: " + str(r);
+print;
+
+ps = """
+( 
+ ( z^3 - y ),
+ ( y z - x ),
+ ( y^3 - x^2 z ),
+ ( x z^2 - y^2 )
+) 
+""";
+
+f = Ideal( r, ps );
+print "Ideal: " + str(f);
+print;
+
+from edu.jas.module import Syzygy;
+from edu.jas.module import ModuleList;
+from edu.jas.module import ModGroebnerBase;
+
+R = Syzygy.resolution( f.pset );
+
+for i in range(0,R.size()): 
+   print "\n %s. resolution" % (i+1);
+   print "\n ", R[i];
+
