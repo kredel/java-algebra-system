baf9401e7f51d9b92015cce63f42c003e2163a2b
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2005-08-13 17:59:18 +0000

    added documentation comments
    git-svn-id: file:///home/SUBVERSION/jas/trunk@479 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- structure/RingElem.java
+++ structure/RingElem.java
@@ -15,40 +15,128 @@ public interface RingElem<C extends RingElem> extends Cloneable,
                                                       Comparable<C>, 
                                                       Serializable {
 
+
+    /**
+     * Clone this RingElem.
+     * @return Creates and returns a copy of this object.
+     */
     public C clone();
 
 
+
+    /**
+     * Test if this is zero.
+     * @return true if this is 0, else false.
+     */
     public boolean isZERO();
 
+
+    /**
+     * Test if this is one.
+     * @return true if this is 1, else false.
+     */
     public boolean isONE();
 
+
+    /**
+     * Test if this is a unit. 
+     * I.e. there exists x with this.multiply(x).isONE() == true.
+     * @return true if this is a unit, else false.
+     */
     public boolean isUnit();
 
 
+
+    /**
+     * Test if this is equal to b.
+     * @param b
+     * @return true if this is equal to b, else false.
+     */
     public boolean equals(Object b);
 
+
+    /**
+     * Hashcode of this RingElem.
+     * @return the hashCode.
+     */
     public int hashCode();
 
+
+    /**
+     * Compare this is to b.
+     * @param b
+     * @return 0 if this is equal to b, else +1 or -1.
+     */
     public int compareTo(C b);
 
+
+    /**
+     * Signum.
+     * @return the sign of this.
+     */
     public int signum();
 
 
+
+    /**
+     * Add S to this.
+     * @param S
+     * @return this + S.
+     */
     public C add(C S);
 
+
+    /**
+     * Subtract S from this.
+     * @param S
+     * @return this - S.
+     */
     public C subtract(C S);
 
+
+    /**
+     * Negate this.
+     * @return - this.
+     */
     public C negate();
 
+
+    /**
+     * Absolute value of this.
+     * @return |this|.
+     */
     public C abs();
 
 
+
+    /**
+     * Multiply S with this.
+     * @param S
+     * @return this * S.
+     */
     public C multiply(C S);
 
+
+    /**
+     * Divide this by S.
+     * @param S
+     * @return this / S.
+     */
     public C divide(C S);
 
+
+    /**
+     * Remainder after division by S.
+     * @param S
+     * @return this - (this / S) * S.
+     */
     public C remainder(C S);
 
+
+    /**
+     * Inverse of this.
+     * @return x with this.multiply(x).isONE().
+     */
     public C inverse();
 
 }
--- structure/RingFactory.java
+++ structure/RingFactory.java
@@ -16,20 +16,66 @@ import java.io.Serializable;
 
 public interface RingFactory<C extends RingElem> extends Serializable {
 
+
+    /**
+     * Get the constant zero for the RingElem.
+     * @return 0.
+     */
     public C getZERO();
 
+
+    /**
+     * Get the constant one for the RingElem.
+     * @return 1.
+     */
     public C getONE();
 
+
+    /**
+     * Get the RingElem for a.
+     * @param a
+     * @return a: RingElem.
+     */
     public C fromInteger(long a);
 
+
+    /**
+     * Get the RingElem for a.
+     * @param a
+     * @return a: RingElem.
+     */
     public C fromInteger(BigInteger a);
 
+
+    /**
+     * Generate a random RingElem with size less equal to 2^n.
+     * @param n
+     * @return a random element.
+     */
     public C random(int n);
 
+
+    /**
+     * Create a copy of RingElem c.
+     * @param c
+     * @return a copy of c.
+     */
     public C copy(C c);
 
+
+    /**
+     * Parse from String.
+     * @param s String.
+     * @return a RingElem corresponding to s.
+     */
     public C parse(String s);
 
+
+    /**
+     * Parse from Reader.
+     * @param r Reader.
+     * @return the next RingElem found on r.
+     */
     public C parse(Reader r);
 
 }
