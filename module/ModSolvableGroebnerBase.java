2736b1fd1adac07ace0f7ab7918e3ffa1d01c411
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2006-02-11 18:15:54 +0000

    new right case methods
    git-svn-id: file:///home/SUBVERSION/jas/trunk@592 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- module/ModSolvableGroebnerBase.java
+++ module/ModSolvableGroebnerBase.java
@@ -200,12 +200,58 @@ public class ModSolvableGroebnerBase<C extends RingElem<C>> {
 
 
     /**
-     * Left reduction base.
-     * Reduce matrix to diagonal form using rows with units.
+     * Module right Groebner base test.
+     * @param C coefficient type.
+     * @param modv number of modul variables.
+     * @param F a module basis.
+     * @return true, if F is a right Groebner base, else false.
      */
-    /* ----------------------------------------------------------------------
+    public boolean 
+           isRightGB(int modv, List<GenSolvablePolynomial<C>> F) {  
+        return sbb.isRightGB(modv,F);
+    }
+
+
+    /**
+     * Module right Groebner base test.
+     * @param C coefficient type.
+     * @param M a module basis.
+     * @return true, if M is a right Groebner base, else false.
+     */
+    public boolean 
+           isRightGB(ModuleList<C> M) {  
+        if ( M == null || M.list == null ) {
+            return true;
+        }
+        if ( M.rows == 0 || M.cols == 0 ) {
+            return true;
+        }
+        int modv = M.cols; // > 0  
+        PolynomialList<C> F = M.getPolynomialList();
+        return sbb.isRightGB(modv,F.castToSolvableList());
+    }
+
+
+    /**
+     * Right Groebner base using pairlist class.
+     * @param C coefficient type.
+     * @param modv number of modul variables.
+     * @param F a module basis.
+     * @return rightGB(F) a right Groebner base for F.
+     */
+    public List<GenSolvablePolynomial<C>> 
+           rightGB(int modv, List<GenSolvablePolynomial<C>> F) {  
+        return sbb.rightGB(modv,F);
+    }
+
+    /**
+     * Right Groebner base using pairlist class.
+     * @param C coefficient type.
+     * @param M a module basis.
+     * @return rightGB(M) a right Groebner base for M.
+     */
     public ModuleList<C> 
-           leftReductionBase(ModuleList<C> M) {  
+           rightGB(ModuleList<C> M) {  
         ModuleList<C> N = M;
         if ( M == null || M.list == null ) {
             return N;
@@ -213,301 +259,16 @@ public class ModSolvableGroebnerBase<C extends RingElem<C>> {
         if ( M.rows == 0 || M.cols == 0 ) {
             return N;
         }
+        PolynomialList<C> F = M.getPolynomialList();
+        GenSolvablePolynomialRing<C> sring 
+            = (GenSolvablePolynomialRing<C>)F.ring;
         int modv = M.cols;
-        List<List<GenSolvablePolynomial<C>>> mo = M.list;
-
-        // find rows with unit components
-        List<List<GenSolvablePolynomial<C>>> noOne 
-            = new ArrayList<List<GenSolvablePolynomial<C>>>();
-        List<List<GenSolvablePolynomial<C>>> withOne 
-            = new ArrayList<List<GenSolvablePolynomial<C>>>();
-        for (Iterator it = mo.iterator(); it.hasNext(); ) {
-            List<GenSolvablePolynomial<C>> row = it.next();
-            if ( hasOne( row ) >= 0 ) {
-                withOne.add( row );
-            } else {
-                noOne.add( row );
-            }
-        }
-        // sort rows with unit components to reduction order
-        int kk = ((List)withOne.get( 0 )).size();
-        Object[] temp = new Object[ kk ];
-        for ( int i = 0; i < withOne.size(); i++ ) {
-            List w = (List)withOne.get( i );
-            int k = hasOne( w );
-            temp[ k ] = w;
-        }
-        withOne = new ArrayList( withOne.size() );
-        for ( int i = temp.length-1; i >= 0; i-- ) {
-            if ( temp[ i ] != null ) {
-               withOne.add( temp[ i ] );
-            }
-        }
-        List old = new ArrayList( withOne );
-        // interreduce this vectors
-        for ( int i = 0; i < withOne.size(); i++ ) {
-            Object r = withOne.remove( 0 );
-            List R = new ArrayList();
-            R.add( r );
-            withOne = gaussReduce( withOne, R );
-            withOne.add( r );
-        }
-
-        if ( true || logger.isDebugEnabled() ) {
-           ModuleList m1 = new ModuleList(M.coeff,M.vars,M.tord,old,M.table);
-           logger.info("old rows with 1:\n" + m1);
-           //System.out.println("old rows with 1:\n" + m1);
-
-           m1 = new ModuleList(M.coeff,M.vars,M.tord,withOne,M.table);
-           logger.info("rows with 1:\n" + m1);
-           System.out.println("rows with 1:\n" + m1);
-
-           ModuleList mn = new ModuleList(M.coeff,M.vars,M.tord,noOne,M.table);
-        }
-
-        // Gaussian reduction loops
-        List red = gaussReduce( noOne, withOne );
-
-        N = new ModuleList(M.coeff,M.vars,M.tord,red,M.table);
-        N.list.addAll( withOne );
+        List<GenSolvablePolynomial<C>> G 
+            = sbb.rightGB(modv,F.castToSolvableList());
+        F = new PolynomialList<C>(sring,G);
+        //System.out.println("rightGB -------------------- \n" + F);
+        N = F.getModuleList(modv);
         return N;
     }
-    */
-
-    /**
-     * test if vector has a component with a unit.
-     * return last index of one component.
-     */
-    /*
-    public int hasOne( List row ) {
-        int col = -1;
-        int i = 0;
-        for (Iterator it = row.iterator(); it.hasNext(); i++ ) {
-            OrderedPolynomial pol = (OrderedPolynomial)it.next();
-            if ( pol != null ) {
-                if ( pol.isONE() || pol.negate().isONE() ) {
-                    col = i;
-                }
-            }
-        }
-        return col;
-    }
-    */
-
-
-    /**
-     * number of trailing zero components.
-     */
-    /*
-    public int trailingZeros( List row ) {
-        int col = row.size()-1;
-        for (int i = col; i >= 0; i-- ) {
-            OrderedPolynomial pol = (OrderedPolynomial)row.get(i);
-            if ( pol != null ) {
-                if ( !pol.isZERO() ) {
-                    return col-i;
-                }
-            }
-        }
-        return row.size();
-    }
-    */
-
-    /**
-     * Linear combination.
-     * compute a + l b.
-     */
-    /*
-    public List linearCombination( List a, SolvablePolynomial l, List b ) {
-        if ( a == null ) {
-            throw new RuntimeException("a null " + a );
-            //return null; // wrong @fixme
-        }
-        if ( l == null || b == null ) {
-            return a; 
-        }
-        if ( a.size() != b.size() ) {
-            throw new RuntimeException("a,b wrong sizes " + a + ", " + b );
-        }
-        if ( l.isZERO() ) {
-            //System.out.println("l is zero");
-            return a;
-        }
-        List c = new ArrayList( a.size() );
-        Iterator jt = b.iterator();
-        for (Iterator it = a.iterator(); it.hasNext(); ) {
-            SolvablePolynomial ap = (SolvablePolynomial)it.next();
-            SolvablePolynomial bp = (SolvablePolynomial)jt.next();
-            OrderedPolynomial cp = ap.add( l.multiply(bp) );
-            c.add( cp );
-        }
-        return c;
-    }
-    */
-
-    /**
-     * Reduce a with b.
-     */
-    /*
-    public List gaussReduce( List a, List b ) {
-        if ( a == null ) {
-            return null; 
-        }
-        if ( b == null ) {
-            return a; 
-        }
-        // Gaussian reduction loops
-        List red = new ArrayList();
-        for ( Iterator it = a.iterator(); it.hasNext(); ) {
-            List r = (List) it.next();
-            //PolynomialList F = new PolynomialList(M.coeff,M.vars,M.tord,r,M.table);
-            //System.out.println("r = " + F);
-            for ( Iterator jt = b.iterator(); jt.hasNext(); ) {
-                List m = (List) jt.next();
-
-                int k = hasOne( m );
-                // assert k >= 0;
-                SolvablePolynomial l = (SolvablePolynomial)r.get(k);
-                SolvablePolynomial e = (SolvablePolynomial)m.get(k);
-                if ( e.isONE() ) { 
-                   l = (SolvablePolynomial)l.negate();
-                } // else e = -1
-                //System.out.println("k = " + k);
-                //System.out.println("l = " + l);
-
-                r = linearCombination( r, l, m );
-            }
-            //F = new PolynomialList(M.coeff,M.vars,M.tord,r,M.table);
-            //System.out.println("r = " + F);
-            //System.out.println("trailingZeros(r) = " + trailingZeros( r ));
-            if ( trailingZeros( r ) < r.size() ) {
-               red.add(r);
-            }
-        }
-
-        return red;
-    }
-    */
-
-
-    /**
-     * intersect to n components.
-     */
-    /*
-    public List intersect( int p, List row ) {
-        List irow = new ArrayList( p );
-        for (int i = 0; i < p; i++ ) {
-            OrderedPolynomial pol = (OrderedPolynomial)row.get(i);
-            irow.add( pol );
-        }
-        return irow;
-    }
-    */
-
-    /**
-     * Intersection to n components.
-     */
-    /*
-    public ModuleList intersection(int p, ModuleList M) {  
-        ModuleList N = M;
-        List mo = (List)M.list;
-        if ( mo == null || mo.size() == 0 ) {
-           return N;
-        }
-        List red = new ArrayList();
-        for ( Iterator it = mo.iterator(); it.hasNext(); ) {
-            List r = (List) it.next();
-            //System.out.println("s-trailingZeros(r) = " + (r.size()-trailingZeros( r )));
-            if ( r.size()-trailingZeros( r ) <= p ) {
-               r = intersect(p,r);
-               red.add(r);
-            }
-        }
-        N = new ModuleList(M.coeff,M.vars,M.tord,red,M.table);
-        return N;
-    }
-    */
-
-    /**
-     * Left irreducible base.
-     */
-    /*
-    public ArrayList leftIrreducibleBase(int modv, List F) {  
-        return leftIrreducibleSet(modv,F);
-    }
-    */
-
-
-    /**
-     * Left irreducible base.
-     */
-    /*
-    public ModuleList leftIrreducibleBase(ModuleList M) {  
-        ModuleList N = M;
-        List mo = (List)M.list;
-        int modv = 0;
-        if ( mo == null || mo.size() == 0 ) {
-           return N;
-        }
-        modv = ((List)mo.get(0)).size();
-        PolynomialList F = M.getPolynomialList();
-        List G = leftIrreducibleSet(modv,F.list);
-        F = new PolynomialList(F.coeff,F.vars,F.tord,G,F.table);
-        N = ModuleList.getModuleList(modv,F);
-        return N;
-    }
-    */
-
-
-    /**
-     * Left irreducible set.
-     */
-    /*
-    public ArrayList leftIrreducibleSet(int modv, List Pp) {  
-        SolvablePolynomial a;
-        ArrayList P = new ArrayList();
-        ListIterator it = Pp.listIterator();
-        while ( it.hasNext() ) { 
-            a = (SolvablePolynomial) it.next();
-            if ( a.length() != 0 ) {
-               a = (SolvablePolynomial)a.monic();
-               P.add( a );
-	    }
-	}
-        int l = P.size();
-        if ( l <= 1 ) return P;
-
-        int irr = 0;
-        ExpVector e;        
-        ExpVector f;        
-        logger.debug("irr = ");
-        while ( irr != l ) {
-            it = P.listIterator(); 
-	    a = (SolvablePolynomial) it.next();
-            P.remove(0);
-            e = a.leadingExpVector();
-            a = Reduction.leftNormalform( P, a );
-            logger.debug(String.valueOf(irr));
-            if ( a.length() == 0 ) { l--;
-	       if ( l <= 1 ) { return P; }
-	    } else {
-	       f = a.leadingExpVector();
-               if ( ExpVector.EVSIGN( f ) == 0 ) { 
-		  P = new ArrayList(); 
-                  P.add( (SolvablePolynomial)a.monic() ); 
-	          return P;
-               }    
-               if ( e.equals( f ) ) {
-		  irr++;
-	       } else {
-                  irr = 0; a = (SolvablePolynomial)a.monic();
-	       }
-               P.add( a );
-	    }
-	}
-        //System.out.println();
-	return P;
-    }
-    */
 
 }
