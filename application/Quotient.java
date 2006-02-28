ee6e1dabdcde8f7cb3fcf21a8607afe09047b377
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2006-02-28 17:27:19 +0000

    new rational quotient clases
    git-svn-id: file:///home/SUBVERSION/jas/trunk@709 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ application/Quotient.java
@@ -0,0 +1,288 @@
+/*
+ * $Id: Quotient.java $
+ */
+
+package edu.jas.application;
+
+import edu.jas.poly.GenPolynomial;
+import edu.jas.structure.PrettyPrint;
+import edu.jas.structure.RingElem;
+
+
+/**
+ * Quotient class element based on GenPolynomial with RingElem interface.
+ * Objects of this class are (nearly) immutable.
+ * @author Heinz Kredel
+ */
+public class Quotient<C extends RingElem<C> > 
+             implements RingElem< Quotient<C> > {
+
+
+    /** Quotient class factory data structure. 
+     */
+    protected final QuotientRing<C> ring;
+
+
+    /** Numerator part of the element data structure. 
+     */
+    protected final GenPolynomial<C> num;
+
+
+    /** Denominator part of the element data structure. 
+     */
+    protected final GenPolynomial<C> den;
+
+
+    /** The constructor creates a Quotient object 
+     * from a ring factory. 
+     * @param r ring factory.
+     */
+    public Quotient(QuotientRing<C> r) {
+        this( r, r.ring.getZERO() );
+    }
+
+
+    /** The constructor creates a Quotient object 
+     * from a ring factory and a numerator polynomial. 
+     * The denominator is assumed to be 1.
+     * @param r ring factory.
+     * @param n numerator polynomial.
+     */
+    public Quotient(QuotientRing<C> r, GenPolynomial<C> n) {
+        this( r, n, r.ring.getONE() );
+    }
+
+
+    /** The constructor creates a Quotient object 
+     * from a ring factory and a numerator and denominator polynomial. 
+     * @param r ring factory.
+     * @param n numerator polynomial.
+     * @param d denominator polynomial.
+     */
+    public Quotient(QuotientRing<C> r, 
+                    GenPolynomial<C> n, GenPolynomial<C> d) {
+        ring = r;
+        // must reduce to lowest terms
+        if ( d == null || d.isZERO() ) {
+           throw new RuntimeException("denominator may not be zero");
+        }
+        if ( d.signum() >= 0 ) {
+           num = n;
+           den = d;
+        } else {
+           num = n.negate();
+           den = d.negate();
+        }
+    }
+
+
+    /**  Clone this.
+     * @see java.lang.Object#clone()
+     */
+    public Quotient<C> clone() {
+        return new Quotient<C>( ring, num, den );
+    }
+   
+
+    /** Is Quotient zero. 
+     * @return If this is 0 then true is returned, else false.
+     * @see edu.jas.structure.RingElem#isZERO()
+     */
+    public boolean isZERO() {
+        return num.isZERO();
+    }
+
+
+    /** Is Quotient one. 
+     * @return If this is 1 then true is returned, else false.
+     * @see edu.jas.structure.RingElem#isONE()
+     */
+    public boolean isONE() {
+        return num.equals( den );
+    }
+
+
+    /** Is Quotient unit. 
+     * @return If this is a unit then true is returned, else false.
+     * @see edu.jas.structure.RingElem#isUnit()
+     */
+    public boolean isUnit() {
+        if ( num.isZERO() ) {
+           return false;
+        } else {
+           return true;
+        }
+    }
+
+
+    /** Get the String representation as RingElem.
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        if ( PrettyPrint.isTrue() ) {
+           return num.toString( ring.ring.getVars() ) 
+                  + "/" + den.toString( ring.ring.getVars() );
+        } else {
+           return "Quotient[ " + num.toString() 
+                   + " / " + den.toString() + " ]";
+        }
+    }
+
+
+    /** Quotient comparison.  
+     * @param b Quotient.
+     * @return sign(this-b).
+     */
+    public int compareTo(Quotient<C> b) {
+        Quotient<C> s = this.subtract( b );
+        return s.signum();
+    }
+
+
+    /** Comparison with any other object.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @SuppressWarnings("unchecked") // not jet working
+    @Override
+    public boolean equals(Object b) {
+        if ( ! ( b instanceof Quotient ) ) {
+           return false;
+        }
+        Quotient<C> a = null;
+        try {
+            a = (Quotient<C>) b;
+        } catch (ClassCastException e) {
+        }
+        if ( a == null ) {
+            return false;
+        }
+        return ( 0 == compareTo( a ) );
+    }
+
+
+    /** Quotient absolute value.
+     * @return the absolute value of this.
+     * @see edu.jas.structure.RingElem#abs()
+     */
+    public Quotient<C> abs() {
+        return new Quotient<C>( ring, num.abs(), den );
+    }
+
+
+    /** Quotient addition.
+     * @param S Quotient.
+     * @return this+S.
+     */
+    public Quotient<C> add(Quotient<C> S) {
+        if ( S == null || S.isZERO() ) {
+           return this;
+        }
+        GenPolynomial<C> d = den.multiply( S.den );
+        GenPolynomial<C> n = num.multiply( S.den );
+        n.add( den.multiply( S.num ) ); 
+        return new Quotient<C>( ring, n, d );
+    }
+
+
+    /** Quotient negate.
+     * @return -this.
+     * @see edu.jas.structure.RingElem#negate()
+     */
+    public Quotient<C> negate() {
+        return new Quotient<C>( ring, num.negate(), den );
+    }
+
+
+    /** Quotient signum.
+     * @see edu.jas.structure.RingElem#signum()
+     * @return signum(this).
+     */
+    public int signum() {
+        return num.signum();
+    }
+
+
+    /** Quotient subtraction.
+     * @param S Quotient.
+     * @return this-S.
+     */
+    public Quotient<C> subtract(Quotient<C> S) {
+        if ( S == null || S.isZERO() ) {
+           return this;
+        }
+        GenPolynomial<C> d = den.multiply( S.den );
+        GenPolynomial<C> n = num.multiply( S.den );
+        n.subtract( den.multiply( S.num ) ); 
+        return new Quotient<C>( ring, n, d );
+    }
+
+
+    /** Quotient division.
+     * @param S Quotient.
+     * @return this/S.
+     */
+    public Quotient<C> divide(Quotient<C> S) {
+        return multiply( S.inverse() );
+    }
+
+
+    /** Quotient inverse.  
+     * @see edu.jas.structure.RingElem#inverse()
+     * @return S with S = 1/this. 
+     */
+    public Quotient<C> inverse() {
+        return new Quotient<C>( ring, den, num );
+    }
+
+
+    /** Quotient remainder.
+     * @param S Quotient.
+     * @return this - (this/S)*S.
+     */
+    public Quotient<C> remainder(Quotient<C> S) {
+        if ( this.isZERO() ) {
+
+        }
+        return ring.getZERO();
+    }
+
+
+    /** Quotient multiplication.
+     * @param S Quotient.
+     * @return this*S.
+     */
+    public Quotient<C> multiply(Quotient<C> S) {
+        if ( S == null || S.isZERO() ) {
+           return S;
+        }
+        if ( num.isZERO() ) {
+           return this;
+        }
+        if ( S.isONE() ) {
+           return this;
+        }
+        if ( this.isONE() ) {
+           return S;
+        }
+        GenPolynomial<C> d = den.multiply( S.den );
+        GenPolynomial<C> n = num.multiply( S.num );
+        return new Quotient<C>( ring, n, d );
+
+    }
+
+ 
+    /** Quotient monic.
+     * @return this with monic value part.
+     */
+    public Quotient<C> monic() {
+        if ( num.isZERO() ) {
+           return this;
+        }
+        C lbc = num.leadingBaseCoefficient();
+        lbc = lbc.inverse();
+        GenPolynomial<C> n = num.multiply( lbc );
+        GenPolynomial<C> d = den.multiply( lbc );
+        return new Quotient<C>( ring, n, d );
+    }
+
+}
---
+++ application/QuotientRing.java
@@ -0,0 +1,173 @@
+/*
+ * $Id: QuotientRing.java $
+ */
+
+package edu.jas.application;
+
+import java.util.Random;
+import java.io.Reader;
+
+import org.apache.log4j.Logger;
+
+import edu.jas.structure.RingElem;
+import edu.jas.structure.RingFactory;
+import edu.jas.structure.PrettyPrint;
+
+import edu.jas.poly.GenPolynomial;
+import edu.jas.poly.GenPolynomialRing;
+
+
+/**
+ * Algebraic number class based on GenPolynomial with RingElem interface.
+ * Objects of this class are immutable.
+ * @author Heinz Kredel
+ */
+public class QuotientRing<C extends RingElem<C> > 
+             implements RingFactory< Quotient<C> >  {
+
+     private static Logger logger = Logger.getLogger(QuotientRing.class);
+     private boolean debug = logger.isDebugEnabled();
+
+
+    /** Polynomial ring of the factory. 
+     */
+    protected final GenPolynomialRing<C> ring;
+
+
+    /** The constructor creates a QuotientRing object 
+     * from a GenPolynomialRing and a GenPolynomial list. 
+     * @param r polynomial ring.
+     */
+    public QuotientRing(GenPolynomialRing<C> r) {
+        ring = r;
+    }
+
+
+    /** Copy Quotient element c.
+     * @param c
+     * @return a copy of c.
+     */
+    public Quotient<C> copy(Quotient<C> c) {
+        return new Quotient<C>( c.ring, c.num, c.den );
+    }
+
+
+    /** Get the zero element.
+     * @return 0 as Quotient.
+     */
+    public Quotient<C> getZERO() {
+        return new Quotient<C>( this, ring.getZERO() );
+    }
+
+
+    /**  Get the one element.
+     * @return 1 as Quotient.
+     */
+    public Quotient<C> getONE() {
+        return new Quotient<C>( this, ring.getONE() );
+    }
+
+    
+    /** Get a Quotient element from a BigInteger value.
+     * @param a BigInteger.
+     * @return a Quotient.
+     */
+    public Quotient<C> fromInteger(java.math.BigInteger a) {
+        return new Quotient<C>( this, ring.fromInteger(a) );
+    }
+
+
+    /** Get a Quotient element from a long value.
+     * @param a long.
+     * @return a Quotient.
+     */
+    public Quotient<C> fromInteger(long a) {
+        return new Quotient<C>( this, ring.fromInteger(a) );
+    }
+    
+
+    /** Get the String representation as RingFactory.
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return "Quotient[ " 
+                + ring.toString() + " ]";
+    }
+
+
+    /** Comparison with any other object.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    @SuppressWarnings("unchecked") // not jet working
+    public boolean equals(Object b) {
+        if ( ! ( b instanceof QuotientRing ) ) {
+           return false;
+        }
+        QuotientRing<C> a = null;
+        try {
+            a = (QuotientRing<C>) b;
+        } catch (ClassCastException e) {
+        }
+        if ( a == null ) {
+            return false;
+        }
+        return ring.equals( a.ring );
+    }
+
+
+    /** Quotient random.
+     * @param n such that 0 &le; v &le; (2<sup>n</sup>-1).
+     * @return a random residue element.
+     */
+    public Quotient<C> random(int n) {
+        GenPolynomial<C> x = ring.random( n ).monic();
+        return new Quotient<C>( this, x );
+    }
+
+
+    /**
+     * Generate a random residum polynomial.
+     * @param k bitsize of random coefficients.
+     * @param l number of terms.
+     * @param d maximal degree in each variable.
+     * @param q density of nozero exponents.
+     * @return a random residue polynomial.
+     */
+    public Quotient<C> random(int k, int l, int d, float q) {
+        GenPolynomial<C> x = ring.random(k,l,d,q).monic();
+        return new Quotient<C>( this, x );
+    }
+
+
+    /** Quotient random.
+     * @param n such that 0 &le; v &le; (2<sup>n</sup>-1).
+     * @param rnd is a source for random bits.
+     * @return a random residue element.
+     */
+    public Quotient<C> random(int n, Random rnd) {
+        GenPolynomial<C> x = ring.random( n, rnd ).monic();
+        return new Quotient<C>( this, x);
+    }
+
+
+    /** Parse Quotient from String.
+     * @param s String.
+     * @return Quotient from s.
+     */
+    public Quotient<C> parse(String s) {
+        GenPolynomial<C> x = ring.parse( s );
+        return new Quotient<C>( this, x );
+    }
+
+
+    /** Parse Quotient from Reader.
+     * @param r Reader.
+     * @return next Quotient from r.
+     */
+    public Quotient<C> parse(Reader r) {
+        GenPolynomial<C> x = ring.parse( r );
+        return new Quotient<C>( this, x );
+    }
+
+}
