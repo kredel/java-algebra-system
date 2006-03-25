e1882b75f8bc7f653f9e0f03257463da41d4dc38
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2006-03-25 10:47:41 +0000

    some string utilities
    git-svn-id: file:///home/SUBVERSION/jas/trunk@823 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ util/StringUtil.java
@@ -0,0 +1,51 @@
+/*
+ * $Id$
+ */
+
+package edu.jas.util;
+
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.IOException;
+
+
+/**
+ * Class with static String and Reader methods.
+ * @author Heinz Kredel
+ */
+
+public class StringUtil {
+
+
+    /** Parse white space delimited String from Reader.
+     * @param r Reader.
+     * @return next non white space String from r.
+     */
+    public static String nextString(Reader r) {
+        StringWriter sw = new StringWriter();
+        try {
+            char buffer;
+            int i;
+            // skip white space
+            while ( ( i = r.read() ) > -1 ) {
+                buffer = (char) i;
+                if ( ! Character.isWhitespace( buffer ) ) {
+                   sw.write(buffer);
+                   break;
+                }
+            }
+            // read non white space, ignore new lines ?
+            while ( ( i = r.read() ) > -1 ) {
+                buffer = (char) i;
+                if ( Character.isWhitespace( buffer ) ) {
+                   break;
+                }
+                sw.write(buffer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sw.toString();
+    }
+
+}
