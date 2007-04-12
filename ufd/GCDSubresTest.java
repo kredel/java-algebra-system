9b34a4ae8cecca9bbb283ca4222e4fb810639b54
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-04-12 19:04:13 +0000

    added tests for sub resultants
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1004 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- ufd/GCDSubresTest.java
+++ ufd/GCDSubresTest.java
@@ -120,7 +120,7 @@ public class GCDSubresTest extends TestCase {
          a = dfac.random(kl*(i+2),ll+2*i,el+2,q);
          b = dfac.random(kl*(i+2),ll+2*i,el+2,q);
          c = dfac.random(kl*(i+2),ll+2,el+2,q);
-         c = c.multiply( cfac.univariate(0) );
+         c = c.multiply( dfac.univariate(0) );
          if ( c.isZERO() ) {
              // skip for this turn
             continue;
@@ -266,5 +266,154 @@ public class GCDSubresTest extends TestCase {
      }
  }
 
+
+/**
+ * Test base subresultant.
+ * 
+ */
+ public void testBaseSubresultant() {
+
+     GreatestCommonDivisorSubres<BigInteger> ufd 
+        = new GreatestCommonDivisorSubres<BigInteger>();
+
+     dfac = new GenPolynomialRing<BigInteger>(new BigInteger(1),1,to);
+
+     for (int i = 0; i < 1; i++) {
+         a = dfac.random(kl*(i+2),ll+2*i,el+2,q);
+         b = dfac.random(kl*(i+2),ll+2*i,el+2,q);
+         c = dfac.random(kl,ll,2,q);
+         //c = c.multiply( cfac.univariate(0) );
+         //c = dfac.getONE();
+         if ( c.isZERO() ) {
+             // skip for this turn
+            continue;
+         }
+         //System.out.println("a  = " + a);
+         //System.out.println("b  = " + b);
+         //System.out.println("c  = " + c);
+
+         assertTrue("length( c"+i+" ) <> 0", c.length() > 0);
+         //assertTrue(" not isZERO( c"+i+" )", !c.isZERO() );
+         //assertTrue(" not isONE( c"+i+" )", !c.isONE() );
+         
+         a = a.multiply(c);
+         b = b.multiply(c);
+
+         d = ufd.baseResultant(a,b);
+         e = ufd.baseGcd(a,b);
+         //System.out.println("d  = " + d);
+         //System.out.println("c  = " + c);
+         //System.out.println("e  = " + e);
+         if ( !e.isConstant() ) {
+            assertTrue("res(a,b) == 0 " + d, d.isZERO() );
+         } else {
+            assertTrue("res(a,b) != 0 " + d, !d.isZERO() );
+         }
+     }
+ }
+
+
+/**
+ * Test recursive subresultant.
+ * 
+ */
+ public void testRecursiveSubresultant() {
+
+     GreatestCommonDivisorSubres<BigInteger> ufd 
+        = new GreatestCommonDivisorSubres<BigInteger>();
+
+     di = new BigInteger( 1 );
+     dfac = new GenPolynomialRing<BigInteger>(new BigInteger(1),2,to);
+     cfac = new GenPolynomialRing<BigInteger>(new BigInteger(1),2-1,to);
+     rfac = new GenPolynomialRing<GenPolynomial<BigInteger>>(cfac,1,to);
+
+     for (int i = 0; i < 5; i++) {
+         ar = rfac.random(kl,ll,el+i,q);
+         br = rfac.random(kl,ll,el,q);
+         cr = rfac.random(kl,ll,2,q);
+         //cr = rfac.getONE(); 
+         //cr = ufd.recursivePrimitivePart(cr).abs();
+         //cr = cr.multiply( rfac.univariate(0) );
+         //System.out.println("ar = " + ar);
+         //System.out.println("br = " + br);
+         //System.out.println("cr = " + cr);
+
+         if ( ar.isZERO() || br.isZERO() || cr.isZERO() ) {
+            // skip for this turn
+            continue;
+         }
+         assertTrue("length( cr"+i+" ) <> 0", cr.length() > 0);
+         //assertTrue(" not isZERO( c"+i+" )", !c.isZERO() );
+         //assertTrue(" not isONE( c"+i+" )", !c.isONE() );
+         
+         ar = ar.multiply(cr);
+         br = br.multiply(cr);
+         //System.out.println("ar = " + ar);
+         //System.out.println("br = " + br);
+
+         dr = ufd.recursiveResultant(ar,br);
+         //System.out.println("cr = " + cr);
+         //System.out.println("dr = " + dr);
+         er = ufd.recursiveGcd(ar,br);
+         //System.out.println("er = " + er);
+
+         if ( !er.isConstant() ) {
+            assertTrue("res(a,b) == 0 " + dr, dr.isZERO() || !d.isConstant() );
+         } else {
+            assertTrue("res(a,b) != 0 " + dr, !dr.isZERO() );
+         }
+     }
+ }
+
+
+/**
+ * Test subresultant.
+ * 
+ */
+ public void testSubres() {
+
+     GreatestCommonDivisorSubres<BigInteger> ufd 
+        = new GreatestCommonDivisorSubres<BigInteger>();
+
+     dfac = new GenPolynomialRing<BigInteger>(new BigInteger(1),3,to);
+
+     for (int i = 0; i < 2; i++) {
+         a = dfac.random(kl,ll,el,q);
+         b = dfac.random(kl,ll,el,q);
+         c = dfac.random(kl,ll,2,q);
+         //c = dfac.getONE();
+         //c = c.multiply( dfac.univariate(0) );
+         //c = ufd.primitivePart(c).abs();
+         //System.out.println("a = " + a);
+         //System.out.println("b = " + b);
+         //System.out.println("c = " + c);
+
+         if ( a.isZERO() || b.isZERO() || c.isZERO() ) {
+            // skip for this turn
+            continue;
+         }
+         assertTrue("length( c"+i+" ) <> 0", c.length() > 0);
+         //assertTrue(" not isZERO( c"+i+" )", !c.isZERO() );
+         //assertTrue(" not isONE( c"+i+" )", !c.isONE() );
+         
+         a = a.multiply(c);
+         b = b.multiply(c);
+         //System.out.println("a = " + a);
+         //System.out.println("b = " + b);
+
+         d = ufd.resultant(a,b);
+         e = ufd.gcd(a,b);
+         //System.out.println("c = " + c);
+         //System.out.println("d = " + d);
+         //System.out.println("e = " + e);
+
+         if ( !e.isConstant() ) {
+            assertTrue("res(a,b) = 0  " + d, d.isZERO() || !d.isConstant() );
+         } else {
+            assertTrue("res(a,b) != 0 " + d, !d.isZERO() );
+         }
+
+     }
+ }
 
 }
