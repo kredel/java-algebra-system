7c8ef436b53f3e7a48ae450e6a0e0602eea832dd
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-04-15 12:19:38 +0000

    class to use multiple CPUs for gcd calculation
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1031 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ ufd/GCDProxy.java
@@ -0,0 +1,264 @@
+
+/*
+ * $Id$
+ */
+
+package edu.jas.ufd;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.Callable;
+
+import org.apache.log4j.Logger;
+
+import edu.jas.structure.RingElem;
+import edu.jas.structure.GcdRingElem;
+import edu.jas.structure.RingFactory;
+
+import edu.jas.arith.ModInteger;
+import edu.jas.arith.BigInteger;
+import edu.jas.arith.BigRational;
+
+import edu.jas.poly.GenPolynomial;
+
+import edu.jas.ufd.GreatestCommonDivisor;
+import edu.jas.ufd.GreatestCommonDivisorAbstract;
+import edu.jas.ufd.GreatestCommonDivisorSubres;
+import edu.jas.ufd.GreatestCommonDivisorPrimitive;
+import edu.jas.ufd.GreatestCommonDivisorModular;
+import edu.jas.ufd.GreatestCommonDivisorModEval;
+
+
+/**
+ * Greatest common divisor parallel proxy.
+ * @author Heinz Kredel
+ */
+
+public class GCDProxy<C extends GcdRingElem<C>>
+             implements GreatestCommonDivisor<C> {
+
+    private static final Logger logger = Logger.getLogger(GCDProxy.class);
+
+
+    /**
+      * GCD engines.
+      */
+
+    public final GreatestCommonDivisor<C> e1;
+
+    public final GreatestCommonDivisor<C> e2;
+
+
+    /**
+      * Thread pool.
+      */
+
+    protected ExecutorService pool; 
+
+
+    /**
+      * Thread pool size.
+      */
+
+    protected static final int anzahl = 2;
+
+
+    /**
+      * Thread poll intervall.
+      */
+
+    protected static final int dauer = 5;
+
+
+
+    /**
+     * Proxy constructor.
+     */
+     public GCDProxy(GreatestCommonDivisor<C> e1, GreatestCommonDivisor<C> e2 ) {
+         this.e1 = e1; 
+         this.e2 = e2; 
+         if ( pool == null ) {
+            pool = Executors.newFixedThreadPool(anzahl);
+         }
+     }                                              
+
+
+    /**
+     * Terminate proxy.
+     */
+     public void terminate() {
+         List<Runnable> r = pool.shutdownNow();
+         if ( r.size() != 0 ) {
+            // throw new RuntimeException("there are unfinished tasks " + r);
+            // System.out.println("there are " + r.size() + " unfinished tasks ");
+            logger.info("there are " + r.size() + " unfinished tasks ");
+         }
+     }
+
+
+    /**
+     * GenPolynomial content.
+     * Entry driver method.
+     * @param P GenPolynomial.
+     * @return cont(P).
+     */
+    public GenPolynomial<C> content( GenPolynomial<C> P ) {
+           throw new RuntimeException("content not implemented");
+    }
+
+
+    /**
+     * GenPolynomial primitive part.
+     * Entry driver method.
+     * @param P GenPolynomial.
+     * @return pp(P).
+     */
+    public GenPolynomial<C> primitivePart( GenPolynomial<C> P ) {
+           throw new RuntimeException("content not implemented");
+    }
+
+
+    /**
+     * GenPolynomial greatest comon divisor.
+     * Main entry driver method.
+     * @param P GenPolynomial.
+     * @param S GenPolynomial.
+     * @return gcd(P,S).
+     */
+     public GenPolynomial<C> gcd( final GenPolynomial<C> P, final GenPolynomial<C> S ) {
+         GenPolynomial<C> g = null;
+
+         Callable<GenPolynomial<C>> c0;
+         Callable<GenPolynomial<C>> c1;
+         List<Callable<GenPolynomial<C>>> cs = new ArrayList<Callable<GenPolynomial<C>>>(2);
+
+         cs.add( new Callable<GenPolynomial<C>>() {
+                     public GenPolynomial<C> call() {
+                         GenPolynomial<C> g = e1.gcd(P,S);
+                         System.out.println("GCDProxy done e1 " + e1);
+                         return g;
+                     }
+                 }
+                 );
+         cs.add( new Callable<GenPolynomial<C>>() {
+                     public GenPolynomial<C> call() {
+                         GenPolynomial<C> g = e2.gcd(P,S);
+                         System.out.println("GCDProxy done e2 " + e2);
+                         return g;
+                     }
+                 }
+                 );
+         try {
+             g = pool.invokeAny( cs );
+         } catch (InterruptedException ignored) { 
+         } catch (ExecutionException ignored) { 
+         }
+         return g;
+     }
+
+
+    /**
+     * GenPolynomial greatest comon divisor.
+     * Main entry driver method.
+     * @param P GenPolynomial.
+     * @param S GenPolynomial.
+     * @return gcd(P,S).
+     */
+     public GenPolynomial<C> gcd2( final GenPolynomial<C> P, final GenPolynomial<C> S ) {
+         GenPolynomial<C> g = null;
+
+         Future<GenPolynomial<C>> f0;
+         Future<GenPolynomial<C>> f1;
+         f0 = pool.submit( new Callable<GenPolynomial<C>>() {
+                               public GenPolynomial<C> call() {
+                                     return e1.gcd(P,S);
+                               }
+                           }
+                         );
+         f1 = pool.submit( new Callable<GenPolynomial<C>>() {
+                               public GenPolynomial<C> call() {
+                                   return e2.gcd(P,S);
+                               }
+                           }
+                         );
+
+         while ( g == null ) {
+             try {
+                 if ( g == null && f0.isDone() /*&& ! f0.isCancelled()*/ ) {
+                    g = f0.get(); 
+                    f1.cancel(true);
+                    System.out.println("GCDProxy done e1 " + e1);
+                 }
+                 if ( g == null && f1.isDone() /*&& ! f1.isCancelled()*/ ) {
+                    g = f1.get(); 
+                    f0.cancel(true);
+                    System.out.println("GCDProxy done e2 " + e2);
+                 }
+                 if ( g == null ) {
+                    Thread.currentThread().sleep(dauer);
+                 }
+             } catch (InterruptedException ignored) { 
+             } catch (CancellationException ignored) { 
+             } catch (ExecutionException ignored) { 
+             }
+         }
+         if ( !f0.isDone() || !f1.isDone() ) {
+            System.out.println("GCDProxy not done, f0 = " + f0 + ", f1 = " + f1);
+         }
+         return g;
+     }
+
+
+    /**
+     * GenPolynomial least comon multiple.
+     * Main entry driver method.
+     * @param P GenPolynomial.
+     * @param S GenPolynomial.
+     * @return lcm(P,S).
+     */
+    public GenPolynomial<C> lcm( GenPolynomial<C> P,GenPolynomial<C> S ) {
+           throw new RuntimeException("content not implemented");
+    }
+
+
+    /**
+     * GenPolynomial resultant.
+     * Main entry driver method.
+     * @param P GenPolynomial.
+     * @param S GenPolynomial.
+     * @return res(P,S).
+     */
+    public GenPolynomial<C> resultant( GenPolynomial<C> P, GenPolynomial<C> S ) {
+           throw new RuntimeException("content not implemented");
+    }
+
+
+    /**
+     * GenPolynomial greatest squarefree divisor.
+     * Entry driver method.
+     * @param P GenPolynomial.
+     * @return squarefree(P).
+     */
+    public GenPolynomial<C> squarefreePart( GenPolynomial<C> P ) {
+           throw new RuntimeException("content not implemented");
+    }
+
+
+    /**
+     * GenPolynomial squarefree factorization.
+     * Entry driver method.
+     * @param P GenPolynomial.
+     * @return squarefreeFactors(P).
+     */
+    public Map<Integer,GenPolynomial<C>> squarefreeFactors( GenPolynomial<C> P ) {
+           throw new RuntimeException("content not implemented");
+    }
+
+
+}
---
+++ ufd/GCDProxyTest.java
@@ -0,0 +1,320 @@
+/*
+ * $Id$
+ */
+
+package edu.jas.ufd;
+
+import java.util.Map;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import edu.jas.arith.BigInteger;
+import edu.jas.arith.BigRational;
+import edu.jas.arith.ModInteger;
+import edu.jas.arith.BigComplex;
+
+import edu.jas.poly.AlgebraicNumber;
+import edu.jas.poly.AlgebraicNumberRing;
+import edu.jas.poly.ExpVector;
+import edu.jas.poly.TermOrder;
+import edu.jas.poly.GenPolynomial;
+import edu.jas.poly.GenPolynomialRing;
+import static edu.jas.poly.PolyUtil.*;
+
+
+/**
+ * GreatestCommonDivisor Test using JUnit.
+ * @author Heinz Kredel.
+ */
+
+public class GCDProxyTest extends TestCase {
+
+/**
+ * main.
+ */
+   public static void main (String[] args) {
+          junit.textui.TestRunner.run( suite() );
+          System.exit(0);
+   }
+
+/**
+ * Constructs a <CODE>GCDProxyTest</CODE> object.
+ * @param name String.
+ */
+   public GCDProxyTest(String name) {
+          super(name);
+   }
+
+/**
+ */ 
+ public static Test suite() {
+     TestSuite suite= new TestSuite(GCDProxyTest.class);
+     return suite;
+   }
+
+   //private final static int bitlen = 100;
+
+   TermOrder to = new TermOrder( TermOrder.INVLEX );
+
+   GenPolynomialRing<BigInteger> dfac;
+   GenPolynomialRing<BigInteger> cfac;
+   GenPolynomialRing<GenPolynomial<BigInteger>> rfac;
+
+   BigInteger ai;
+   BigInteger bi;
+   BigInteger ci;
+   BigInteger di;
+   BigInteger ei;
+
+   GenPolynomial<BigInteger> a;
+   GenPolynomial<BigInteger> b;
+   GenPolynomial<BigInteger> c;
+   GenPolynomial<BigInteger> d;
+   GenPolynomial<BigInteger> e;
+
+   GenPolynomial<GenPolynomial<BigInteger>> ar;
+   GenPolynomial<GenPolynomial<BigInteger>> br;
+   GenPolynomial<GenPolynomial<BigInteger>> cr;
+   GenPolynomial<GenPolynomial<BigInteger>> dr;
+   GenPolynomial<GenPolynomial<BigInteger>> er;
+
+   int rl = 5; 
+   int kl = 4;
+   int ll = 5;
+   int el = 3;
+   float q = 0.3f;
+
+   protected void setUp() {
+       a = b = c = d = e = null;
+       ai = bi = ci = di = ei = null;
+       ar = br = cr = dr = er = null;
+       dfac = new GenPolynomialRing<BigInteger>(new BigInteger(1),rl,to);
+       cfac = new GenPolynomialRing<BigInteger>(new BigInteger(1),rl-1,to);
+       rfac = new GenPolynomialRing<GenPolynomial<BigInteger>>(cfac,1,to);
+   }
+
+   protected void tearDown() {
+       a = b = c = d = e = null;
+       ai = bi = ci = di = ei = null;
+       ar = br = cr = dr = er = null;
+       dfac = null;
+       cfac = null;
+       rfac = null;
+   }
+
+
+/**
+ * Test get BigInteger implementation.
+ * 
+ */
+ public void xtestBigInteger() {
+     BigInteger bi = new BigInteger();
+
+     GreatestCommonDivisor<BigInteger> ufd_par; 
+     GreatestCommonDivisorAbstract<BigInteger> ufd; 
+
+     ufd_par = GCDFactory.<BigInteger>getProxy(bi);
+     System.out.println("ufd_par = " + ufd_par);
+     assertTrue("ufd_par != null " + ufd_par, ufd_par != null);
+
+     ufd = new GreatestCommonDivisorSubres<BigInteger>(); 
+
+     System.out.println("ufd = " + ufd);
+     assertTrue("ufd != null " + ufd, ufd != null);
+
+     dfac = new GenPolynomialRing<BigInteger>(bi,4,to);
+
+     for (int i = 0; i < 5; i++) {
+         a = dfac.random(kl+i*2,ll+i,2*el,q);
+         b = dfac.random(kl+i*2,ll+i,2*el,q);
+         c = dfac.random(kl,ll,el,q);
+         //c = dfac.getONE();
+         //c = c.multiply( dfac.univariate(0) );
+         c = ufd.primitivePart(c).abs();
+         //System.out.println("a = " + a);
+         //System.out.println("b = " + b);
+         //System.out.println("c = " + c);
+
+         if ( a.isZERO() || b.isZERO() || c.isZERO() ) {
+            // skip for this turn
+            continue;
+         }
+         assertTrue("length( c"+i+" ) <> 0", c.length() > 0);
+         //assertTrue(" not isZERO( c"+i+" )", !c.isZERO() );
+         //assertTrue(" not isONE( c"+i+" )", !c.isONE() );
+         
+         a = a.multiply(c);
+         b = b.multiply(c);
+         //System.out.println("a = " + a);
+         //System.out.println("b = " + b);
+
+         d = ufd_par.gcd(a,b);
+         //System.out.println("c = " + c);
+         //System.out.println("d = " + d);
+         //System.out.println("e = " + e);
+
+         e = ufd.basePseudoRemainder(d,c);
+         //System.out.println("e = " + e);
+         assertTrue("c | gcd(ac,bc) " + e, e.isZERO() );
+     }
+
+     ((GCDProxy<BigInteger>)ufd_par).terminate();
+ }
+
+
+/**
+ * Test get ModInteger implementation.
+ * 
+ */ 
+ public void testModInteger() {
+     // ModInteger mi = new ModInteger(19,0,true);
+     ModInteger mi = new ModInteger(536870909,0,true);
+
+     GenPolynomial<ModInteger> a, b, c, d, e;
+
+     GreatestCommonDivisor<ModInteger> ufd_par; 
+     GreatestCommonDivisorAbstract<ModInteger> ufd; 
+
+     ufd_par = GCDFactory.<ModInteger>getProxy(mi);
+     System.out.println("ufd_par = " + ufd_par);
+     assertTrue("ufd_par != null " + ufd_par, ufd_par != null);
+
+     ufd = new GreatestCommonDivisorSubres<ModInteger>(); 
+
+     System.out.println("ufd = " + ufd);
+     assertTrue("ufd != null " + ufd, ufd != null);
+
+     GenPolynomialRing<ModInteger> dfac;
+     dfac = new GenPolynomialRing<ModInteger>(mi,4,to);
+
+     for (int i = 0; i < 4; i++) {
+         a = dfac.random(kl+i*2,ll+i,2*el,q);
+         b = dfac.random(kl+i*2,ll+i,2*el,q);
+         c = dfac.random(kl,ll,el,q);
+         //a = dfac.random(kl,ll+i,el,q);
+         //b = dfac.random(kl,ll+i,el,q);
+         //c = dfac.random(kl,ll,el,q);
+         //c = dfac.getONE();
+         //c = c.multiply( dfac.univariate(0) );
+         c = ufd.primitivePart(c).abs();
+         //System.out.println("a = " + a);
+         //System.out.println("b = " + b);
+         //System.out.println("c = " + c);
+
+         if ( a.isZERO() || b.isZERO() || c.isZERO() ) {
+            // skip for this turn
+            continue;
+         }
+         assertTrue("length( c"+i+" ) <> 0", c.length() > 0);
+         //assertTrue(" not isZERO( c"+i+" )", !c.isZERO() );
+         //assertTrue(" not isONE( c"+i+" )", !c.isONE() );
+         
+         a = a.multiply(c);
+         b = b.multiply(c);
+         //System.out.println("a = " + a);
+         //System.out.println("b = " + b);
+
+         d = ufd_par.gcd(a,b);
+         //System.out.println("c = " + c);
+         //System.out.println("d = " + d);
+         //System.out.println("e = " + e);
+
+         e = ufd.basePseudoRemainder(d,c);
+         //System.out.println("e = " + e);
+         assertTrue("c | gcd(ac,bc) " + e, e.isZERO() );
+     }
+
+     ((GCDProxy<ModInteger>)ufd_par).terminate();
+ }
+
+
+/**
+ * Test get BigRational implementation.
+ * 
+ */
+ public void xtestBigRational() {
+     BigRational b = new BigRational();
+     GreatestCommonDivisor<BigRational> ufd; 
+
+     ufd = GCDFactory.<BigRational>getImplementation(b);
+     //System.out.println("ufd = " + ufd);
+     assertTrue("ufd = Primitive " + ufd, ufd instanceof GreatestCommonDivisorPrimitive);
+ }
+
+
+/**
+ * Test get BigComplex implementation.
+ * 
+ public void testBigComplex() {
+     BigComplex b = new BigComplex();
+     GreatestCommonDivisor<BigComplex> ufd; 
+
+     ufd = GCDFactory.<BigComplex>getImplementation(b);
+     //System.out.println("ufd = " + ufd);
+     assertTrue("ufd != Simple " + ufd, ufd instanceof GreatestCommonDivisorSimple);
+ }
+ */
+
+
+/**
+ * Test get AlgebraicNumber<BigRational> implementation.
+ * 
+ */
+ public void xtestAlgebraicNumberBigRational() {
+     BigRational b = new BigRational();
+     GenPolynomialRing<BigRational> fac;
+     fac = new GenPolynomialRing<BigRational>( b, 1 );
+     GenPolynomial<BigRational> mo = fac.random(kl,ll,el,q);
+     while ( mo.isConstant() ) {
+          mo = fac.random(kl,ll,el,q);
+     }
+
+     AlgebraicNumberRing<BigRational> afac;
+     afac = new AlgebraicNumberRing<BigRational>( mo );
+
+     GreatestCommonDivisor<AlgebraicNumber<BigRational>> ufd; 
+
+     ufd = GCDFactory.<AlgebraicNumber<BigRational>>getImplementation(afac);
+     //System.out.println("ufd = " + ufd);
+     assertTrue("ufd = Subres " + ufd, ufd instanceof GreatestCommonDivisorSubres);
+
+
+     mo = fac.univariate(0).subtract( fac.getONE() );
+     afac = new AlgebraicNumberRing<BigRational>( mo, true );
+
+     ufd = GCDFactory.<AlgebraicNumber<BigRational>>getImplementation(afac);
+     //System.out.println("ufd = " + ufd);
+     assertTrue("ufd = Simple " + ufd, ufd instanceof GreatestCommonDivisorSimple);
+
+ }
+
+
+/**
+ * Test get AlgebraicNumber<ModInteger> implementation.
+ * 
+ */
+ public void xtestAlgebraicNumberModInteger() {
+     ModInteger b = new ModInteger(19,0,true);
+     GenPolynomialRing<ModInteger> fac;
+     fac = new GenPolynomialRing<ModInteger>( b, 1 );
+     GenPolynomial<ModInteger> mo = fac.random(kl,ll,el,q);
+     while ( mo.isConstant() ) {
+          mo = fac.random(kl,ll,el,q);
+     }
+
+     AlgebraicNumberRing<ModInteger> afac;
+     afac = new AlgebraicNumberRing<ModInteger>( mo );
+
+
+     AlgebraicNumber<ModInteger> a = afac.getONE();
+     GreatestCommonDivisor<AlgebraicNumber<ModInteger>> ufd; 
+
+     ufd = GCDFactory.<AlgebraicNumber<ModInteger>>getImplementation(afac);
+     //System.out.println("ufd = " + ufd);
+     assertTrue("ufd = Subres " + ufd, ufd instanceof GreatestCommonDivisorSubres);
+ }
+
+
+}
