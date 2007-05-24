fb91c7421c74a5892e0c063110562464b5b2504c
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-05-24 20:56:35 +0000

    new examples
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1094 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ examples/raksanyi.py
@@ -0,0 +1,35 @@
+#
+# jython examples for jas.
+# $Id$
+#
+
+import sys;
+
+from jas import Ring
+from jas import Ideal
+
+# Raksanyi & Walter example
+# rational function coefficients
+
+r = Ring( "RatFunc(a1, a2, a3, a4) (x1, x2, x3, x4) L" );
+print "Ring: " + str(r);
+print;
+
+ps = """
+(
+ ( x4 - { a4 - a2 } ),
+ ( x1 + x2 + x3 + x4 - { a1 + a3 + a4 } ),
+ ( x1 x3 + x1 x4 + x2 x3 + x3 x4 - { a1 a4 + a1 a3 + a3 a4 } ),
+ ( x1 x3 x4 - { a1 a3 a4 } )
+) 
+""";
+
+f = r.ideal( ps );
+print "Ideal: " + str(f);
+print;
+
+rg = f.GB();
+print "GB:", rg;
+print;
+
+
