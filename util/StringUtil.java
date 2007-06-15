3935a49e662fd3eed27b58d257e8ddf875904a4f
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-06-15 19:05:56 +0000

    new nextString with delimiter
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1174 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- util/StringUtil.java
+++ util/StringUtil.java
@@ -48,4 +48,29 @@ public class StringUtil {
         return sw.toString();
     }
 
+
+    /** Parse String with given delimiter from Reader.
+     * @param c delimiter.
+     * @param r Reader.
+     * @return next String up to c from r.
+     */
+    public static String nextString(Reader r, char c) {
+        StringWriter sw = new StringWriter();
+        try {
+            char buffer;
+            int i;
+            // read chars != c, ignore new lines ?
+            while ( ( i = r.read() ) > -1 ) {
+                buffer = (char) i;
+                if ( buffer == c ) {
+                   break;
+                }
+                sw.write(buffer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sw.toString();
+    }
+
 }
