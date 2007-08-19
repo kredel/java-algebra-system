a548ed6ef7a13a74b460fbd8f09e64adbc632ca1
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-08-19 14:21:07 +0000

    added timing
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1347 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- jython_tests.sh
+++ jython_tests.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 # run most py files
 
-jython examples/armbruster.py
-jython examples/intprog.py
-jython examples/intprog2.py
-jython examples/intprog3.py
-jython examples/intprog4.py
-jython examples/intprog4a.py
-jython examples/katsura.py
-jython examples/preimage.py
-jython examples/quantumplane.py
-jython examples/rose.py
-jython examples/solvablemodule.py
-jython examples/solvmodright.py
-jython examples/syz.py
-jython examples/syzsolv.py
-jython examples/syzy2.py
-jython examples/syzy3.py
-jython examples/trinks.py
-jython examples/u_2_wa_1.py
-jython examples/u_sl_2.py
-jython examples/u_sl_2_e.py
-jython examples/u_sl_2_f.py
-jython examples/u_sl_2_wa_1.py
-jython examples/u_sl_3.py
-jython examples/u_sl_3_prod.py
-jython examples/u_so_3.py
-jython examples/wa_1.py
-jython examples/wa_32.py
-jython examples/wa_32_syz.py
+time jython examples/armbruster.py
+time jython examples/intprog.py
+time jython examples/intprog2.py
+time jython examples/intprog3.py
+time jython examples/intprog4.py
+time jython examples/intprog4a.py
+time jython examples/katsura.py
+time jython examples/preimage.py
+time jython examples/quantumplane.py
+time jython examples/rose.py
+time jython examples/solvablemodule.py
+time jython examples/solvmodright.py
+time jython examples/syz.py
+time jython examples/syzsolv.py
+time jython examples/syzy2.py
+time jython examples/syzy3.py
+time jython examples/trinks.py
+time jython examples/u_2_wa_1.py
+time jython examples/u_sl_2.py
+time jython examples/u_sl_2_e.py
+time jython examples/u_sl_2_f.py
+time jython examples/u_sl_2_wa_1.py
+time jython examples/u_sl_3.py
+time jython examples/u_sl_3_prod.py
+time jython examples/u_so_3.py
+time jython examples/wa_1.py
+time jython examples/wa_32.py
+time jython examples/wa_32_syz.py
