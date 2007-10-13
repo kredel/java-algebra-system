e985e1955742fec71b048fd3f6ae6d6c9328ab63
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-10-13 14:02:07 +0000

    new polynomials
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1430 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- examples/mark_kr.py
+++ examples/mark_kr.py
@@ -9,17 +9,17 @@ from jas import Ring
 from jas import Ideal
 from jas import startLog
 
-# mark, d-gb diplom example
+# mark, d-gb diplom example, due to kandri-rody 1984
 
-r = Ring( "Z(x,y,z) L" );
+r = Ring( "Z(x,y) L" );
 print "Ring: " + str(r);
 print;
 
 ps = """
 ( 
- ( z + x y**2 + 4 x**2 + 1 ),
- ( y**2 z + 2 x + 1 ),
- ( x**2 z + y**2 + x )
+ ( y**6 + x**4 y**4 - x**2 y**4 - y**4 - x**4 y**2 + 2 x**2 y**2 + x**6 - x**4 ),
+ ( 2 x**3 y**4 - x y**4 - 2 x**3 y**2 + 2 x y**2 + 3 x**5 - 2 x** 3 ),
+ ( 3 y**5 + 2 x**4 y**3 - 2 x**2 y**3 - 2 y**3 - x**4 y + 2 x**2 y )
 ) 
 """;
 
@@ -40,6 +40,9 @@ rg = r.ideal(list=eg);
 print "seq e-GB:", rg;
 print "is e-GB:", egbs.isGB(eg);
 print;
+
+
+startLog();
 
 dg = dgbs.GB( f.list );
 rg = r.ideal(list=dg);
