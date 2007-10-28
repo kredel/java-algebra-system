d39124c7c7fc194e0955674330e019bb89f4c56b
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-10-28 11:26:56 +0000

    added prints
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1471 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/ring/RGroebnerBaseSeqTest.java
+++ src/edu/jas/ring/RGroebnerBaseSeqTest.java
@@ -123,6 +123,7 @@ public class RGroebnerBaseSeqTest extends TestCase {
 
      L = bb.GB( L );
      assertTrue("isGB( { a } )", bb.isGB(L) );
+     System.out.println("L = " + L );
 
      assertTrue("not isZERO( b )", !b.isZERO() );
      L.add(b);
@@ -130,61 +131,29 @@ public class RGroebnerBaseSeqTest extends TestCase {
 
      L = bb.GB( L );
      assertTrue("isGB( { a, b } )", bb.isGB(L) );
+     System.out.println("L = " + L );
 
      assertTrue("not isZERO( c )", !c.isZERO() );
      L.add(c);
 
      L = bb.GB( L );
      assertTrue("isGB( { a, ,b, c } )", bb.isGB(L) );
+     System.out.println("L = " + L );
 
      assertTrue("not isZERO( d )", !d.isZERO() );
      L.add(d);
 
      L = bb.GB( L );
      assertTrue("isGB( { a, ,b, c, d } )", bb.isGB(L) );
+     System.out.println("L = " + L );
 
      assertTrue("not isZERO( e )", !e.isZERO() );
      L.add(e);
 
      L = bb.GB( L );
      assertTrue("isGB( { a, ,b, c, d, e } )", bb.isGB(L) );
+     System.out.println("L = " + L );
  }
 
-
-/**
- * Test Trinks7 GBase.
- * 
- @SuppressWarnings("unchecked")
- public void testTrinks7GBase() {
-     String exam = "(B,S,T,Z,P,W) L "
-                 + "( "  
-                 + "( 45 P + 35 S - 165 B - 36 ), " 
-                 + "( 35 P + 40 Z + 25 T - 27 S ), "
-                 + "( 15 W + 25 S P + 30 Z - 18 T - 165 B**2 ), "
-                 + "( - 9 W + 15 T P + 20 S Z ), "
-                 + "( P W + 2 T Z - 11 B**3 ), "
-                 + "( 99 W - 11 B S + 3 B**2 ), "
-                 + "( B**2 + 33/50 B + 2673/10000 ) "
-                 + ") ";
-     Reader source = new StringReader( exam );
-     GenPolynomialTokenizer parser
-                  = new GenPolynomialTokenizer( source );
-     try {
-         F = (PolynomialList<BigRational>) parser.nextPolynomialSet();
-     } catch(ClassCastException e) {
-         fail(""+e);
-     } catch(IOException e) {
-         fail(""+e);
-     }
-     //System.out.println("F = " + F);
-
-     G = bb.GB(F.list);
-     assertTrue("isGB( GB(Trinks7) )", bb.isGB(G) );
-     assertEquals("#GB(Trinks7) == 6", 6, G.size() );
-     PolynomialList<BigRational> trinks 
-           = new PolynomialList<BigRational>(F.ring,G);
-     //System.out.println("G = " + trinks);
- }
- */
 
 }
