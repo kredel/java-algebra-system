43268fe72428ced68048b6e4374b314e53ee1b4a
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2007-12-29 18:32:57 +0000

    new fraction free reduction
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1550 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ src/edu/jas/ring/RPseudoReductionSeq.java
@@ -0,0 +1,278 @@
+/*
+ * $Id$
+ */
+
+package edu.jas.ring;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import edu.jas.poly.ExpVector;
+import edu.jas.poly.GenPolynomial;
+import edu.jas.poly.GenSolvablePolynomial;
+
+import edu.jas.structure.RingElem;
+import edu.jas.structure.RegularRingElem;
+
+
+/**
+ * Polynomial regular ring pseudo reduction sequential use algorithm.
+ * Implements fraction free normalform algorithm.
+ * @author Heinz Kredel
+ */
+
+public class RPseudoReductionSeq<C extends RegularRingElem<C>>
+             extends RReductionSeq<C> 
+             /*implements RReduction<C>*/ {
+
+    private static final Logger logger = Logger.getLogger(RPseudoReductionSeq.class);
+    private final boolean debug = logger.isDebugEnabled();
+
+
+    /**
+     * Constructor.
+     */
+    public RPseudoReductionSeq() {
+    }
+
+
+    /**
+     * Normalform using r-reduction.
+     * @typeparam C coefficient type.
+     * @param Ap polynomial.
+     * @param Pp polynomial list.
+     * @return r-nf(Ap) with respect to Pp.
+     */
+    @SuppressWarnings("unchecked") 
+    public GenPolynomial<C> normalform(List<GenPolynomial<C>> Pp, 
+                                       GenPolynomial<C> Ap) {  
+        if ( Pp == null || Pp.isEmpty() ) {
+           return Ap;
+        }
+        if ( Ap == null || Ap.isZERO() ) {
+           return Ap;
+        }
+        int l;
+        GenPolynomial<C>[] P;
+        synchronized (Pp) {
+            l = Pp.size();
+            P = (GenPolynomial<C>[])new GenPolynomial[l];
+            //P = Pp.toArray();
+            for ( int i = 0; i < Pp.size(); i++ ) {
+                P[i] = Pp.get(i).abs();
+            }
+        }
+        //System.out.println("l = " + l);
+        Map.Entry<ExpVector,C> m;
+        ExpVector[] htl = new ExpVector[ l ];
+        C[] lbc = (C[]) new RegularRingElem[ l ]; // want <C>
+        GenPolynomial<C>[] p = (GenPolynomial<C>[])new GenPolynomial[ l ];
+        int i;
+        int j = 0;
+        for ( i = 0; i < l; i++ ) { 
+            p[i] = P[i];
+            m = p[i].leadingMonomial();
+            if ( m != null ) { 
+               p[j] = p[i];
+               htl[j] = m.getKey();
+               lbc[j] = m.getValue();
+               j++;
+            }
+        }
+        l = j;
+        ExpVector e, f;
+        C a, b;
+        C r = null;
+        boolean mt = false;
+        GenPolynomial<C> R = Ap.ring.getZERO();
+        GenPolynomial<C> Q = null;
+        GenPolynomial<C> S = Ap;
+        while ( S.length() > 0 ) { 
+              m = S.leadingMonomial();
+              e = m.getKey();
+              a = m.getValue();
+              for ( i = 0; i < l; i++ ) {
+                  mt = ExpVector.EVMT( e, htl[i] );
+                  if ( mt ) {
+                     C c = lbc[i];
+                     r = a.idempotent().multiply( c.idempotent() );
+                     //System.out.println("r = " + r);
+                     mt = ! r.isZERO(); // && mt
+                     if ( mt ) {
+                        b = a.remainder( c );
+                        if ( b.isZERO() ) {
+                           a = a.divide( c );
+                        } else {
+                           S = S.multiply( c );
+                           R = R.multiply( c );
+                        }
+                        f = ExpVector.EVDIF( e, htl[i] );
+                        //logger.info("red div = " + f);
+                        Q = p[i].multiply( a, f );
+                        S = S.subtract( Q ); // not ok with reductum
+                        f = S.leadingExpVector();
+                        if ( !e.equals(f) ) {
+                           a = Ap.ring.coFac.getZERO();
+                           break;
+                        } 
+                        a = S.leadingBaseCoefficient();
+                     }
+                  }
+              }
+              if ( !a.isZERO() ) { //! mt ) { 
+                 //logger.debug("irred");
+                 R = R.sum( a, e );
+                 S = S.reductum(); 
+              }
+        }
+        return R.abs(); // not monic if not boolean closed
+    }
+
+
+    /**
+     * Normalform with recording.
+     * <b>Note:</b> do not use, multiplication factor is lost. 
+     * Will be removed or replaced in future.
+     * @typeparam C coefficient type.
+     * @param row recording matrix, is modified.
+     * @param Pp a polynomial list for reduction.
+     * @param Ap a polynomial.
+     * @return nf(Pp,Ap), the normal form of Ap wrt. Pp.
+     * @deprecated multiplication factor will be lost.
+     */
+    public GenPolynomial<C> 
+        normalform(List<GenPolynomial<C>> row,
+                   List<GenPolynomial<C>> Pp, 
+                   GenPolynomial<C> Ap) {  
+        List<C> mf = new ArrayList<C>(1);
+        logger.error("multiplication factor is lost");
+        return normalform(mf,row,Pp,Ap);
+    }
+
+
+    /**
+     * Normalform with recording.
+     * @typeparam C coefficient type.
+     * @param row recording matrix, is modified.
+     * @param mf  multiplication factor for Ap, is modified.
+     * @param Pp a polynomial list for reduction.
+     * @param Ap a polynomial.
+     * @return nf(Pp,mf*Ap), the normal form of mf*Ap wrt. Pp.
+     */
+    @SuppressWarnings("unchecked") 
+    public GenPolynomial<C> 
+        normalform(List<C> mf,
+                   List<GenPolynomial<C>> row,
+                   List<GenPolynomial<C>> Pp, 
+                   GenPolynomial<C> Ap) {  
+        if ( Pp == null || Pp.isEmpty() ) {
+            return Ap;
+        }
+        if ( Ap == null || Ap.isZERO() ) {
+            return Ap;
+        }
+        //throw new RuntimeException("not jet implemented");
+        int l;
+        GenPolynomial<C>[] P;
+        synchronized (Pp) {
+            l = Pp.size();
+            P = (GenPolynomial<C>[])new GenPolynomial[l];
+            //P = Pp.toArray();
+            for ( int i = 0; i < Pp.size(); i++ ) {
+                P[i] = Pp.get(i).abs();
+            }
+        }
+        //System.out.println("l = " + l);
+        Map.Entry<ExpVector,C> m;
+        ExpVector[] htl = new ExpVector[ l ];
+        C[] lbc = (C[]) new RegularRingElem[ l ]; // want <C>
+        GenPolynomial<C>[] p = (GenPolynomial<C>[])new GenPolynomial[ l ];
+        int i;
+        int j = 0;
+        for ( i = 0; i < l; i++ ) { 
+            p[i] = P[i];
+            m = p[i].leadingMonomial();
+            if ( m != null ) { 
+               p[j] = p[i];
+               htl[j] = m.getKey();
+               lbc[j] = m.getValue();
+               j++;
+            }
+        }
+        l = j;
+        ExpVector e, f;
+        C a, b;
+        C r = null;
+        boolean mt = false;
+        C mfac = mf.get(0);
+        if ( mfac == null ) {
+           mfac = Ap.ring.getONECoefficient();
+        }
+        GenPolynomial<C> fac = null;
+        GenPolynomial<C> zero = Ap.ring.getZERO();
+        GenPolynomial<C> R = Ap.ring.getZERO();
+        GenPolynomial<C> Q = null;
+        GenPolynomial<C> S = Ap;
+        while ( S.length() > 0 ) { 
+              m = S.leadingMonomial();
+              e = m.getKey();
+              a = m.getValue();
+              for ( i = 0; i < l; i++ ) {
+                  mt = ExpVector.EVMT( e, htl[i] );
+                  if ( mt ) {
+                     C c = lbc[i];
+                     r = a.idempotent().multiply( c.idempotent() );
+                     //System.out.println("r = " + r);
+                     mt = ! r.isZERO(); // && mt
+                     if ( mt ) {
+                        b = a.remainder( c );
+                        if ( b.isZERO() ) {
+                           a = a.divide( c );
+                        } else {
+                           mfac = mfac.multiply( c );
+                           S = S.multiply( c );
+                           R = R.multiply( c );
+                        }
+                        f = ExpVector.EVDIF( e, htl[i] );
+                        //logger.info("red div = " + f);
+                        Q = p[i].multiply( a, f );
+                        S = S.subtract( Q ); // not ok with reductum
+
+                        fac = row.get(i);
+                        if ( fac == null ) {
+                            fac = zero.sum( a, f );
+                        } else {
+                            fac = fac.sum( a, f );
+                        }
+                        row.set(i,fac);
+
+                        f = S.leadingExpVector();
+                        if ( !e.equals(f) ) {
+                           a = Ap.ring.coFac.getZERO();
+                           break;
+                        } 
+                        a = S.leadingBaseCoefficient();
+                     }
+                  }
+              }
+              if ( !a.isZERO() ) { //! mt ) { 
+                 //logger.debug("irred");
+                 R = R.sum( a, e );
+                 S = S.reductum(); 
+              }
+        }
+        mf.set(0,mfac);
+        return R; //.abs(); // not monic if not boolean closed
+    }
+
+
+    /*
+     * -------- boolean closure stuff -----------------------------------------
+     * -------- is all in superclass
+     */
+
+}
