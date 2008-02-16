2531d59694bcfcce778c48d18a3565956d4f117d
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2008-02-16 17:38:35 +0000

    added idempotent and, or, fill methods
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1679 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/structure/RegularRingElem.java
+++ src/edu/jas/structure/RegularRingElem.java
@@ -22,7 +22,7 @@ public interface RegularRingElem<C extends RegularRingElem<C>>
      */
 
 
-    /** Is Product full. 
+    /** Is regular ring element full. 
      * @return If every component occurs, then true is returned, else false.
      * @see edu.jas.structure.RingElem#isONE()
      */
@@ -41,10 +41,36 @@ public interface RegularRingElem<C extends RegularRingElem<C>>
     public C idempotent();
 
 
-    /** Product idempotent complement.  
+    /** Regular ring element idempotent complement.  
      * @return 1-this.idempotent().
      */
     public C idemComplement();
 
+
+    /** Regular ring element idempotent and.  
+     * @param S Product.
+     * @return this.idempotent() and S.idempotent().
+     */
+    public C idempotentAnd(C S);
+
+
+    /** Regular ring element idempotent or.  
+     * @param S Product.
+     * @return this.idempotent() or S.idempotent().
+     */
+    public C idempotentOr(C S);
+
+
+    /** Regular ring element fill with idempotent.  
+     * @param S Product.
+     * @return fill this with S.idempotent().
+     */
+    public C fillIdempotent(C S);
+
+
+    /** Regular ring element fill with one.  
+     * @return fill this with one.
+     */
+    public C fillOne();
 
 }
