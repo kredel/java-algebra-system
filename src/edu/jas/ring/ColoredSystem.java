a16892e2d534e2e79f13547ea75c8c622f9c4681
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2008-05-22 13:57:09 +0000

    new checks and re determination
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1812 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/ring/ColoredSystem.java
+++ src/edu/jas/ring/ColoredSystem.java
@@ -6,12 +6,16 @@ package edu.jas.ring;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 import edu.jas.structure.RingElem;
 import edu.jas.structure.GcdRingElem;
 
+import edu.jas.poly.Monomial;
 import edu.jas.poly.ColorPolynomial;
 import edu.jas.poly.GenPolynomial;
+import edu.jas.poly.PolyIterator;
 import edu.jas.poly.GenPolynomialRing;
 
 import edu.jas.application.Ideal;
@@ -68,27 +72,141 @@ public class ColoredSystem<C extends GcdRingElem<C>> {
             List<GenPolynomial<C>> r = c.getConditionNonZero();
             N.addAll( r );
         }
-        return N;
+        List<GenPolynomial<C>> M = new ArrayList<GenPolynomial<C>>();
+        for ( GenPolynomial<C> c : N ) {
+            GenPolynomial<C> m = c.monic();
+            if ( m.isONE() ) {
+               continue;
+            }
+            if ( !M.contains( m ) ) {
+               M.add( m );
+            }
+        }
+        return M;
     }
 
 
     /**
-     * Check ordering invariants.
-     * TT(green) > LT(red) and TT(red) > LT(green).
-     * return true, if all ordering invariants are met, else false.
+     * Get list of full polynomials. 
+     * @return list of all full polynomials.
+     */
+    public List<GenPolynomial<GenPolynomial<C>>> getPolynomialList() {
+        List<GenPolynomial<GenPolynomial<C>>> F
+            = new ArrayList<GenPolynomial<GenPolynomial<C>>>();
+        for ( ColorPolynomial<C> s : S ) {
+            F.add( s.getPolynomial() );
+        }
+        return F;
+    }
+
+
+    /**
+     * Check invariants.
+     * return true, if all invariants are met, else false.
      */
     public boolean checkInvariant() {
+        if ( !isDetermined() ) {
+           return false;
+        }
+        Ideal<C> id = conditions;
+        for ( ColorPolynomial<C> s : S ) {
+            if ( !s.checkInvariant() ) {
+               System.out.println("notInvariant " + s);
+               System.out.println("Ideal: " + id);
+               return false;
+            }
+            for ( GenPolynomial<C> g : s.green.getMap().values() ) {
+                if ( ! id.contains( g ) ) {
+                   System.out.println("notGreen " + g);
+                   System.out.println("Ideal: " + id);
+                   System.out.println("colors: " + s);
+                   return false;
+                }
+            }
+            for ( GenPolynomial<C> r : s.red.getMap().values() ) {
+                if ( id.contains( r ) ) {
+                   System.out.println("notRed " + r);
+                   System.out.println("Ideal: " + id);
+                   System.out.println("colors: " + s);
+                   return false;
+                }
+            }
+        }
         return true;
     }
 
 
     /**
-     * Is this polynomial determined.
-     * return true, if there are nonzero red terms, else false.
+     * Is this colored system completely determined.
+     * return true, if each ColorPolynomial is determined, else false.
      */
     public boolean isDetermined() {
+        for ( ColorPolynomial<C> s : S ) {
+            if ( !s.isDetermined() ) {
+               System.out.println("notDetermined " + s);
+               System.out.println("Ideal: " + conditions);
+               return false;
+            }
+        }
         return true;
     }
 
+
+    /**
+     * Re determine colored polynomial.
+     * @param s colored polynomial.
+     * @return determined colored polynomial wrt. this.conditions.
+     */
+    public ColorPolynomial<C> reDetermine( ColorPolynomial<C> s ) {
+        Ideal<C> id = conditions;
+        if ( id.isONE() ) {
+           return s;
+        }
+        GenPolynomial<GenPolynomial<C>> green = s.green;
+        GenPolynomial<GenPolynomial<C>> red = s.red.clone();
+        GenPolynomial<GenPolynomial<C>> white = s.white;
+        Iterator<Monomial<GenPolynomial<C>>> ri = red.monomialIterator();
+        while ( ri.hasNext() ) {
+            Monomial<GenPolynomial<C>> m = ri.next();
+            if ( !id.contains( m.c ) ) {
+               break;
+            }
+            ri.remove();
+            green = green.sum( m.c, m.e );
+        }
+        if ( !red.isZERO() ) {
+           return new ColorPolynomial<C>( green, red, white );
+        }
+        // now red == 0
+        white = s.white.clone();
+        Iterator<Monomial<GenPolynomial<C>>> wi = white.monomialIterator();
+        while ( wi.hasNext() ) {
+            Monomial<GenPolynomial<C>> m = wi.next();
+            wi.remove();
+            if ( !id.contains( m.c ) ) {
+               red = red.sum( m.c, m.e );
+               break;
+            }
+            green = green.sum( m.c, m.e );
+        }
+        return new ColorPolynomial<C>( green, red, white );
+    }
+
+
+    /**
+     * Re determine colorings of polynomials.
+     * @return re determined colored polynomials wrt. this.conditions.
+     */
+    public ColoredSystem<C> reDetermine() {
+        if ( conditions.isONE() ) {
+           return this;
+        }
+        List<ColorPolynomial<C>> Sn = new ArrayList<ColorPolynomial<C>>( S.size() );
+        for ( ColorPolynomial<C> c : S ) {
+            ColorPolynomial<C> a = reDetermine( c ); 
+            Sn.add( a );
+        }
+        return new ColoredSystem<C>(conditions,Sn);
+    }
 
 }
