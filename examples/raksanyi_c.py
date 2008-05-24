e62f2177ae4c7e38acc7db657f90ffba7c8ca921
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2008-05-24 19:51:15 +0000

    new parameter ideals and CGBs
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1836 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- examples/raksanyi_c.py
+++ examples/raksanyi_c.py
@@ -6,7 +6,7 @@
 import sys;
 
 from jas import Ring
-from jas import Ideal
+from jas import ParamIdeal
 from jas import startLog
 from jas import terminate
 
@@ -28,121 +28,36 @@ ps = """
 ) 
 """;
 
-f = r.ideal( ps );
-print "Ideal: " + str(f);
-print;
-
-from edu.jas.application import PolyUtilApp;
-from edu.jas.poly import PolynomialList;
-from edu.jas.application import ComprehensiveGroebnerBaseSeq;  
-
-startLog();
-
-cofac = r.ring.coFac.coFac;
-#print "cofac:", cofac;
-#print;
-cgb = ComprehensiveGroebnerBaseSeq( cofac );
-#print "cgb:", cgb;
-#print;
-
-cl = cgb.GB( f.list );
-#print "cl:", cl;
-#print;
-c = r.ideal( list=cl );
-print "c:", c;
-print;
-
-bg = cgb.isGB( cl );
-if bg:
-    print "isCGB: true";
-else:
-    print "isCGB: false";
-print;
-
-terminate();
-#------------------------------------------
-sys.exit();
-
-from edu.jas.application import PolyUtilApp;
-from edu.jas.poly import PolynomialList;
-
-pl = PolyUtilApp.productEmptyDecomposition( f.list );
-print;
-print "product decomposition:", pl;
-print;
-
-sl = PolyUtilApp.productSlice( pl );
-#print;
-#print "product slice:", sl;
-#print;
-
-ssl = PolyUtilApp.productSliceToString( sl );
-print;
-print "product slice:", ssl;
+f = r.paramideal( ps );
+print "ParamIdeal: " + str(f);
 print;
 
 #sys.exit();
 
 #startLog();
 
-from edu.jas.ring import RCGroebnerBasePseudoSeq;  
-from edu.jas.application import ComprehensiveGroebnerBaseSeq;  
+## gs = f.CGBsystem();
+## print "CGBsystem: " + str(gs);
+## print;
 
-pr = Ring( ring=pl.ring );
+## bg = gs.isCGBsystem();
+## if bg:
+##     print "isCGBsystem: true";
+## else:
+##     print "isCGBsystem: false";
+## print;
 
-pf = pr.ideal( list=pl.list );
-print;
-print "Ideal of product decomposition: \n" + str(pf);
-print;
-
-cofac = pl.ring.coFac;
-#rgbp = RCGroebnerBasePseudoSeq( cofac );
-cgb = ComprehensiveGroebnerBaseSeq( cofac );
-
-#sys.exit();
-
-#bg = rgbp.isGB(pl.list);
-bg = cgb.isGB(pl.list);
-print "isGB:", bg;
+gs = f.CGB();
+print "CGB: " + str(gs);
 print;
 
-#rg = rgbp.GB(pl.list);
-rg = cgb.GB(pl.list);
-
-pg = pr.ideal( list=rg );
-print "Ideal, GB: " + str(pg);
-print;
-
-rgl = PolynomialList(pl.ring,rg);
-
-sl = PolyUtilApp.productSlice( rgl );
-#print;
-#print "product slice:", sl;
-#print;
-
-ssl = PolyUtilApp.productSliceToString( sl );
-print;
-print "product slice:", ssl;
+bg = gs.isCGB();
+if bg:
+    print "isCGB: true";
+else:
+    print "isCGB: false";
 print;
 
 
-#bg = rgbp.isGB(rg);
-bg = cgb.isGB(rg);
-print "isGB:", bg;
-print;
+#terminate();
 
-cpl = PolyUtilApp.productSlice( rgl, 0 );
-cplist = cpl.list;
-bg = cgb.isCGB(0,cplist);
-print "isCGB:", bg;
-print;
-
-cpl = PolyUtilApp.productSlicesUnion( rgl );
-cplist = cpl.list;
-bg = cgb.isCGB(0,cplist);
-print "isCGB:", bg;
-print;
-
-startLog();
-terminate();
-#sys.exit();
