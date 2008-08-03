658442c2ba0b836445dfd07c8ca55dfd72d238c4
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2008-08-03 09:56:01 +0000

    coe moved to Examples
    git-svn-id: file:///home/SUBVERSION/jas/trunk@1976 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/poly/Examples.java
+++ src/edu/jas/poly/Examples.java
@@ -24,6 +24,7 @@ public class Examples {
  * main.
  */
    public static void main (String[] args) {
+       //example0();
        /*
        example1();
        example2();
@@ -40,6 +41,40 @@ public class Examples {
        //example11();
        //example12();
    }
+
+
+/**
+ * example0.
+ * for PPPJ 2006.
+ */
+public static void example0() {
+        BigInteger z = new BigInteger();
+
+        TermOrder to = new TermOrder();
+        String[] vars = new String[] { "x1", "x2", "x3" };
+        GenPolynomialRing<BigInteger> ring;
+        ring = new GenPolynomialRing<BigInteger>(z,3,to,vars);
+        System.out.println("ring = " + ring);
+
+        GenPolynomial<BigInteger> pol;
+        pol = ring.parse( "3 x1^2 x3^4 + 7 x2^5 - 61" );
+        System.out.println("pol = " + pol);
+        System.out.println("pol = " + pol.toString(ring.getVars()));
+
+        GenPolynomial<BigInteger> one;
+        one = ring.parse( "1" );
+        System.out.println("one = " + one);
+        System.out.println("one = " + one.toString(ring.getVars()));
+
+        GenPolynomial<BigInteger> p;
+        p = pol.subtract(pol);
+        System.out.println("p = " + p);
+        System.out.println("p = " + p.toString(ring.getVars()));
+
+        p = pol.multiply(pol);
+        System.out.println("p = " + p);
+        System.out.println("p = " + p.toString(ring.getVars()));
+}
 
 
 /**
