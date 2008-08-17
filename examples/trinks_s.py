290d913147a70e897d3dc83f2b12a0271a32cfaa
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2008-08-17 09:52:31 +0000

    allow power to 0
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2076 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- examples/trinks_s.py
+++ examples/trinks_s.py
@@ -19,15 +19,16 @@ from jas import terminate
 #r = Ring( "Z(B,S,T,Z,P,W) L" );
 #r = Ring( "C(B,S,T,Z,P,W) L" );
 #r = Ring( "Z(B,S,T,Z,P,W) L" );
-#r = Ring( "IntFunc(e,f)(B,S,T,Z,P,W) L" );
-r = Ring( "Z(B,S,T,Z,P,W) L" );
+r = Ring( "IntFunc(e,f)(B,S,T,Z,P,W) L" );
+#r = Ring( "Z(B,S,T,Z,P,W) L" );
 print "Ring: " + str(r);
 print;
 
+# sage like: with generators for the polynomial ring
 print "r.gens() = ", [ str(f) for f in r.gens() ];
 print;
-#[e,f,B,S,T,Z,P,W] = r.gens();
-[B,S,T,Z,P,W] = r.gens();
+[e,f,B,S,T,Z,P,W] = r.gens();
+#[B,S,T,Z,P,W] = r.gens();
 
 f1 = 45 * P + 35 * S - 165 * B - 36;
 f2 = 35 * P + 40 * Z + 25 * T - 27 * S;
@@ -36,9 +37,9 @@ f4 = - 9 * W + 15 * T * P + 20 * S * Z;
 f5 = P * W + 2 * T * Z - 11 * B**3;
 f6 = 99 * W - 11 *B * S + 3 * B**2;
 f7 = 10000 * B**2 + 6600 * B + 2673;
-#f7 = f7 + e * f6**2;
+f7 = f7 + e * f6**0;
 #f7 = f7 + 5959345574908321469098512640906154241024000000**2 * f6;
-f7 = f7 + 35555/332 * f1;
+#f7 = f7 + 35555/332 * f1;
 F = [ f1, f2, f3, f4, f5, f6, f7 ];
 #F = [ f1, f2, f3, f4, f5, f6 ]; 
 #print "F = ", [ str(f) for f in F ];
