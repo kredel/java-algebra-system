c23f1c6ff9965423cec65f9541982ed32c9ebd1d
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2008-10-03 12:50:42 +0000

    added more examples
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2162 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- examples/powerseries.py
+++ examples/powerseries.py
@@ -5,11 +5,17 @@
 
 import sys;
 
+from jas import QQ
+from jas import DD
 from jas import SeriesRing
 from jas import startLog
 
 # example for power series
 #
+#
+
+#
+# rational number examples
 #
 
 pr = SeriesRing("Q(y)");
@@ -40,5 +46,49 @@ r4 = r1 * r2;
 print "r4:", r4;
 print;
 
+e = pr.exp();
+print "e:", e;
+print;
+
+s = pr.sin();
+print "s:", s;
+print;
+
+s1 = s.evaluate( QQ(0) );
+print "s1:", s1;
+print;
+
+c = pr.cos();
+print "c:", c;
+print;
+
+c1 = c.evaluate( QQ(0) );
+print "c1:", c1;
+print;
+
+
+#
+# floating point examples
+#
+
+dr = SeriesRing(cofac=DD());
+print "dr:", dr;
+print;
+
+de = dr.exp();
+print "de:", de;
+print;
+
+d0 = de.evaluate( DD(0) );
+print "d0:", d0;
+print;
+
+d1 = de.evaluate( DD(0.5) );
+print "d1:", d1;
+print;
+
+d01 = de.evaluate( DD(0.000000000000000001) );
+print "d01:", d01;
+print;
 
 #sys.exit();
