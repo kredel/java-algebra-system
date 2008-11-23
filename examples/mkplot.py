a980e0fdb8b0bef722bd712ae84a064106d2dfba
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2008-11-23 20:29:49 +0000

    new script for generation of plots
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2231 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- examples/mkplot.py
+++ examples/mkplot.py
@@ -2,96 +2,134 @@
 # read output of jas runnings and prepare input for gnuplot
 #
 
+from sys import argv
+from sys import exit
+from time import strftime
 import re;
-import os;
+import os
+from os import system
 
-exam = "kat_6";
-runs = "t16";
-
-#bname = exam + "_" + runs;
-bname = "k6seqpair-2"
-fname = bname + ".out";
-oname = bname + ".po";
-
-f=open(fname,"r");
-print f;
-o=open(oname,"w");
-print o;
-
+dpat = re.compile(r'd = (\d+), time = (\d+) milliseconds, (\d+) start-up');
+ppat = re.compile(r'time = (\d+) milliseconds');
+fpat = re.compile(r'_p(\d+)\.');
+rpat = re.compile(r'pairlist #put = (\d+) #rem = (\d+)');
 res = {};
-resold = {};
+pairs = None;
 
-for line in f:
-    if line.find("executed in") > 0:
-       print line,
-       if line.find("sequential") >= 0:
-          s = re.search(".*in (\d+)",line);
-          if s != None:
-             t = ('0', s.group(1));
-             res[ int( t[0] ) ] = float( t[1] );
-       else:
-          if line.find("-old") >= 0:
-             s = re.search("(\d+).*in (\d+)",line);
-             if s != None:
-                t = s.groups();
-                t0 = int( t[0] );
-                if resold.has_key( t0 ):
-                   resold[ t0 ] = ( resold[ t0 ] + float(t[1]) )/2.0;
-                else:
-                   resold[ t0 ] = float( t[1] );
-          else:
-             s = re.search("(\d+).*in (\d+)",line);
-             if s != None:
-                t = s.groups();
-                t0 = int( t[0] );
-                if res.has_key( t0 ):
-                   res[ t0 ] = ( res[ t0 ] + float(t[1]) )/2.0;
-                else:
-                   res[ t0 ] = float( t[1] );
-#            print "t = ", t, "\n";
+for fname in argv[1:]:
+    dn = -1;
+    print "file =", fname
+    f = open(fname,"r");
+    for line in f:
+        ma = dpat.search(line);
+        if ma:
+            print "1: ", line,
+            dn = ma.group(1);
+            tm = ma.group(2);
+            su = ma.group(3);
+            res[ int(dn) ] = { 't': tm, 's': su };
+        else:
+            ma = ppat.search(line);
+            if ma:
+                print "2: ", line,
+                tm = ma.group(1);
+                fm = fpat.search(fname);
+                if fm:
+                    print fname
+                    dn = fm.group(1);
+                    res[ int(dn) ] = { 't': tm, 's': "0" };
+        if int(dn) >= 0 and pairs != None:
+            res[ int(dn) ]['p'] = pairs[0];
+            res[ int(dn) ]['r'] = pairs[1];
+            pairs = None;
+        pm = rpat.search(line);
+        if pm:
+            print "3: ", line,
+            np = pm.group(1);
+            nr = pm.group(2);
+            if pairs == None:
+                pairs = ( np, nr );
+                #print "put = " + np + ", rem = " + nr;
+            
+
 
 print "\nres = ", res;
 
 ks = res.keys();
-#print "ks = ", ks;
+print "ks = ", ks;
 
 ks.sort();
-#print "ks = ", ks;
+print "ks = ", ks;
+
+#for d in ks:
+#    print " " + str(d) + " & " + res[d]['t'] + " & " + res[d]['s'] 
 
 s1 = ks[0];
-st = res[ s1 ];
+st = int( res[ s1 ]['t'] );
 
-#print "s1 = ", s1;
-#print "st = ", st;
+print "s1 = ", s1;
+print "st = ", st;
 
 speed = {};
-speedold = {};
 
 for k in ks:
-    speed[ k ] = st / res[ k ]; 
+    speed[ k ] = st / float(res[ k ]['t']); 
 
-#print "speed = ", speed;
+print "speed = ", speed;
 
-print;
+bname = "kat6_";
+tag = argv[1];
+i = tag.find("/");
+if i >= 0:
+    bname = tag[:i];
+print "bname = " + bname;
 
-o.write("\n#threads time speedup\n");
-print "#threads time speedup";
+summary =   "\n% run = " + tag + "\n";
+summary += r"\begin{tabular}{|r|r|r|r|r|r|}" + "\n";
+summary += r"\hline" + "\n";
+summary += r"\#" + " threads / nodes & time & startup & speedup & put & rem \n";
+summary += r"\\ \hline" + "\n";
+
+for d in ks:
+    if res[d]['s'] != "0":
+        summary += " " + str(d) + " & " + res[d]['t'] + " & " + res[d]['s'] + " & " + str(speed[d])[:4];
+    else: 
+        summary += " " + str(d) + " & " + res[d]['t'] + " & " + " & " + str(speed[d])[:4];
+    if res[d]['p'] != "":
+        summary += " & " + res[d]['p'] + " & " + res[d]['r'] + "\n" 
+    summary += r"\\ \hline" + "\n";
+
+summary += r"\end{tabular}" + "\n";
+print summary;
+
+tname = bname+".tex";
+tt=open(tname,"w");
+tt.write(summary);
+tt.close();
+
+
+oname = bname+".po";
+o=open(oname,"w");
+ploting = "\n#threads time speedup\n";
 for k in ks:
-    o.write(str(k) + " " + str(res[k]) + " " + str(speed[k]) +"\n");
-    print(str(k) + " " + str(res[k]) + " " + str(speed[k]));
+    ploting += str(k) + " " + str(res[k]['t']) + " " + str(speed[k]) + "\n";
 
-f.close();
+ploting += "\n";
+
+print ploting;
+o.write(ploting);
 o.close();
 
+#exit();
 
 #---------------------------------------
 pscript = """
 set grid 
 set term %s
 set output "%s.ps"
-set title "GBs of Katsuras example on compute" 
+set title "GBs of Katsuras example on a grid cluster" 
 set time
-set xlabel "#CPUs" 
+set xlabel "#nodes" 
 
 set multiplot
 
@@ -111,22 +149,28 @@ plot  "%s.po" using 1:3 title '%s speedup' with linespoints, \
 
 unset multiplot
 pause mouse
-""";
+"""
 #---------------------------------------
 
+exam = bname;
 psname = bname + ".ps";
 pname = "plotin.gp"
 p=open(pname,"w");
 print p;
 
-pscript = pscript %       ("x11",bname,bname,exam,bname,str(res[0]),exam,bname,exam,bname,exam);
-#pscript = pscript % ("postscript",bname,bname,exam,bname,str(res[0]),exam,bname,exam,bname,exam);
+mkpdf = 0; 
+
+if mkpdf:
+    pscript = pscript % ("postscript",bname,bname,exam,bname,str(res[ks[0]]['t']),exam,bname,exam,bname,exam);
+else:
+    pscript = pscript %       ("x11",bname,bname,exam,bname,str(res[ks[0]]['t']),exam,bname,exam,bname,exam);
 
 p.write(pscript);
 p.close();
 
 os.system("gnuplot plotin.gp");
 
-cmd = "ps2pdf " + psname;
-print "cmd: " + cmd;
-#os.system(cmd);
+if mkpdf:
+    cmd = "ps2pdf " + psname;
+    print "cmd: " + cmd;
+    os.system(cmd);
