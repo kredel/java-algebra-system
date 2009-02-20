668218b58ea1aee473e5acf3b3b4812282f02330
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2009-02-20 15:22:52 +0000

    improved evaluation parameters
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2443 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/ufd/FactorAlgebraic.java
+++ src/edu/jas/ufd/FactorAlgebraic.java
@@ -98,52 +98,41 @@ public class FactorAlgebraic<C extends GcdRingElem<C>> extends FactorAbstract<Al
         long ks = k;
         GenPolynomial<C> res = null;
         boolean sqf = false;
+        //        int[] klist = new int[]{ 0, 1, 2, 3, -1, -2, -3 , -4};
+        int[] klist = new int[]{ 0, -1, -2, 1, 2 };
+        int ki = 0;
         while (!sqf) {
-            if (k >= 6) {
+            // k = 0,1,2,-1,-2
+            if ( ki >= klist.length ) {
                 break;
             }
-            if (k > 4) {
-                k = -1;
-                //break;
-            }
-            if (k < -4) {
-                k = 17;
-            }
+            k = klist[ki]; ki++;
             // compute norm with x -> ( y - k x )
             ks = k;
             res = PolyUfdUtil.<C> norm(P, ks);
             //System.out.println("res = " + res);
             if (res.isZERO() || res.isConstant()) {
-                if (k < 0) {
-                    k--;
-                } else {
-                    k++;
-                }
                 continue;
             }
             sqf = factorCoeff.isSquarefree(res);
-            //System.out.println("sqf("+ks+") = " + sqf + "\n");
+            //System.out.println("sqf("+ks+") = " + res.degree());
             //System.out.println("resfact = " + factorCoeff.baseFactors(res) + "\n");
-            if (k < 0) {
-                k--;
-            } else {
-                k++;
-            }
         }
-        // if Res is now squarefree, we can factor it, else must stop
+        // if Res is now squarefree, else must take radical factorization
         List<GenPolynomial<C>> nfacs;
         if (!sqf) {
-            System.out.println("\nres = " + res); 
+            //System.out.println("\nres = " + res); 
             System.out.println("sqf("+ks+") = " + sqf + "\n");
-            throw new RuntimeException("resultant not squarefree after 10 tries");
+            //res = factorCoeff.squarefreePart(res); // better use obtained factors
+            //res = factorCoeff.baseFactors(res).lastKey();
         } 
-        nfacs = factorCoeff.baseFactorsSquarefree(res);
-        if (debug && !factorCoeff.isFactorization(res, nfacs)) {
-            System.out.println("\nres = " + res); 
-            System.out.println("nfacs = " + nfacs); 
-            throw new RuntimeException("isFactorization = false");
+        //res = res.monic();
+        if ( logger.isInfoEnabled() ) {
+            logger.info("res = " + res); 
+            //System.out.println("\nres = " + res); 
         }
-        if ( debug ) {
+        nfacs = factorCoeff.baseFactorsRadical(res);
+        if ( logger.isInfoEnabled() ) {
             logger.info("res facs = " + nfacs); // Q[X]
             //System.out.println("\nnfacs = " + nfacs); // Q[X]
         }
@@ -159,31 +148,34 @@ public class FactorAlgebraic<C extends GcdRingElem<C>> extends FactorAbstract<Al
         for (GenPolynomial<C> nfi : nfacs) {
             //System.out.println("nfi = " + nfi);
             Ni = PolyUfdUtil.<C> substituteConvertToAlgebraicCoefficients(pfac, nfi, ks);
-            if (logger.isInfoEnabled()) {
+            if (logger.isDebugEnabled()) {
                 logger.info("Ni = " + Ni);
+                //System.out.println("Pp = " + Pp);
+                //System.out.println("Ni = " + Ni);
             }
-            //System.out.println("Pp = " + Pp);
-            //System.out.println("Ni = " + Ni);
             // compute gcds of factors with polynomial
             GenPolynomial<AlgebraicNumber<C>> pni = engine.gcd(Ni, Pp);
-            //System.out.println("pni = " + pni);
             if (!pni.leadingBaseCoefficient().isONE()) {
                 System.out.println("gcd(Ni,Pp) not monic " + pni);
                 pni = pni.monic();
             }
-            //System.out.println("gcd(Ni,Pp) = " + pni);
+            if (logger.isInfoEnabled()) {
+                logger.info("gcd(Ni,Pp) = " + pni);
+                //System.out.println("gcd(Ni,Pp) = " + pni);
+            }
             if (!pni.isONE()) {
                 factors.add(pni);
                 Pp = Pp.divide(pni);
-            } else {
-                GenPolynomial<AlgebraicNumber<C>> qni = Pp.divide(Ni);
-                GenPolynomial<AlgebraicNumber<C>> rni = Pp.remainder(Ni);
-                System.out.println("div qni = " + qni);
-                System.out.println("div rni = " + rni);
-                throw new RuntimeException("gcd(Ni,Pp) == 1");
+//             } else {
+//                 GenPolynomial<AlgebraicNumber<C>> qni = Pp.divide(Ni);
+//                 GenPolynomial<AlgebraicNumber<C>> rni = Pp.remainder(Ni);
+//                 System.out.println("div qni = " + qni);
+//                 System.out.println("div rni = " + rni);
+//                 continue;
+//                 //throw new RuntimeException("gcd(Ni,Pp) == 1");
             }
         }
-        if (!Pp.isZERO() && !Pp.isONE()) { // hack to pretend factorization
+        if (!Pp.isZERO() && !Pp.isONE()) { // hack to pretend irreducible
             factors.add(Pp);
         }
         //System.out.println("afactors = " + factors);
