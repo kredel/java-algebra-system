a222866a7a8d38458fed8f7d47043d26299f9920
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2009-03-22 14:45:34 +0000

    tests for real algebraic numbers
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2498 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ src/edu/jas/root/RealAlgebraicTest.java
@@ -0,0 +1,289 @@
+
+/*
+ * $Id$
+ */
+
+package edu.jas.root;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import edu.jas.arith.BigRational;
+
+//import edu.jas.structure.RingElem;
+import edu.jas.structure.NotInvertibleException;
+
+import edu.jas.poly.GenPolynomial;
+import edu.jas.poly.GenPolynomialRing;
+
+import edu.jas.root.RealAlgebraicNumber;
+import edu.jas.root.RealAlgebraicRing;
+
+
+/**
+ * RealAlgebraicNumber Test using JUnit. 
+ * @author Heinz Kredel.
+ */
+
+public class RealAlgebraicTest extends TestCase {
+
+/**
+ * main.
+ */
+   public static void main (String[] args) {
+          junit.textui.TestRunner.run( suite() );
+   }
+
+/**
+ * Constructs a <CODE>RealAlgebraicTest</CODE> object.
+ * @param name String.
+ */
+   public RealAlgebraicTest(String name) {
+          super(name);
+   }
+
+/**
+ * suite.
+ */ 
+ public static Test suite() {
+     TestSuite suite= new TestSuite(RealAlgebraicTest.class);
+     return suite;
+   }
+
+   //private final static int bitlen = 100;
+
+   RealAlgebraicRing<BigRational> fac;
+   GenPolynomialRing<BigRational> mfac;
+
+   RealAlgebraicNumber< BigRational > a;
+   RealAlgebraicNumber< BigRational > b;
+   RealAlgebraicNumber< BigRational > c;
+   RealAlgebraicNumber< BigRational > d;
+   RealAlgebraicNumber< BigRational > e;
+
+   int rl = 1; 
+   int kl = 10;
+   int ll = 10;
+   int el = ll;
+   float q = 0.5f;
+
+   protected void setUp() {
+       a = b = c = d = e = null;
+       BigRational l = new BigRational(1);
+       BigRational r = new BigRational(2);
+       mfac = new GenPolynomialRing<BigRational>( new BigRational(1), rl );
+       GenPolynomial<BigRational> mo = mfac.univariate(0,2);
+       mo = mo.subtract( mfac.fromInteger(2) ); // x^2 -2 
+       fac = new RealAlgebraicRing<BigRational>( mo, new Interval<BigRational>(l,r) );
+   }
+
+   protected void tearDown() {
+       a = b = c = d = e = null;
+       fac = null;
+   }
+
+
+/**
+ * Test constructor and toString.
+ * 
+ */
+ public void testConstruction() {
+     c = fac.getONE();
+     //System.out.println("c = " + c);
+     //System.out.println("c.getVal() = " + c.getVal());
+     assertTrue("length( c ) = 1", c.getVal().length() == 1);
+     assertTrue("isZERO( c )", !c.isZERO() );
+     assertTrue("isONE( c )", c.isONE() );
+
+     d = fac.getZERO();
+     //System.out.println("d = " + d);
+     //System.out.println("d.getVal() = " + d.getVal());
+     assertTrue("length( d ) = 0", d.getVal().length() == 0);
+     assertTrue("isZERO( d )", d.isZERO() );
+     assertTrue("isONE( d )", !d.isONE() );
+ }
+
+
+/**
+ * Test random polynomial.
+ * 
+ */
+ public void testRandom() {
+     for (int i = 0; i < 7; i++) {
+         a = fac.random(el);
+         //System.out.println("a = " + a);
+
+         // fac.random(rl+i, kl*(i+1), ll+2*i, el+i, q );
+         assertTrue("length( a"+i+" ) <> 0", a.getVal().length() >= 0);
+         assertTrue(" not isZERO( a"+i+" )", !a.isZERO() );
+         assertTrue(" not isONE( a"+i+" )", !a.isONE() );
+     }
+ }
+
+
+/**
+ * Test addition.
+ * 
+ */
+ public void testAddition() {
+     a = fac.random(ll);
+     b = fac.random(ll);
+
+     c = a.sum(b);
+     d = c.subtract(b);
+     assertEquals("a+b-b = a",a,d);
+
+     c = a.sum(b);
+     d = b.sum(a);
+     assertEquals("a+b = b+a",c,d);
+
+     c = fac.random(ll);
+     d = c.sum( a.sum(b) );
+     e = c.sum( a ).sum(b);
+     assertEquals("c+(a+b) = (c+a)+b",d,e);
+
+
+     c = a.sum( fac.getZERO() );
+     d = a.subtract( fac.getZERO() );
+     assertEquals("a+0 = a-0",c,d);
+
+     c = fac.getZERO().sum( a );
+     d = fac.getZERO().subtract( a.negate() );
+     assertEquals("0+a = 0+(-a)",c,d);
+ }
+ 
+
+/**
+ * Test object multiplication.
+ * 
+ */
+ public void testMultiplication() {
+     a = fac.random(ll);
+     assertTrue("not isZERO( a )", !a.isZERO() );
+
+     b = fac.random(ll);
+     assertTrue("not isZERO( b )", !b.isZERO() );
+
+     c = b.multiply(a);
+     d = a.multiply(b);
+     assertTrue("not isZERO( c )", !c.isZERO() );
+     assertTrue("not isZERO( d )", !d.isZERO() );
+
+     //System.out.println("a = " + a);
+     //System.out.println("b = " + b);
+     e = d.subtract(c);
+     assertTrue("isZERO( a*b-b*a ) " + e, e.isZERO() );
+
+     assertTrue("a*b = b*a", c.equals(d) );
+     assertEquals("a*b = b*a",c,d);
+
+     c = fac.random(ll);
+     //System.out.println("c = " + c);
+     d = a.multiply( b.multiply(c) );
+     e = (a.multiply(b)).multiply(c);
+
+     //System.out.println("d = " + d);
+     //System.out.println("e = " + e);
+
+     //System.out.println("d-e = " + d.subtract(c) );
+
+     assertEquals("a(bc) = (ab)c",d,e);
+     assertTrue("a(bc) = (ab)c", d.equals(e) );
+
+     c = a.multiply( fac.getONE() );
+     d = fac.getONE().multiply( a );
+     assertEquals("a*1 = 1*a",c,d);
+
+
+     c = a.inverse();
+     d = c.multiply(a);
+     //System.out.println("a = " + a);
+     //System.out.println("c = " + c);
+     //System.out.println("d = " + d);
+     assertEquals("a*1/a = 1",fac.getONE(),d);
+
+     try {
+         a = fac.getZERO().inverse();
+     } catch(NotInvertibleException expected) {
+         return;
+     }
+     fail("0 invertible");
+ }
+
+
+/**
+ * Test distributive law.
+ * 
+ */
+ public void testDistributive() {
+     a = fac.random( ll );
+     b = fac.random( ll );
+     c = fac.random( ll );
+
+     d = a.multiply( b.sum(c) );
+     e = a.multiply( b ).sum( a.multiply(c) );
+
+     assertEquals("a(b+c) = ab+ac",d,e);
+ }
+
+
+/**
+ * Test sign of real algebraic numbers.
+ * 
+ */
+ public void testSignum() {
+     a = fac.random( ll );
+     b = fac.random( ll );
+     c = fac.random( ll );
+
+     int sa = a.signum();
+     int sb = b.signum();
+     int sc = c.signum();
+
+     d = a.multiply( b );
+     e = a.multiply( c );
+
+     int sd = d.signum();
+     int se = e.signum();
+
+     assertEquals("sign(a*b) = sign(a)*sign(b) ",sa*sb,sd);
+     assertEquals("sign(a*c) = sign(a)*sign(c) ",sa*sc,se);
+
+     b = a.negate();
+     sb = b.signum();
+     assertEquals("sign(-a) = -sign(a) ",-sa,sb);
+ }
+
+
+/**
+ * Test compareTo of real algebraic numbers.
+ * 
+ */
+ public void testCompare() {
+     a = fac.random( ll ).abs();
+     b = a.sum( fac.getONE() );
+     c = b.sum( fac.getONE() );
+
+     int ab = a.compareTo(b);
+     int bc = b.compareTo(c);
+     int ac = a.compareTo(c);
+
+     assertTrue("a < a+1 ", ab < 0);
+     assertTrue("a+1 < a+2 ", bc < 0);
+     assertTrue("a < a+2 ", ac < 0);
+
+     a = a.negate();
+     b = a.sum( fac.getONE() );
+     c = b.sum( fac.getONE() );
+
+     ab = a.compareTo(b);
+     bc = b.compareTo(c);
+     ac = a.compareTo(c);
+
+     assertTrue("a < a+1 ", ab < 0);
+     assertTrue("a+1 < a+2 ", bc < 0);
+     assertTrue("a < a+2 ", ac < 0);
+ }
+
+}
