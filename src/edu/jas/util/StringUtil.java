a151734ec57f81548694a0535bfcbb84da31d0a7
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2009-04-12 20:07:42 +0000

    new variableList() method
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2554 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/util/StringUtil.java
+++ src/edu/jas/util/StringUtil.java
@@ -8,6 +8,10 @@ import java.io.Reader;
 import java.io.StringWriter;
 import java.io.IOException;
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 
 /**
  * Static String and Reader methods.
@@ -15,6 +19,43 @@ import java.io.IOException;
  */
 
 public class StringUtil {
+
+
+    /** Parse variable list from String.
+     * @param s String.
+     * Syntax: (n1,...,nk) or (n1 ... nk), brackest are also optional.
+     * @return array of variable names found in s.
+     */
+    public static String[] variableList(String s) {
+        String[] vl = null;
+        if ( s == null ) {
+            return vl;
+        }
+        String st = s.trim();
+        if ( st.length() == 0 ) {
+            return new String[0];
+        }
+        if ( st.charAt(0) == '(' ) {
+            st = st.substring(1);
+        }
+        if ( st.charAt(st.length()-1) == ')' ) {
+            st = st.substring(0,st.length()-1);
+        }
+        st = st.replaceAll(","," ");
+        List<String> sl = new ArrayList<String>();
+        Scanner sc = new Scanner(st);
+        while ( sc.hasNext() ) {
+            String sn = sc.next();
+            sl.add(sn);
+        }
+        vl = new String[sl.size()];
+        int i = 0;
+        for ( String si : sl ) {
+            vl[i] = si;
+            i++;
+        }
+        return vl;
+    }
 
 
     /** Parse white space delimited String from Reader.
