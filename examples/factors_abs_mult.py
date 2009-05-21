8ef70d1a53d9d58aca70b84a6110da0534f8844b
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2009-05-21 16:44:17 +0000

    example with absolute factoring
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2637 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ examples/factors_abs_mult.py
@@ -0,0 +1,52 @@
+#
+# jython examples for jas.
+# $Id$
+#
+
+from java.lang import System
+from java.lang import Integer
+
+from jas import Ring
+from jas import Ideal
+from jas import terminate
+from jas import startLog
+
+# polynomial examples: absolute factorization over Q
+
+#r = Ring( "Rat(x) L" );
+r = Ring( "Q(x,y) L" );
+
+print "Ring: " + str(r);
+print;
+
+[one,x,y] = r.gens();
+
+f1 = x**2 + y**2;
+f2 = x**3 + y**2;
+f3 = x**4 + 4;
+
+f = f1**3 * f2**1 * f3**2;
+
+print "f = ", f;
+print;
+
+startLog();
+
+t = System.currentTimeMillis();
+#G = r.squarefreeFactors(f);
+G = r.factorsAbsolute(f);
+t = System.currentTimeMillis() - t;
+print "G = ", G;
+print
+print "factor time =", t, "milliseconds";
+
+g = one;
+for h, i in G.iteritems():
+    print "h**i = (", h, ")**" + str(i);
+    h = h**i;
+    #g = g*h;
+#print "g = ", g;
+print
+
+#startLog();
+terminate();
