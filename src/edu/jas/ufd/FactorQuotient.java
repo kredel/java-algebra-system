a0937d600ff2c19c6e7b7cf400251a002a79c8fd
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2009-07-04 13:48:15 +0000

    code improvements
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2684 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/ufd/FactorQuotient.java
+++ src/edu/jas/ufd/FactorQuotient.java
@@ -40,7 +40,7 @@ public class FactorQuotient<C extends GcdRingElem<C>> extends FactorAbstract<Quo
     /**
      * Factorization engine for normal coefficients.
      */
-    protected final FactorAbstract<C> iengine;
+    protected final FactorAbstract<C> nengine;
 
 
     /**
@@ -57,17 +57,7 @@ public class FactorQuotient<C extends GcdRingElem<C>> extends FactorAbstract<Quo
      */
     public FactorQuotient(QuotientRing<C> fac) {
         super( fac );
-        iengine = FactorFactory.<C>getImplementation( fac.ring.coFac );
-    }
-
-
-    /**
-     * Constructor. 
-     * <b>Note:</b> for convenience, to be able to use *polynom*() methods.
-     * @param fac coefficient polynomial factory.
-     */
-    public FactorQuotient(GenPolynomialRing<C> fac) {
-        this( new QuotientRing<C>(fac) );
+        nengine = FactorFactory.<C>getImplementation( fac.ring.coFac );
     }
 
 
@@ -78,56 +68,7 @@ public class FactorQuotient<C extends GcdRingElem<C>> extends FactorAbstract<Quo
      */
     @Override
     public List<GenPolynomial<Quotient<C>>> baseFactorsSquarefree(GenPolynomial<Quotient<C>> P) {
-        if (P == null) {
-            throw new RuntimeException(this.getClass().getName() + " P == null");
-        }
-        List<GenPolynomial<Quotient<C>>> factors = new ArrayList<GenPolynomial<Quotient<C>>>();
-        if (P.isZERO()) {
-            return factors;
-        }
-        if (P.isONE()) {
-            factors.add(P);
-            return factors;
-        }
-        GenPolynomialRing<Quotient<C>> pfac = P.ring;
-        if (pfac.nvar > 1) {
-            throw new RuntimeException(this.getClass().getName() + " only for univariate polynomials");
-        }
-        GenPolynomial<Quotient<C>> Pr = P;
-        Quotient<C> ldcf = P.leadingBaseCoefficient();
-        if (!ldcf.isONE()) {
-            //System.out.println("ldcf = " + ldcf);
-            Pr = Pr.monic();
-        }
-        QuotientRing<C> qi = (QuotientRing<C>)pfac.coFac;
-        GenPolynomialRing<C> ci = qi.ring;
-        GenPolynomialRing<GenPolynomial<C>> ifac = new GenPolynomialRing<GenPolynomial<C>>(ci, pfac);
-        GenPolynomial<GenPolynomial<C>> Pi = PolyUfdUtil.<C>integralFromQuotientCoefficients(ifac, Pr);
-        //System.out.println("Pi = " + Pi);
-        // factor in C[x_1,...,x_n][y]
-        List<GenPolynomial<GenPolynomial<C>>> ifacts = polynomFactorsSquarefree(Pi);
-        if (logger.isInfoEnabled()) {
-            logger.info("ifacts = " + ifacts);
-        }
-        if (ifacts.size() <= 1) {
-            factors.add(P);
-            return factors;
-        }
-        List<GenPolynomial<Quotient<C>>> rfacts = PolyUfdUtil.<C>quotientFromIntegralCoefficients(pfac, ifacts);
-        //System.out.println("rfacts = " + rfacts);
-        rfacts = PolyUtil.monic(rfacts);
-        //System.out.println("rfacts = " + rfacts);
-        if ( !ldcf.isONE() ) {
-            GenPolynomial<Quotient<C>> r = rfacts.get(0);
-            rfacts.remove(r);
-            r = r.multiply(ldcf);
-            rfacts.add(0, r);
-        }
-        if (logger.isInfoEnabled()) {
-            logger.info("rfacts = " + rfacts);
-        }
-        factors.addAll(rfacts);
-        return factors;
+        return factorsSquarefree(P);
     }
 
 
@@ -161,85 +102,30 @@ public class FactorQuotient<C extends GcdRingElem<C>> extends FactorAbstract<Quo
         GenPolynomialRing<GenPolynomial<C>> ifac = new GenPolynomialRing<GenPolynomial<C>>(ci, pfac);
         GenPolynomial<GenPolynomial<C>> Pi = PolyUfdUtil.<C>integralFromQuotientCoefficients(ifac, Pr);
         //System.out.println("Pi = " + Pi);
+
 	// factor in C[x_1,...,x_n][y_1,...,y_m]
-        List<GenPolynomial<GenPolynomial<C>>> ifacts = polynomFactorsSquarefree(Pi);
+        List<GenPolynomial<GenPolynomial<C>>> irfacts = nengine.recursiveFactorsSquarefree(Pi);
         if (logger.isInfoEnabled()) {
-            logger.info("ifacts = " + ifacts);
+            logger.info("irfacts = " + irfacts);
         }
-        if (ifacts.size() <= 1) {
+        if (irfacts.size() <= 1) {
             factors.add(P);
             return factors;
         }
-        List<GenPolynomial<Quotient<C>>> rfacts = PolyUfdUtil.<C>quotientFromIntegralCoefficients(pfac, ifacts);
-        //System.out.println("rfacts = " + rfacts);
-        rfacts = PolyUtil.monic(rfacts);
-        //System.out.println("rfacts = " + rfacts);
+        List<GenPolynomial<Quotient<C>>> qfacts = PolyUfdUtil.<C>quotientFromIntegralCoefficients(pfac, irfacts);
+        //System.out.println("qfacts = " + qfacts);
+        qfacts = PolyUtil.monic(qfacts);
+        //System.out.println("qfacts = " + qfacts);
         if ( !ldcf.isONE() ) {
-            GenPolynomial<Quotient<C>> r = rfacts.get(0);
-            rfacts.remove(r);
+            GenPolynomial<Quotient<C>> r = qfacts.get(0);
+            qfacts.remove(r);
             r = r.multiply(ldcf);
-            rfacts.add(0, r);
+            qfacts.add(0, r);
         }
         if (logger.isInfoEnabled()) {
-            logger.info("rfacts = " + rfacts);
+            logger.info("qfacts = " + qfacts);
         }
-        factors.addAll(rfacts);
-        return factors;
-    }
-
-
-    /**
-     * GenPolynomial factorization of a squarefree polynomial.
-     * <b>Note:</b> can not be placed in its own class,
-     * since GenPolynomial doesn't implement GcdRingElem.
-     * @param P squarefree GenPolynomial.
-     * @return [p_1,...,p_k] with P = prod_{i=1, ..., k} p_i.
-     */
-    public List<GenPolynomial<GenPolynomial<C>>> polynomFactorsSquarefree(GenPolynomial<GenPolynomial<C>> P) {
-        if (P == null) {
-            throw new RuntimeException(this.getClass().getName() + " P == null");
-        }
-        List<GenPolynomial<GenPolynomial<C>>> factors = new ArrayList<GenPolynomial<GenPolynomial<C>>>();
-        if (P.isZERO()) {
-            return factors;
-        }
-        if (P.isONE()) {
-            factors.add(P);
-            return factors;
-        }
-        GenPolynomialRing<GenPolynomial<C>> pfac = P.ring;
-        GenPolynomialRing<C> qi = (GenPolynomialRing<C>)pfac.coFac;
-        GenPolynomialRing<C> ifac = qi.extend(pfac.nvar);
-        GenPolynomial<C> Pi = PolyUtil.<C>distribute(ifac, P);
-        //System.out.println("Pi = " + Pi);
-
-        C ldcf = Pi.leadingBaseCoefficient();
-        if (!ldcf.isONE()) {
-            //System.out.println("ldcf = " + ldcf);
-            Pi = Pi.monic();
-        }
-
-        // factor in C[x_1,...,x_n,y_1,...,y_m]
-        List<GenPolynomial<C>> ifacts = iengine.factorsSquarefree(Pi);
-        if (logger.isInfoEnabled()) {
-            logger.info("ifacts = " + ifacts);
-        }
-        if (ifacts.size() <= 1) {
-            factors.add(P);
-            return factors;
-        }
-        if ( !ldcf.isONE() ) {
-            GenPolynomial<C> r = ifacts.get(0);
-            ifacts.remove(r);
-            r = r.multiply(ldcf);
-            ifacts.add(0, r);
-        }
-        List<GenPolynomial<GenPolynomial<C>>> rfacts = PolyUtil.<C>recursive(pfac, ifacts);
-        //System.out.println("rfacts = " + rfacts);
-        if (logger.isInfoEnabled()) {
-            logger.info("rfacts = " + rfacts);
-        }
-        factors.addAll(rfacts);
+        factors.addAll(qfacts);
         return factors;
     }
 
