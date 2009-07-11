aa89fecdd180ef6a74e69237c8587782e70e5fb8
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2009-07-11 10:32:02 +0000

    must check for unit because of residue classes
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2732 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/gb/PseudoReductionSeq.java
+++ src/edu/jas/gb/PseudoReductionSeq.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 
 import edu.jas.poly.ExpVector;
 import edu.jas.poly.GenPolynomial;
+//import edu.jas.poly.PolyUtil;
 
 import edu.jas.structure.RingElem;
 
@@ -110,8 +111,9 @@ public class PseudoReductionSeq<C extends RingElem<C>>
                  e =  e.subtract( htl[i] );
                  //logger.info("red div = " + e);
                  C c = (C) lbc[i];
-                 if ( a.remainder(c).isZERO() ) {   //c.isUnit() ) {
+                 if ( c.isUnit() ) { //a.remainder(c).isZERO() ) {   //c.isUnit() ) {
                     a = a.divide( c );
+                    //System.out.println(" a = " + a);
                  } else {
                     S = S.multiply( c );
                     R = R.multiply( c );
@@ -202,7 +204,7 @@ public class PseudoReductionSeq<C extends RingElem<C>>
                  e =  e.subtract( htl[i] );
                  //logger.info("red div = " + e);
                  C c = (C) lbc[i];
-                 if ( a.remainder(c).isZERO() ) {   //c.isUnit() ) {
+                 if ( c.isUnit() ) { //a.remainder(c).isZERO() ) {   //c.isUnit() ) {
                     a = a.divide( c );
                  } else {
                     S = S.multiply( c );
@@ -292,7 +294,7 @@ public class PseudoReductionSeq<C extends RingElem<C>>
                 e =  e.subtract( htl[i] );
                 //logger.info("red div = " + e);
                 C c = (C) lbc[i];
-                if ( a.remainder(c).isZERO() ) { //c.isUnit() ) {
+                if ( c.isUnit() ) { //a.remainder(c).isZERO() ) { //c.isUnit() ) {
                    a = a.divide( c );
                    //System.out.print("|");
                 } else {
