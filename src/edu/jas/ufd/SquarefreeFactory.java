d4539d4a4e465fae9895f8f73644d5e49d05f12a
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2009-07-12 12:03:54 +0000

    polynomial coefficient case added
    git-svn-id: file:///home/SUBVERSION/jas/trunk@2733 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/ufd/SquarefreeFactory.java
+++ src/edu/jas/ufd/SquarefreeFactory.java
@@ -15,6 +15,8 @@ import edu.jas.arith.ModInteger;
 import edu.jas.arith.ModIntegerRing;
 import edu.jas.poly.AlgebraicNumber;
 import edu.jas.poly.AlgebraicNumberRing;
+import edu.jas.poly.GenPolynomial;
+import edu.jas.poly.GenPolynomialRing;
 import edu.jas.structure.GcdRingElem;
 import edu.jas.structure.RingFactory;
 
@@ -101,7 +103,7 @@ public class SquarefreeFactory {
     /**
      * Determine suitable implementation of squarefree factorization algorithms,
      * case AlgebraicNumber&lt;C&gt;.
-     * @param fac AlgebraicNumber&lt;C&gt;.
+     * @param fac AlgebraicNumberRing&lt;C&gt;.
      * @param <C> coefficient type, e.g. BigRational, ModInteger.
      * @return squarefree factorization algorithm implementation.
      */
@@ -118,7 +120,7 @@ public class SquarefreeFactory {
     /**
      * Determine suitable implementation of squarefree factorization algorithms,
      * case Quotient&lt;C&gt;.
-     * @param fac Quotient&lt;C&gt;.
+     * @param fac QuotientRing&lt;C&gt;.
      * @param <C> coefficient type, e.g. BigRational, ModInteger.
      * @return squarefree factorization algorithm implementation.
      */
@@ -128,6 +130,28 @@ public class SquarefreeFactory {
             return new SquarefreeFieldChar0<Quotient<C>>(fac);
         } else {
             return new SquarefreeInfiniteFieldCharP<C>(fac);
+        }
+    }
+
+
+    /**
+     * Determine suitable implementation of squarefree factorization algorithms,
+     * case GenPolynomial&lt;C&gt;.
+     * @param fac GenPolynomialRing&lt;C&gt;.
+     * @param <C> coefficient type, e.g. BigRational, ModInteger.
+     * @return squarefree factorization algorithm implementation.
+     */
+    public static <C extends GcdRingElem<C>> 
+           SquarefreeAbstract<C> getImplementation( GenPolynomialRing<C> fac) {
+        if (fac.characteristic().signum() == 0) {
+            if ( fac.coFac.isField() ) {
+                return new SquarefreeFieldChar0<C>(fac.coFac);
+            } else {
+                return new SquarefreeRingChar0<C>(fac.coFac);
+            }
+        } else {
+            logger.warn("not checked if finite or infinite field" + fac.coFac);
+            return new SquarefreeFiniteFieldCharP<C>(fac.coFac);
         }
     }
 
@@ -147,6 +171,7 @@ public class SquarefreeFactory {
         SquarefreeAbstract/*raw type<C>*/ufd = null;
         AlgebraicNumberRing afac = null;
         QuotientRing qfac = null;
+        GenPolynomialRing pfac = null;
         while (true) { // switch
             Object ofac = fac;
             if (ofac instanceof BigInteger) {
@@ -182,13 +207,15 @@ public class SquarefreeFactory {
                 t = 7;
                 break;
             }
+            if (ofac instanceof GenPolynomialRing) {
+                //System.out.println("qfac_o = " + ofac);
+                pfac = (GenPolynomialRing) ofac;
+                t = 8;
+                break;
+            }
             break;
         }
         //System.out.println("ft = " + t);
-        if (t == 0) {
-            throw new RuntimeException("no squarefree factorization implementation for "
-                    + fac.getClass().getName());
-        }
         if (t == 1) { // BigInteger
             ufd = new SquarefreeRingChar0/*raw*/(fac);
         }
@@ -211,6 +238,22 @@ public class SquarefreeFactory {
             } else {
                 ufd = new SquarefreeInfiniteFieldCharP/*raw <C>*/(qfac);
             }
+        }
+        if (t == 8) { // GenPolynomial
+            if (pfac.characteristic().signum() == 0) {
+                if ( pfac.coFac.isField() ) {
+                    ufd = new SquarefreeFieldChar0/*raw <C>*/(pfac.coFac);
+                } else {
+                    ufd = new SquarefreeRingChar0/*raw <C>*/(pfac.coFac);
+                }
+            } else {
+                ufd = new SquarefreeFiniteFieldCharP/*raw <C>*/(pfac.coFac);
+                logger.warn("not checked if finite or infinite field" + pfac.coFac);
+            }
+        }
+        if (ufd == null) {
+            throw new RuntimeException("no squarefree factorization implementation for "
+                    + fac.getClass().getName());
         }
         logger.debug("ufd = " + ufd);
         //System.out.println("ufd = " + ufd);
