4fe013e88f9b31dfb8fd7913a7e159b61059c025
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2010-12-04 12:05:32 +0000

    print cosmetic
    git-svn-id: file:///home/SUBVERSION/jas/trunk@3388 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/gb/PairListTest.java
+++ src/edu/jas/gb/PairListTest.java
@@ -93,30 +93,30 @@ public class PairListTest extends TestCase {
     /**
      * Test and compare random OrderedPairlist and CriticalPairList.
      */
-    public void xtestRandomPL() {
+    public void testRandomPL() {
         pairlist = new OrderedPairlist<BigRational>(fac);
-        System.out.println("pairlist = " + pairlist);
+        //System.out.println("pairlist = " + pairlist);
 
         CriticalPairList<BigRational> cpl = new CriticalPairList<BigRational>(fac);
-        System.out.println("cpl = " + cpl);
+        //System.out.println("cpl = " + cpl);
 
         L = new ArrayList<GenPolynomial<BigRational>>();
         for ( int i = 0; i < 7; i++ ) {
-             a = fac.random(kl, ll, el, q );
-             if ( a.isZERO() ) {
-                 continue;
-             }
-             pairlist.put(a);
-             cpl.put(a);
+            a = fac.random(kl, ll, el, q );
+            if ( a.isZERO() ) {
+                continue;
+            }
+            pairlist.put(a);
+            cpl.put(a);
         }
-        System.out.println("pairlist = " + pairlist);
-        System.out.println("cpl = " + cpl);
+        //System.out.println("pairlist = " + pairlist);
+        //System.out.println("cpl = " + cpl);
 
         while ( pairlist.hasNext() && cpl.hasNext() ) {
             Pair<BigRational> pair = pairlist.removeNext();
-            System.out.println("pair = " + pair);
+            //System.out.println("pair = " + pair);
             CriticalPair<BigRational> cpair = cpl.getNext();
-            System.out.println("cpair = " + cpair);
+            //System.out.println("cpair = " + cpair);
             if ( cpair != null ) {
                 cpl.update(cpair,fac.getZERO());
             } else {
@@ -127,8 +127,8 @@ public class PairListTest extends TestCase {
                 assertTrue("pair == cpair ", t);
             }
         }
-        System.out.println("pairlist = " + pairlist);
-        System.out.println("cpl = " + cpl);
+        //System.out.println("pairlist = " + pairlist);
+        //System.out.println("cpl = " + cpl);
         boolean t = pairlist.hasNext() || cpl.hasNext();
         assertFalse("#pairlist == #cpl ", t);
     }
@@ -139,70 +139,37 @@ public class PairListTest extends TestCase {
      */
     public void testRandomSyzPL() {
         pairlist = new OrderedPairlist<BigRational>(fac);
-        System.out.println("pairlist = " + pairlist);
+        //System.out.println("pairlist = " + pairlist);
 
         OrderedSyzPairlist<BigRational> spl = new OrderedSyzPairlist<BigRational>(fac);
-        System.out.println("spl = " + spl);
+        //System.out.println("spl = " + spl);
 
         L = new ArrayList<GenPolynomial<BigRational>>();
         for ( int i = 0; i < 7; i++ ) {
-             a = fac.random(kl, ll, el, q );
-             if ( a.isZERO() ) {
-                 continue;
-             }
-             pairlist.put(a);
-             spl.put(a);
+            a = fac.random(kl, ll, el, q );
+            if ( a.isZERO() ) {
+                continue;
+            }
+            pairlist.put(a);
+            spl.put(a);
         }
-        System.out.println("pairlist = " + pairlist);
-        System.out.println("spl = " + spl);
+        //System.out.println("pairlist = " + pairlist);
+        //System.out.println("spl = " + spl);
 
         while ( pairlist.hasNext() && spl.hasNext() ) {
             Pair<BigRational> pair = pairlist.removeNext();
-            System.out.println("pair = " + pair);
+            //System.out.println("pair = " + pair);
             Pair<BigRational> spair = spl.removeNext();
-            System.out.println("spair = " + spair);
+            //System.out.println("spair = " + spair);
             if ( pair != null && spair != null ) {
                 boolean t = (pair.i == spair.i) && (pair.j == spair.j);
-                //assertTrue("pair == spair " + pair + ", " + spair, t);
+                //not always true: assertTrue("pair == spair " + pair + ", " + spair, t);
             }
         }
-        System.out.println("pairlist = " + pairlist);
-        System.out.println("spl = " + spl);
+        //System.out.println("pairlist = " + pairlist);
+        //System.out.println("spl = " + spl);
         boolean t = pairlist.hasNext() && spl.hasNext();
         assertFalse("#pairlist == #spl ", t);
-    }
-
-
-    /**
-     * Test Trinks7 GBase.
-     * 
-     */
-    @SuppressWarnings("unchecked") // not jet working
-    public void xtestTrinks7GBase() {
-        String exam = "(B,S,T,Z,P,W) L "
-            + "( "  
-            + "( 45 P + 35 S - 165 B - 36 ), " 
-            + "( 35 P + 40 Z + 25 T - 27 S ), "
-            + "( 15 W + 25 S P + 30 Z - 18 T - 165 B**2 ), "
-            + "( - 9 W + 15 T P + 20 S Z ), "
-            + "( P W + 2 T Z - 11 B**3 ), "
-            + "( 99 W - 11 B S + 3 B**2 ), "
-            + "( B**2 + 33/50 B + 2673/10000 ) "
-            + ") ";
-        Reader source = new StringReader( exam );
-        GenPolynomialTokenizer parser
-            = new GenPolynomialTokenizer( source );
-        try {
-            F = (PolynomialList<BigRational>) parser.nextPolynomialSet();
-        } catch(ClassCastException e) {
-            fail(""+e);
-        } catch(IOException e) {
-            fail(""+e);
-        }
-        //System.out.println("F = " + F);
-
-        pairlist = new OrderedPairlist<BigRational>(F.ring);
-
     }
 
 }
