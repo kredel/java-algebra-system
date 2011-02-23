b31cf209f1ec04da128075e6657972e7e58d4246
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2011-02-23 21:53:33 +0000

    removed type parameters
    git-svn-id: file:///home/SUBVERSION/jas/trunk@3543 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/application/ExtensionFieldBuilder.java
+++ src/edu/jas/application/ExtensionFieldBuilder.java
@@ -22,13 +22,13 @@ import edu.jas.ufd.Quotient;
  * Biulder for extension field towers.
  * @author Heinz Kredel
  */
-public class ExtensionFieldBuilder<C extends GcdRingElem<C>> implements Serializable {
+public class ExtensionFieldBuilder implements Serializable {
 
 
     /**
      * The current factory.
      */
-    public final RingFactory<C> factory;
+    public final RingFactory factory; // must be a raw type
 
 
     /**
@@ -43,13 +43,14 @@ public class ExtensionFieldBuilder<C extends GcdRingElem<C>> implements Serializ
      * Constructor.
      * @param base the base field.
      */
-    public ExtensionFieldBuilder(RingFactory<C> base) {
+    public ExtensionFieldBuilder(RingFactory base) {
         factory = base;
     }
 
 
     /**
      * Build the field tower.
+     * TODO: build at the end and optimize field tower for faster computation.
      */
     public RingFactory build() {
         return factory;
@@ -57,15 +58,24 @@ public class ExtensionFieldBuilder<C extends GcdRingElem<C>> implements Serializ
 
 
     /**
+     * Set base field.
+     * @param base the base field for the extensions.
+     */
+    public static ExtensionFieldBuilder baseField(RingFactory base) {
+        return new ExtensionFieldBuilder(base);
+    }
+
+
+    /**
      * Transcendent field extension.
      * @param vars names for the transcendent generators.
      */
-    public ExtensionFieldBuilder<C> transcendentExtension(String vars) {
+    public ExtensionFieldBuilder transcendentExtension(String vars) {
         String[] variables = GenPolynomialTokenizer.variableList(vars);
-        GenPolynomialRing<C> pfac = new GenPolynomialRing<C>(factory,variables);
-        QuotientRing<C> qfac = new QuotientRing<C>(pfac);
-        RingFactory<C> base = (RingFactory<C>) (RingFactory) qfac;
-        return new ExtensionFieldBuilder<C>(base);
+        GenPolynomialRing pfac = new GenPolynomialRing(factory,variables);
+        QuotientRing qfac = new QuotientRing(pfac);
+        RingFactory base = (RingFactory) qfac;
+        return new ExtensionFieldBuilder(base);
     }
 
 
@@ -74,13 +84,13 @@ public class ExtensionFieldBuilder<C extends GcdRingElem<C>> implements Serializ
      * @param var name for the algebraic generator.
      * @param expr generating expresion, a univariate polynomial in var.
      */
-    public ExtensionFieldBuilder<C> algebraicExtension(String var, String expr) {
+    public ExtensionFieldBuilder algebraicExtension(String var, String expr) {
         String[] variables = new String[] { var };
-        GenPolynomialRing<C> pfac = new GenPolynomialRing<C>(factory,variables);
-        GenPolynomial<C> gen = pfac.parse(expr);
-        AlgebraicNumberRing<C> afac = new AlgebraicNumberRing<C>(gen);
-        RingFactory<C> base = (RingFactory<C>) (RingFactory) afac;
-        return new ExtensionFieldBuilder<C>(base);
+        GenPolynomialRing pfac = new GenPolynomialRing(factory,variables);
+        GenPolynomial gen = pfac.parse(expr);
+        AlgebraicNumberRing afac = new AlgebraicNumberRing(gen);
+        RingFactory base = (RingFactory) afac;
+        return new ExtensionFieldBuilder(base);
     }
 
 
--- src/edu/jas/application/ExtensionFieldBuilderTest.java
+++ src/edu/jas/application/ExtensionFieldBuilderTest.java
@@ -30,83 +30,135 @@ import edu.jas.poly.GenPolynomialRing;
 
 public class ExtensionFieldBuilderTest extends TestCase {
 
-/**
- * main.
- */
-   public static void main (String[] args) {
-       BasicConfigurator.configure();
-       junit.textui.TestRunner.run( suite() );
-   }
+    /**
+     * main.
+     */
+    public static void main (String[] args) {
+        BasicConfigurator.configure();
+        junit.textui.TestRunner.run( suite() );
+    }
 
-/**
- * Constructs a <CODE>ExtensionFieldBuilderTest</CODE> object.
- * @param name String.
- */
-   public ExtensionFieldBuilderTest(String name) {
-          super(name);
-   }
+    /**
+     * Constructs a <CODE>ExtensionFieldBuilderTest</CODE> object.
+     * @param name String.
+     */
+    public ExtensionFieldBuilderTest(String name) {
+        super(name);
+    }
 
-/**
- * suite.
- */ 
- public static Test suite() {
-     TestSuite suite= new TestSuite(ExtensionFieldBuilderTest.class);
-     return suite;
-   }
-
-
-   ExtensionFieldBuilder<BigRational> builder;
+    /**
+     * suite.
+     */ 
+    public static Test suite() {
+        TestSuite suite= new TestSuite(ExtensionFieldBuilderTest.class);
+        return suite;
+    }
 
 
-   protected void setUp() {
-       builder = null;
-   }
-
-   protected void tearDown() {
-       builder = null;
-       ComputerThreads.terminate();
-   }
+    ExtensionFieldBuilder builder;
 
 
-/**
- * Test constructor and toString.
- * 
- */
- public void testConstruction() {
-     builder = new ExtensionFieldBuilder<BigRational>(new BigRational(1));
-     System.out.println("builder = " + builder.toString());
+    protected void setUp() {
+        builder = null;
+    }
 
-     RingFactory fac = builder.build();
-     System.out.println("fac     = " + fac.toScript());
 
-     builder = builder.algebraicExtension("w", "w^2 - 2");
-     System.out.println("builder = " + builder.toString());
+    protected void tearDown() {
+        builder = null;
+        ComputerThreads.terminate();
+    }
 
-     fac = builder.build();
-     System.out.println("fac     = " + fac.toScript());
 
-     builder = builder.transcendentExtension("x");
-     System.out.println("builder = " + builder.toString());
+    /**
+     * Test construction Q(sqrt(2))(x)(sqrt(x)).
+     */
+    public void testConstructionF1() {
+        builder = ExtensionFieldBuilder.baseField(new BigRational(1));
+        System.out.println("builder = " + builder.toString());
 
-     fac = builder.build();
-     System.out.println("fac     = " + fac.toScript());
+        RingFactory fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
 
-     builder = builder.algebraicExtension("wx", "wx^2 - { x }");
-     System.out.println("builder = " + builder.toString());
+        builder = builder.algebraicExtension("w2", "w2^2 - 2");
+        System.out.println("builder = " + builder.toString());
 
-     fac = builder.build();
-     System.out.println("fac     = " + fac.toScript());
+        fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
 
-     RingElem elem = (RingElem)fac.random(2);
-     //System.out.println("elem     = " + elem.toScript());
-     System.out.println("elem     = " + elem);
+        builder = builder.transcendentExtension("x");
+        System.out.println("builder = " + builder.toString());
 
-     RingElem inv = (RingElem)elem.inverse();
-     //System.out.println("inv      = " + inv.toScript());
-     System.out.println("inv      = " + inv);
+        fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
 
-     RingElem a = (RingElem)elem.multiply(inv);
-     assertTrue("e / e == 1 " + a, a.isONE() );
- }
+        builder = builder.algebraicExtension("wx", "wx^2 - { x }"); // how to know number of { }
+        System.out.println("builder = " + builder.toString());
+
+        fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
+
+        int s = fac.generators().size();
+        System.out.println("gens    = " + fac.generators());
+        assertTrue("#gens == 4 " + s, s == 4 );
+
+        RingElem elem = (RingElem)fac.random(2);
+        //System.out.println("elem     = " + elem.toScript());
+        System.out.println("elem    = " + elem);
+
+        RingElem inv = (RingElem)elem.inverse();
+        //System.out.println("inv      = " + inv.toScript());
+        System.out.println("inv     = " + inv);
+
+        RingElem a = (RingElem)elem.multiply(inv);
+        assertTrue("e / e == 1 " + a, a.isONE() );
+    }
+
+
+    /**
+     * Test construction Q(x)(sqrt(2))(sqrt(x)).
+     */
+    public void testConstructionF2() {
+        builder = ExtensionFieldBuilder.baseField(new BigRational(1));
+        System.out.println("builder = " + builder.toString());
+
+        RingFactory fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
+
+        builder = builder.transcendentExtension("x");
+        System.out.println("builder = " + builder.toString());
+
+        fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
+        builder = builder.algebraicExtension("w2", "w2^2 - 2");
+        System.out.println("builder = " + builder.toString());
+
+        fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
+
+        builder = builder.algebraicExtension("wx", "wx^2 - { { x } }"); // how to know number of { }
+        System.out.println("builder = " + builder.toString());
+
+        fac = builder.build();
+        System.out.println("fac     = " + fac.toScript());
+
+        List<RingElem> gens = fac.generators();
+        int s = gens.size();
+        System.out.println("gens    = " + gens);
+        assertTrue("#gens == 4 " + s, s == 4 );
+
+        RingElem elem = (RingElem)fac.random(1);
+        if ( elem.isZERO() || elem.isONE() ) {
+            elem = gens.get(s-1);
+	}
+        //System.out.println("elem     = " + elem.toScript());
+        System.out.println("elem    = " + elem);
+
+        RingElem inv = (RingElem)elem.inverse();
+        //System.out.println("inv      = " + inv.toScript());
+        System.out.println("inv     = " + inv);
+
+        RingElem a = (RingElem)elem.multiply(inv);
+        assertTrue("e / e == 1 " + a, a.isONE() );
+    }
 
 }
