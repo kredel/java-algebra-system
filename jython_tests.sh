369ef5aca30e53bfeea3b6ea0985db255114e374
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2011-09-16 20:35:07 +0000

    update for jython 2.5
    git-svn-id: file:///home/SUBVERSION/jas/trunk@3763 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- jython_tests.sh
+++ jython_tests.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 # run most py files
 
-time jython examples/armbruster.py
-time jython examples/intprog.py
-time jython examples/intprog2.py
-time jython examples/intprog3.py
-time jython examples/intprog4.py
-time jython examples/intprog4a.py
-time jython examples/katsura.py
-time jython examples/preimage.py
-time jython examples/quantumplane.py
-time jython examples/rose.py
-time jython examples/solvablemodule.py
-time jython examples/solvmodright.py
-time jython examples/syz.py
-time jython examples/syzsolv.py
-time jython examples/syzy2.py
-time jython examples/syzy3.py
-time jython examples/trinks.py
-time jython examples/mark.py
-time jython examples/u_2_wa_1.py
-time jython examples/u_sl_2.py
-time jython examples/u_sl_2_e.py
-time jython examples/u_sl_2_f.py
-time jython examples/u_sl_2_wa_1.py
-time jython examples/u_sl_3.py
-time jython examples/u_sl_3_prod.py
-time jython examples/u_so_3.py
-time jython examples/wa_1.py
-time jython examples/wa_32.py
-time jython examples/wa_32_syz.py
-time jython examples/chebyshev.py
-time jython examples/legendre.py
-time jython examples/arith.py
-time jython examples/powerseries.py
-time jython examples/polynomial.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/armbruster.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog2.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog3.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog4.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog4a.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/katsura.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/preimage.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/quantumplane.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/rose.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/solvablemodule.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/solvmodright.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syz.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syzsolv.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syzy2.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syzy3.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/trinks.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/mark.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_2_wa_1.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_2.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_2_e.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_2_f.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_2_wa_1.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_3.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_3_prod.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_so_3.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_1.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_32.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_32_syz.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/chebyshev.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/legendre.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/arith.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/powerseries.py
+time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/polynomial.py
