e54a0a42287b4941c62afe497676e4ea7bb1bf82
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2011-09-18 12:49:21 +0000

    added jruby scripting
    git-svn-id: file:///home/SUBVERSION/jas/trunk@3769 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- jython/src/edu/jas/kern/ScriptingExample.java
+++ jython/src/edu/jas/kern/ScriptingExample.java
@@ -10,8 +10,14 @@ import javax.script.ScriptException;
 
 public class ScriptingExample {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        runJython();
+        runJruby();
+    }
+
+
+    public static void runJython() {
         ScriptEngine jas = new ScriptEngineManager().getEngineByExtension("py");
         System.out.println("JAS service discovered: " + jas);
 	if ( jas == null ) {
@@ -29,7 +35,7 @@ public class ScriptingExample {
 	    //String ex = "x = 1; print(4*2*x)";
 	    String ex = "from jas import PolyRing, ZZ; r = PolyRing(ZZ(),\"x,y,z\",PolyRing.lex); print str(r);"
                       + "[one,x,y,z] = r.gens(); p = ((x*y)+z)**33; print \"p = \" + str(p)";
-            System.out.println("ex: " + ex);
+            System.out.println("input:  " + ex);
             Object ans = jas.eval(ex);
             millis = System.currentTimeMillis() - millis;
             System.out.println("answer: " + ans);
@@ -41,5 +47,33 @@ public class ScriptingExample {
             e.printStackTrace();
         }
     }
+
+
+    public static void runJruby() {
+        ScriptEngine jas = new ScriptEngineManager().getEngineByExtension("rb");
+        System.out.println("JAS service discovered: " + jas);
+	if ( jas == null ) {
+	    System.out.println("No JAS engine found");
+	    return;
+	}
+        System.out.println("Using JAS engine: " + jas);
+        try {
+            long millis = System.currentTimeMillis();
+	    //String ex = "x = 1; print(4*2*x)";
+	    String ex = "require \"jas\"; r = PolyRing.new(ZZ(),\"x,y,z\",PolyRing.lex); puts r.to_s;"
+                      + "one,x,y,z = r.gens(); p = ((x*y)+z)**33; puts \"p = \" + p.to_s";
+            System.out.println("input:  " + ex);
+            Object ans = jas.eval(ex);
+            millis = System.currentTimeMillis() - millis;
+            System.out.println("answer: " + ans);
+            System.out.println("evaluation took " + millis);
+
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
