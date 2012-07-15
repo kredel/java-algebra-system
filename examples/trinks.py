c926503944e89a0c143e89f3239c143c12e569b3
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2012-07-15 13:04:13 +0000

    use scripting expressions
    git-svn-id: file:///home/SUBVERSION/jas/trunk@3996 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- examples/trinks.py
+++ examples/trinks.py
@@ -5,61 +5,64 @@
 
 import sys;
 
-from jas import QQ, Ring, PolyRing, Ideal
+from jas import Ring, PolyRing, Ideal, QQ, ZZ, GF, ZM, CC
 from jas import startLog, terminate
-
 
 # trinks 6/7 example
 
-#r = Ring( "Mod 19 (B,S,T,Z,P,W) L" );
-#r = Ring( "Mod 1152921504606846883 (B,S,T,Z,P,W) L" ); # 2^60-93
-#r = Ring( "Quat(B,S,T,Z,P,W) L" );
-#r = Ring( "Z(B,S,T,Z,P,W) L" );
-#r = Ring( "C(B,S,T,Z,P,W) L" );
-#r = Ring( "Rat(B,S,T,Z,P,W) L" );
+# QQ = rational numbers, ZZ = integers, CC = complex rational numbers, GF = finite field
+#r = PolyRing( GF(19),"B,S,T,Z,P,W", PolyRing.lex);
+#r = PolyRing( GF(1152921504606846883),"B,S,T,Z,P,W", PolyRing.lex); # 2^60-93
+#r = PolyRing( GF(2**60-93),"B,S,T,Z,P,W", PolyRing.lex);
+#r = PolyRing( CC(),"B,S,T,Z,P,W", PolyRing.lex);
+#r = PolyRing( ZZ(),"B,S,T,Z,P,W", PolyRing.lex); # not for parallel
 r = PolyRing( QQ(),"B,S,T,Z,P,W", PolyRing.lex);
 print "Ring: " + str(r);
 print;
 
+# sage like: with generators for the polynomial ring
+#[one,I,B,S,T,Z,P,W] = r.gens(); # is automaticaly included
+#[one,B,S,T,Z,P,W] = r.gens(); # is automaticaly included
 
-ps = """
-( 
- ( 45 P + 35 S - 165 B - 36 ), 
- ( 35 P + 40 Z + 25 T - 27 S ), 
- ( 15 W + 25 S P + 30 Z - 18 T - 165 B**2 ), 
- ( - 9 W + 15 T P + 20 S Z ), 
- ( P W + 2 T Z - 11 B**3 ), 
- ( 99 W - 11 B S + 3 B**2 ),
- ( 10000 B**2 + 6600 B + 2673 )
-) 
-""";
+f1 = 45 * P + 35 * S - 165 * B - 36;
+f2 = 35 * P + 40 * Z + 25 * T - 27 * S;
+f3 = 15 * W + 25 * S * P + 30 * Z - 18 * T - 165 * B**2;
+f4 = - 9 * W + 15 * T * P + 20 * S * Z;
+f5 = P * W + 2 * T * Z - 11 * B**3;
+f6 = 99 * W - 11 *B * S + 3 * B**2;
+f7 = 10000 * B**2 + 6600 * B + 2673;
 
-# ( 10000 B**2 + 6600 B + 2673 )
-# ( B**2 + 33/50 B + 2673/10000 )
-
-#f = Ideal( r, ps );
-#print "Ideal: " + str(f);
+F = [ f1, f2, f3, f4, f5, f6, f7 ]; # smaller, faster
+#F = [ f1, f2, f3, f4, f5, f6 ]; # bigger, needs more time
+#print "F = ", [ str(f) for f in F ];
 #print;
 
-f = r.ideal( ps );
-print "Ideal: " + str(f);
+I = r.ideal( "", list=F );
+print "Ideal: " + str(I);
 print;
 
-#startLog();
-
-rg = f.GB();
+rg = I.GB();
 #print "seq Output:", rg;
 #print;
 
-rg = f.parGB(2);
+#startLog();
+
+#rg = I.GB();
+#print "seq Output:", rg;
+#print;
+
+#sys.exit(); # if using ZZ coefficients
+
+rg = I.parGB(2);
 #print "par Output:", rg;
 #print;
 
-f.distClient(); # starts in background
-rg = f.distGB(2);
+I.distClient(); # starts in background
+rg = I.distGB(2);
 #print "dist Output:", rg;
 #print;
 
 terminate();
 sys.exit(); # required because of distClient
+
 
--- examples/trinks.rb
+++ examples/trinks.rb
@@ -9,37 +9,36 @@ require "examples/jas"
 
 # trinks 6/7 example
 
-#r = Ring.new( "Mod 19 (B,S,T,Z,P,W) L" );
-#r = Ring.new( "Mod 1152921504606846883 (B,S,T,Z,P,W) L" ); # 2^60-93
-#r = Ring.new( "Quat(B,S,T,Z,P,W) L" );
-#r = Ring.new( "Z(B,S,T,Z,P,W) L" );
-#r = Ring.new( "C(B,S,T,Z,P,W) L" );
-#r = Ring.new( "Rat(B,S,T,Z,P,W) L" );
+# QQ = rational numbers, ZZ = integers, CC = complex rational numbers, GF = finite field
+#r = PolyRing.new( GF(19),"B,S,T,Z,P,W", PolyRing.lex);
+#r = PolyRing.new( GF(1152921504606846883),"B,S,T,Z,P,W", PolyRing.lex);
+#r = PolyRing.new( GF(2**60-93),"B,S,T,Z,P,W", PolyRing.lex);
+#r = PolyRing.new( CC(),"B,S,T,Z,P,W", PolyRing.lex);
+#r = PolyRing.new( ZZ(),"B,S,T,Z,P,W", PolyRing.lex); # not for parallel
 r = PolyRing.new( QQ(),"B,S,T,Z,P,W", PolyRing.lex);
 puts "Ring: " + r.to_s;
 puts;
 
+# sage like: with generators for the polynomial ring
+one,B,S,T,Z,P,W = r.gens(); # capital letter variables not automaticaly included
+#one,I,B,S,T,Z,P,W = r.gens(); 
 
-ps = """
-( 
- ( 45 P + 35 S - 165 B - 36 ), 
- ( 35 P + 40 Z + 25 T - 27 S ), 
- ( 15 W + 25 S P + 30 Z - 18 T - 165 B**2 ), 
- ( - 9 W + 15 T P + 20 S Z ), 
- ( P W + 2 T Z - 11 B**3 ), 
- ( 99 W - 11 B S + 3 B**2 ),
- ( 10000 B**2 + 6600 B + 2673 )
-) 
-""";
+f1 = 45 * P + 35 * S - 165 * B - 36;
+f2 = 35 * P + 40 * Z + 25 * T - 27 * S;
+f3 = 15 * W + 25 * S * P + 30 * Z - 18 * T - 165 * B**2;
+f4 = - 9 * W + 15 * T * P + 20 * S * Z;
+f5 = P * W + 2 * T * Z - 11 * B**3;
+f6 = 99 * W - 11 *B * S + 3 * B**2;
+f7 = 10000 * B**2 + 6600 * B + 2673;
+#f7 = B**2 + 33/50 * B + 2673/10000; # fractions work with ruby
+#puts "f1 = " + f1.to_s;
 
-# ( 10000 B**2 + 6600 B + 2673 )
-# ( B**2 + 33/50 B + 2673/10000 )
+F = [ f1, f2, f3, f4, f5, f6, f7 ]; # smaller, faster
+#F = [ f1, f2, f3, f4, f5, f6 ]; # bigger, needs more time
+puts "F = " + F.map { |f| f.to_s }.join(",");
+puts
 
-#f = Ideal.new( r, ps );
-#puts "Ideal: " + str(f);
-#puts;
-
-f = r.ideal( ps );
+f = r.ideal( "", F );
 puts "Ideal: " + f.to_s;
 puts;
 
@@ -48,6 +47,8 @@ puts;
 rg = f.GB();
 #puts "seq Output:", rg;
 #puts;
+
+#exit!(0); # if using ZZ coefficients
 
 rg = f.parGB(2);
 #puts "par Output:", rg;
@@ -59,6 +60,5 @@ rg = f.distGB(2);
 #puts;
 
 terminate();
-
 exit!(0); # required because of distClient, ! for jirb 
 
