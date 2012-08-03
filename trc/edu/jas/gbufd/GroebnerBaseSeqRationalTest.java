4d5a4d01985cc35948128c6ba793626ef80b135f
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2012-08-03 10:25:34 +0000

    compare test
    git-svn-id: file:///home/SUBVERSION/jas/trunk@4086 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- trc/edu/jas/gbufd/GroebnerBaseSeqRationalTest.java
+++ trc/edu/jas/gbufd/GroebnerBaseSeqRationalTest.java
@@ -5,21 +5,22 @@
 package edu.jas.gbufd;
 
 
-import java.util.List;
-import java.util.ArrayList;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import org.apache.log4j.BasicConfigurator;
-//import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
 
 import edu.jas.arith.BigRational;
 import edu.jas.gb.GroebnerBaseAbstract;
+import edu.jas.gb.GroebnerBaseSeq;
 import edu.jas.poly.GenPolynomial;
 import edu.jas.poly.GenPolynomialRing;
 import edu.jas.poly.GenPolynomialTokenizer;
@@ -33,14 +34,16 @@ import edu.jas.poly.PolynomialList;
 
 public class GroebnerBaseSeqRationalTest extends TestCase {
 
-    //private static final Logger logger = Logger.getLogger(GroebnerBaseSeqTest.class);
+
+    private static final Logger logger = Logger.getLogger(GroebnerBaseSeqRationalTest.class);
+
 
     /**
      * main
      */
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         BasicConfigurator.configure();
-        junit.textui.TestRunner.run( suite() );
+        junit.textui.TestRunner.run(suite());
     }
 
 
@@ -55,42 +58,68 @@ public class GroebnerBaseSeqRationalTest extends TestCase {
 
     /**
      * suite.
-     */ 
+     */
     public static Test suite() {
-        TestSuite suite= new TestSuite(GroebnerBaseSeqRationalTest.class);
+        TestSuite suite = new TestSuite(GroebnerBaseSeqRationalTest.class);
         return suite;
     }
 
 
     GenPolynomialRing<BigRational> fac;
 
+
     List<GenPolynomial<BigRational>> L;
+
+
     PolynomialList<BigRational> F;
+
+
     List<GenPolynomial<BigRational>> G;
+
 
     GroebnerBaseAbstract<BigRational> bb;
 
+
     GenPolynomial<BigRational> a;
+
+
     GenPolynomial<BigRational> b;
+
+
     GenPolynomial<BigRational> c;
+
+
     GenPolynomial<BigRational> d;
+
+
     GenPolynomial<BigRational> e;
 
+
     int rl = 4; //4; //3; 
+
+
     int kl = 7; // 10
+
+
     int ll = 7;
+
+
     int el = 3; // 4
+
+
     float q = 0.2f; //0.4f
 
 
+    @Override
     protected void setUp() {
         BigRational coeff = new BigRational(9);
-        fac = new GenPolynomialRing<BigRational>(coeff,rl);
+        fac = new GenPolynomialRing<BigRational>(coeff, rl);
         a = b = c = d = e = null;
         bb = new GroebnerBaseSeqRational<BigRational>();
     }
 
 
+    @Override
     protected void tearDown() {
         a = b = c = d = e = null;
         fac = null;
@@ -104,87 +133,130 @@ public class GroebnerBaseSeqRationalTest extends TestCase {
     public void testSequentialGBase() {
         L = new ArrayList<GenPolynomial<BigRational>>();
 
-        a = fac.random(kl, ll, el, q );
-        b = fac.random(kl, ll, el, q );
-        c = fac.random(kl, ll, el, q );
-        d = fac.random(kl, ll, el, q );
+        a = fac.random(kl, ll, el, q);
+        b = fac.random(kl, ll, el, q);
+        c = fac.random(kl, ll, el, q);
+        d = fac.random(kl, ll, el, q);
         e = d; //fac.random(kl, ll, el, q );
 
-        if ( a.isZERO() || b.isZERO() || c.isZERO() || d.isZERO() ) {
+        if (a.isZERO() || b.isZERO() || c.isZERO() || d.isZERO()) {
             return;
         }
 
-        assertTrue("not isZERO( a )", !a.isZERO() );
+        assertTrue("not isZERO( a )", !a.isZERO());
         L.add(a);
 
-        L = bb.GB( L );
-        assertTrue("isGB( { a } )", bb.isGB(L) );
+        L = bb.GB(L);
+        assertTrue("isGB( { a } )", bb.isGB(L));
+        assertTrue("isMinimalGB( { a } )", bb.isMinimalGB(L));
 
-        assertTrue("not isZERO( b )", !b.isZERO() );
+        assertTrue("not isZERO( b )", !b.isZERO());
         L.add(b);
         //System.out.println("L = " + L.size() );
 
-        L = bb.GB( L );
-        assertTrue("isGB( { a, b } )", bb.isGB(L) );
+        L = bb.GB(L);
+        assertTrue("isGB( { a, b } )", bb.isGB(L));
+        assertTrue("isMinimalGB( { a, b } )", bb.isMinimalGB(L));
 
-        assertTrue("not isZERO( c )", !c.isZERO() );
+        assertTrue("not isZERO( c )", !c.isZERO());
         L.add(c);
 
-        L = bb.GB( L );
-        assertTrue("isGB( { a, b, c } )", bb.isGB(L) );
+        L = bb.GB(L);
+        assertTrue("isGB( { a, b, c } )", bb.isGB(L));
+        assertTrue("isMinimalGB( { a, b, c } )", bb.isMinimalGB(L));
 
-        assertTrue("not isZERO( d )", !d.isZERO() );
+        assertTrue("not isZERO( d )", !d.isZERO());
         L.add(d);
 
-        L = bb.GB( L );
-        assertTrue("isGB( { a, b, c, d } )", bb.isGB(L) );
+        L = bb.GB(L);
+        assertTrue("isGB( { a, b, c, d } )", bb.isGB(L));
+        assertTrue("isMinimalGB( { a, b, c, d } )", bb.isMinimalGB(L));
 
-        assertTrue("not isZERO( e )", !e.isZERO() );
+        assertTrue("not isZERO( e )", !e.isZERO());
         L.add(e);
 
-        L = bb.GB( L );
-        assertTrue("isGB( { a, b, c, d, e } )", bb.isGB(L) );
+        L = bb.GB(L);
+        assertTrue("isGB( { a, b, c, d, e } )", bb.isGB(L));
+        assertTrue("isMinimalGB( { a, b, c, d, e } )", bb.isMinimalGB(L));
     }
 
 
     /**
      * Test Trinks7 GBase.
      */
-    @SuppressWarnings("unchecked") 
-        public void testTrinks7GBase() {
-        String exam = "(B,S,T,Z,P,W) L "
-            + "( "  
-            + "( 45 P + 35 S - 165 B - 36 ), " 
-            + "( 35 P + 40 Z + 25 T - 27 S ), "
-            + "( 15 W + 25 S P + 30 Z - 18 T - 165 B**2 ), "
-            + "( - 9 W + 15 T P + 20 S Z ), "
-            + "( P W + 2 T Z - 11 B**3 ), "
-            + "( 99 W - 11 B S + 3 B**2 ), "
-            //+ "( B**2 + 33/50 B + 2673/10000 ) "
-            + ") ";
-        String exam2 = "(x,y,z) L "
-            + "( "
-            + "( z y**2 + 2 x + 1/2 )"
-            + "( z x**2 - y**2 - 1/2 x )"
-            + "( -z + y**2 x + 4 x**2 + 1/4 )"
-            + " )";
+    @SuppressWarnings("unchecked")
+    public void testTrinks7GBase() {
+        String exam = "(B,S,T,Z,P,W) L " + "( " + "( 45 P + 35 S - 165 B - 36 ), "
+                        + "( 35 P + 40 Z + 25 T - 27 S ), " + "( 15 W + 25 S P + 30 Z - 18 T - 165 B**2 ), "
+                        + "( - 9 W + 15 T P + 20 S Z ), " + "( P W + 2 T Z - 11 B**3 ), "
+                        + "( 99 W - 11 B S + 3 B**2 ), " + "( B**2 + 33/50 B + 2673/10000 ) " + ") ";
+        String exam2 = "(x,y,z) L " + "( " + "( z y**2 + 2 x + 1/2 )" + "( z x**2 - y**2 - 1/2 x )"
+                        + "( -z + y**2 x + 4 x**2 + 1/4 )" + " )";
 
-        Reader source = new StringReader( exam );
-        GenPolynomialTokenizer parser
-            = new GenPolynomialTokenizer( source );
+        Reader source = new StringReader(exam);
+        GenPolynomialTokenizer parser = new GenPolynomialTokenizer(source);
         try {
             F = (PolynomialList<BigRational>) parser.nextPolynomialSet();
-        } catch(ClassCastException e) {
-            fail(""+e);
-        } catch(IOException e) {
-            fail(""+e);
+        } catch (ClassCastException e) {
+            fail("" + e);
+        } catch (IOException e) {
+            fail("" + e);
         }
         //System.out.println("F = " + F);
 
         G = bb.GB(F.list);
-        assertTrue("isGB( GB(Trinks7) )", bb.isGB(G) );
-        assertTrue("isMinimalGB( GB(Trinks7) )", bb.isMinimalGB(G) );
-        assertEquals("#GB(Trinks7) == 6", 6, G.size() );
+        assertTrue("isGB( GB(Trinks7) )", bb.isGB(G));
+        assertTrue("isMinimalGB( GB(Trinks7) )", bb.isMinimalGB(G));
+        assertEquals("#GB(Trinks7) == 6", 6, G.size());
+        //PolynomialList<BigRational> trinks = new PolynomialList<BigRational>(F.ring,G);
+        //System.out.println("G = " + trinks);
+    }
+
+
+    /**
+     * Test Trinks7 compare GBase.
+     */
+    @SuppressWarnings("unchecked")
+    public void testTrinks7CompareGBase() {
+        String exam = "(B,S,T,Z,P,W) L " + "( " + "( 45 P + 35 S - 165 B - 36 ), "
+                        + "( 35 P + 40 Z + 25 T - 27 S ), " + "( 15 W + 25 S P + 30 Z - 18 T - 165 B**2 ), "
+                        + "( - 9 W + 15 T P + 20 S Z ), " + "( P W + 2 T Z - 11 B**3 ), "
+                        + "( 99 W - 11 B S + 3 B**2 ), "
+                        //+ "( B**2 + 33/50 B + 2673/10000 ) "
+                        + ") ";
+
+        Reader source = new StringReader(exam);
+        GenPolynomialTokenizer parser = new GenPolynomialTokenizer(source);
+        try {
+            F = (PolynomialList<BigRational>) parser.nextPolynomialSet();
+        } catch (ClassCastException e) {
+            fail("" + e);
+        } catch (IOException e) {
+            fail("" + e);
+        }
+        //System.out.println("F = " + F);
+
+        long i = System.currentTimeMillis();
+        G = bb.GB(F.list);
+        i = System.currentTimeMillis() - i;
+        assertTrue("isGB( GB(Trinks7) )", bb.isGB(G));
+        assertTrue("isMinimalGB( GB(Trinks7) )", bb.isMinimalGB(G));
+        assertEquals("#GB(Trinks7) == 6", 6, G.size());
+
+        GroebnerBaseAbstract<BigRational> bbr = new GroebnerBaseSeq<BigRational>();
+        List<GenPolynomial<BigRational>> Gr;
+        long r = System.currentTimeMillis();
+        Gr = bbr.GB(F.list);
+        r = System.currentTimeMillis() - r;
+        assertTrue("isGB( GB(Trinks7) )", bbr.isGB(Gr));
+        assertTrue("isMinimalGB( GB(Trinks7) )", bbr.isMinimalGB(Gr));
+        assertEquals("#GB(Trinks7) == 6", 6, Gr.size());
+
+        if (logger.isInfoEnabled()) {
+            logger.info("i = " + i + ", r = " + r);
+        }
+        assertEquals("GB_r == GB_i", G, Gr);
+
         //PolynomialList<BigRational> trinks = new PolynomialList<BigRational>(F.ring,G);
         //System.out.println("G = " + trinks);
     }
