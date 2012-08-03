d6e1a4d0c50b3be99603446f1832592471201a1a
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2012-08-03 10:26:37 +0000

    cosmetic via eclpise
    git-svn-id: file:///home/SUBVERSION/jas/trunk@4087 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- src/edu/jas/gbufd/GroebnerBaseSeqRational.java
+++ src/edu/jas/gbufd/GroebnerBaseSeqRational.java
@@ -4,35 +4,34 @@
 
 package edu.jas.gbufd;
 
+
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.ListIterator;
 
 import org.apache.log4j.Logger;
 
-//import edu.jas.structure.RingElem;
 import edu.jas.arith.BigInteger;
 import edu.jas.arith.BigRational;
+import edu.jas.gb.GroebnerBaseAbstract;
+import edu.jas.gb.GroebnerBaseSeq;
 import edu.jas.poly.GenPolynomial;
 import edu.jas.poly.GenPolynomialRing;
 import edu.jas.poly.PolyUtil;
-import edu.jas.gb.PairList;
-import edu.jas.gb.Reduction;
-import edu.jas.gb.GroebnerBaseSeq;
-import edu.jas.gb.GroebnerBaseAbstract;
 
 
 /**
- * Groebner Base sequential algorithm for rational coefficients, fraction free computation.
- * Implements Groebner bases.
+ * Groebner Base sequential algorithm for rational coefficients, fraction free
+ * computation. Implements Groebner bases.
  * @param <BigRational> coefficient type
  * @author Heinz Kredel
  */
 
-public class GroebnerBaseSeqRational<C extends BigRational> 
-    extends GroebnerBaseSeq<BigRational>  {
+public class GroebnerBaseSeqRational<C extends BigRational> extends GroebnerBaseSeq<BigRational> {
+
 
     private static final Logger logger = Logger.getLogger(GroebnerBaseSeqRational.class);
+
+
     private final boolean debug = logger.isDebugEnabled();
 
 
@@ -54,26 +53,25 @@ public class GroebnerBaseSeqRational<C extends BigRational>
      * @param F polynomial list.
      * @return GB(F) a Groebner base of F.
      */
-    public List<GenPolynomial<BigRational>> 
-        GB( int modv, 
-            List<GenPolynomial<BigRational>> F ) {
+    @Override
+    public List<GenPolynomial<BigRational>> GB(int modv, List<GenPolynomial<BigRational>> F) {
         List<GenPolynomial<BigRational>> G = F;
-        if ( F == null || F.isEmpty() ) {
+        if (F == null || F.isEmpty()) {
             return G;
         }
         GenPolynomialRing<BigRational> rring = F.get(0).ring;
         BigInteger cf = new BigInteger();
-        GenPolynomialRing<BigInteger> iring = new GenPolynomialRing<BigInteger>(cf,rring);
-        List<GenPolynomial<BigInteger>> Fi = PolyUtil.integerFromRationalCoefficients(iring,F);
+        GenPolynomialRing<BigInteger> iring = new GenPolynomialRing<BigInteger>(cf, rring);
+        List<GenPolynomial<BigInteger>> Fi = PolyUtil.integerFromRationalCoefficients(iring, F);
         //System.out.println("Fi = " + Fi);
         logger.info("#Fi = " + Fi.size());
 
-        List<GenPolynomial<BigInteger>> Gi = bba.GB(modv,Fi);
+        List<GenPolynomial<BigInteger>> Gi = bba.GB(modv, Fi);
         //System.out.println("Gi = " + Gi);
         logger.info("#Gi = " + Gi.size());
 
-        G = PolyUtil.<BigRational>fromIntegerCoefficients(rring,Gi);
-        G = PolyUtil.<BigRational>monic(G);
+        G = PolyUtil.<BigRational> fromIntegerCoefficients(rring, Gi);
+        G = PolyUtil.<BigRational> monic(G);
         return G;
     }
 
@@ -83,38 +81,37 @@ public class GroebnerBaseSeqRational<C extends BigRational>
      * @param Gp a Groebner base.
      * @return a reduced Groebner base of Gp.
      */
-    public List<GenPolynomial<BigRational>> 
-        minimalGB(List<GenPolynomial<BigRational>> Gp) {  
-        if ( Gp == null || Gp.size() <= 1 ) {
+    @Override
+    public List<GenPolynomial<BigRational>> minimalGB(List<GenPolynomial<BigRational>> Gp) {
+        if (Gp == null || Gp.size() <= 1) {
             return Gp;
         }
         // remove zero polynomials
-        List<GenPolynomial<BigRational>> G
-            = new ArrayList<GenPolynomial<BigRational>>( Gp.size() );
-        for ( GenPolynomial<BigRational> a : Gp ) { 
-            if ( a != null && !a.isZERO() ) { // always true in GB()
+        List<GenPolynomial<BigRational>> G = new ArrayList<GenPolynomial<BigRational>>(Gp.size());
+        for (GenPolynomial<BigRational> a : Gp) {
+            if (a != null && !a.isZERO()) { // always true in GB()
                 // already positive a = a.abs();
-                G.add( a );
+                G.add(a);
             }
         }
-        if ( G.size() <= 1 ) {
+        if (G.size() <= 1) {
             return G;
         }
         // remove top reducible polynomials
         GenPolynomial<BigRational> a;
         List<GenPolynomial<BigRational>> F;
-        F = new ArrayList<GenPolynomial<BigRational>>( G.size() );
-        while ( G.size() > 0 ) {
+        F = new ArrayList<GenPolynomial<BigRational>>(G.size());
+        while (G.size() > 0) {
             a = G.remove(0);
-            if ( red.isTopReducible(G,a) || red.isTopReducible(F,a) ) {
+            if (red.isTopReducible(G, a) || red.isTopReducible(F, a)) {
                 // drop polynomial 
-                if ( debug ) {
+                if (debug) {
                     System.out.println("dropped " + a);
                     List<GenPolynomial<BigRational>> ff;
-                    ff = new ArrayList<GenPolynomial<BigRational>>( G );
+                    ff = new ArrayList<GenPolynomial<BigRational>>(G);
                     ff.addAll(F);
-                    a = red.normalform( ff, a );
-                    if ( !a.isZERO() ) {
+                    a = red.normalform(ff, a);
+                    if (!a.isZERO()) {
                         System.out.println("error, nf(a) " + a);
                     }
                 }
@@ -123,22 +120,22 @@ public class GroebnerBaseSeqRational<C extends BigRational>
             }
         }
         G = F;
-        if ( G.size() <= 1 ) {
+        if (G.size() <= 1) {
             return G;
         }
 
         // reduce remaining polynomials
         GenPolynomialRing<BigRational> rring = G.get(0).ring;
         BigInteger cf = new BigInteger();
-        GenPolynomialRing<BigInteger> iring = new GenPolynomialRing<BigInteger>(cf,rring);
-        List<GenPolynomial<BigInteger>> Fi = PolyUtil.integerFromRationalCoefficients(iring,F);
+        GenPolynomialRing<BigInteger> iring = new GenPolynomialRing<BigInteger>(cf, rring);
+        List<GenPolynomial<BigInteger>> Fi = PolyUtil.integerFromRationalCoefficients(iring, F);
         logger.info("#Fi = " + Fi.size());
 
         List<GenPolynomial<BigInteger>> Gi = bba.minimalGB(Fi);
         logger.info("#Gi = " + Gi.size());
 
-        G = PolyUtil.<BigRational>fromIntegerCoefficients(rring,Gi);
-        G = PolyUtil.<BigRational>monic(G);
+        G = PolyUtil.<BigRational> fromIntegerCoefficients(rring, Gi);
+        G = PolyUtil.<BigRational> monic(G);
         return G;
     }
 
