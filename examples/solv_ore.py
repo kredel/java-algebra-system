e3d68325e1b809288b26d77312a3711ae9c2c198
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2013-04-26 15:00:36 +0000

    new examples
    git-svn-id: file:///home/SUBVERSION/jas/trunk@4375 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ examples/solv_ore.py
@@ -0,0 +1,62 @@
+#
+# jython examples for jas.
+# $Id$
+#
+
+from java.lang import System
+from java.lang import Integer
+
+from jas import SolvableRing, SolvPolyRing, PolyRing, SolvableIdeal
+from jas import QQ, startLog
+
+# Ore extension solvable polynomial example, Gomez-Torrecillas, 2003
+
+p = PolyRing(QQ(),"x,y,z,t");
+#is automatic: [one,x,y,z,t] = p.gens();
+
+relations = [z, y,  y * z + x,
+             t, y,  y * t  + y,
+             t, z,  z * t - z
+            ];
+
+print "relations: = " + str([ str(f) for f in relations ]);
+print;
+
+
+rp = SolvPolyRing(QQ(), "x,y,z,t", PolyRing.lex, relations);
+print "SolvPolyRing: " + str(rp);
+print;
+print "gens =", [ str(f) for f in rp.gens() ];
+
+
+f1 = x**2 + y**2 + z**2 + t**2 + 1;
+
+print "f1 = ", f1;
+
+F = [ f1 ];
+print "F =", [ str(f) for f in F ];
+print
+
+I = rp.ideal( list=F );
+print "SolvableIdeal: " + str(I);
+print;
+
+rgl = I.leftGB();
+print "seq left GB:" + str(rgl);
+print "isLeftGB: " + str(rgl.isLeftGB());
+print;
+
+rgr = I.rightGB();
+print "seq right GB:" + str(rgr);
+print "isRightGB: " + str(rgr.isRightGB());
+print;
+
+#startLog();
+
+rgt = I.twosidedGB();
+print "seq twosided GB:" + str(rgt);
+print "isTwosidedGB: " + str(rgt.isTwosidedGB());
+print;
+
+#exit(0);
+
---
+++ examples/solv_ore.rb
@@ -0,0 +1,59 @@
+#
+# jruby examples for jas.
+# $Id$
+#
+
+require "examples/jas"
+
+# Ore extension solvable polynomial example, Gomez-Torrecillas, 2003
+
+p = PolyRing.new(QQ(),"x,y,z,t");
+#is automatic: [one,x,y,z,t] = p.gens();
+
+relations = [z, y,  y * z + x,
+             t, y,  y * t  + y,
+             t, z,  z * t - z
+            ];
+
+puts "relations: = [" + relations.join(", ") { |r| r.to_s } + "]";
+puts;
+
+rp = SolvPolyRing.new(QQ(), "x,y,z,t", PolyRing.lex, relations);
+puts "SolvPolyRing: " + str(rp);
+puts;
+
+puts "gens =" + rp.gens().join(", ") { |r| r.to_s };
+#is automatic: one,x,y,z,t = rp.gens();
+
+f1 = x**2 + y**2 + z**2 + t**2 + 1;
+
+puts "f1 = " +str(f1);
+
+ff = [ f1 ];
+puts "ff = [" + ff.join(", ") { |r| r.to_s } + "]";
+puts
+
+ii = rp.ideal( "", ff );
+puts "SolvableIdeal: " + str(ii);
+puts;
+
+
+rgl = ii.leftGB();
+puts "seq left GB:" + str(rgl);
+puts "isLeftGB: " + str(rgl.isLeftGB());
+puts;
+
+rgr = ii.rightGB();
+puts "seq right GB:" + str(rgr);
+puts "isRightGB: " + str(rgr.isRightGB());
+puts;
+
+startLog();
+
+rgt = ii.twosidedGB();
+puts "seq twosided GB:" + str(rgt);
+puts "isTwosidedGB: " + str(rgt.isTwosidedGB());
+puts;
+
+#terminate();
+
