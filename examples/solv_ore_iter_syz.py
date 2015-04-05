b0c4f9a49ab918af92811bd6992cf78231459b34
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2015-04-05 19:07:31 +0000

    syzygy examples
    git-svn-id: file:///home/SUBVERSION/jas/trunk@5211 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ examples/solv_ore_iter_syz.py
@@ -0,0 +1,142 @@
+#
+# jruby examples for jas.
+# $Id$
+#
+
+import sys;
+
+from jas import SolvableRing, SolvPolyRing, PolyRing, SolvableIdeal
+from jas import QQ, ZZ, GF, SRF, startLog, terminate 
+
+
+# Ore extension solvable polynomial example, Gomez-Torrecillas, 2003
+
+pcz = PolyRing(QQ(),"x,y,z,t", PolyRing.lex);
+#is automatic: [one,x,y,z,t] = p.gens();
+
+zrelations = [z, y,  y * z + x,
+              t, y,  y * t + y,
+              t, z,  z * t - z
+             ];
+
+print "zrelations: = " + str( [ str(r) for r in zrelations ] );
+print;
+
+pz = SolvPolyRing(QQ(), "x,y,z,t", PolyRing.lex, zrelations);
+print "SolvPolyRing: " + str(pz);
+print;
+
+pzq = SRF(pz);
+print "SolvableQuotientRing: " + str(pzq.ring.toScript); # + ", assoz: " + str(pzq::ring.isAssociative);
+#print "gens =" + str( [ str(r) for r in pzq.gens() ] );
+print;
+
+pct = PolyRing(pzq,"u,v,w", PolyRing.lex);
+#is automatic: [one,x,y,z,t,u,v,w] = p.gens();
+print "tgens = " + str( [ str(r) for r in pct.gens() ] );
+print;
+
+relations = [#w, v,  v * w - u,
+             v, u,  v * u + x,
+             w, y,  y * w + y,
+             w, z,  z * w - z
+            ];
+
+print "relations: = " + str( [ str(r) for r in relations ] );
+print;
+
+#startLog();
+
+pt = SolvPolyRing(pzq, "u,v,w", PolyRing.lex, relations);
+print "SolvPolyRing: " + str(pt); # + ", is assoz: " + str(pt.ring.isAssociative);
+print;
+
+print "gens = " + str( [ str(r) for r in pt.gens() ] );
+#is automatic: one,x,y,z,r,s,t = rp.gens();
+
+#exit(0);
+
+#f1 = u**2 + v**2 + w**2;
+#f1 = v;
+f1 = w**2 - t;
+#print "f1 = " + str(f1);
+
+#f2 = t + u - x**2;
+#f2 = f1 * (t - x**2);
+#f2 = v**2 - v;
+#f2 = v**2 - y;
+#f2 = v**2 - z;
+f2 = v**2 - t;
+#print "f2 = " + str(f2);
+
+#f3 = (v - u)*f1;
+#f3 = f1*(v - u);
+f3 = v*f1*u;
+#print "f3 = " + str(f3);
+
+#ff = [ f3, f3 ];
+#ff = [ f1, f2, f3 ];
+#ff = [ f1 ];
+#ff = [ v**2 ]; #isTwosided ideal
+#ff = [ u**2 ]; #isTwosided ideal
+#ff = [ w**2 ]; # twosided == 1
+#ff = [ (v-u)**7 ]; #isTwosided ideal
+#ff = [ (v-u)**5 ]; #isTwosided ideal
+ff = [ (v-u)**3 ]; #isTwosided ideal
+print "ff = " + str( [ str(r) for r in ff ] );
+print
+
+ii = pt.ideal( "", ff );
+print "SolvableIdeal: " + str(ii);
+print;
+
+#syl = ii.leftSyzygy().leftGB();
+#print "left syzygy: " + str(syl);
+#print;
+
+#syr = ii.rightSyzygy().rightGB();
+#print "right syzygy: " + str(syr);
+#print;
+
+
+#exit(0);
+#startLog();
+
+rgl = ii.leftGB();
+print "seq left GB: " + str(rgl);
+print "isLeftGB: " + str(rgl.isLeftGB());
+print;
+
+rgr = ii.rightGB();
+print "seq right GB: " + str(rgr);
+print "isRightGB: " + str(rgr.isRightGB());
+print;
+
+#startLog();
+
+rgt = ii.twosidedGB();
+print "seq twosided GB: " + str(rgt);
+print "isTwosidedGB: " + str(rgt.isTwosidedGB());
+print;
+
+exit(0);
+#startLog();
+
+#rgi = rgl.intersect(rgt);
+#print "leftGB intersect twosidedGB: " + str(rgi);
+#print;
+#rgi = rgl.intersect(rgr);
+#print "leftGB intersect rightGB: " + str(rgi);
+#print;
+
+#exit(0);
+
+syl = rgt.leftSyzygy().leftGB();
+print "left syzygy: " + str(syl);
+print;
+
+syr = rgt.rightSyzygy().rightGB();
+print "right syzygy: " + str(syr);
+print;
+
+#terminate();
---
+++ examples/solv_ore_iter_syz.rb
@@ -0,0 +1,139 @@
+#
+# jruby examples for jas.
+# $Id$
+#
+
+require "examples/jas"
+
+# Ore extension solvable polynomial example, Gomez-Torrecillas, 2003
+
+
+pcz = PolyRing.new(QQ(),"x,y,z,t", PolyRing.lex);
+#is automatic: [one,x,y,z,t] = p.gens();
+
+zrelations = [z, y,  y * z + x,
+              t, y,  y * t + y,
+              t, z,  z * t - z
+             ];
+
+puts "zrelations: = [" + zrelations.join(", ") { |r| r.to_s } + "]";
+puts;
+
+pz = SolvPolyRing.new(QQ(), "x,y,z,t", PolyRing.lex, zrelations);
+puts "SolvPolyRing: " + str(pz);
+puts;
+
+pzq = SRF(pz);
+puts "SolvableQuotientRing: " + str(pzq::ring.toScript); # + ", assoz: " + str(pzq::ring.isAssociative);
+#puts "gens =" + pzq::ring.generators().join(", ") { |r| r.to_s };
+puts;
+
+pct = PolyRing.new(pzq,"u,v,w", PolyRing.lex);
+#is automatic: [one,x,y,z,t,u,v,w] = p.gens();
+puts "tgens = " + pct.gens().join(", ") { |r| r.to_s };
+puts;
+
+relations = [#w, v,  v * w - u,
+             v, u,  v * u + x,
+             w, y,  y * w + y,
+             w, z,  z * w - z
+            ];
+
+puts "relations: = [" + relations.join(", ") { |r| r.to_s } + "]";
+puts;
+
+#startLog();
+
+pt = SolvPolyRing.new(pzq, "u,v,w", PolyRing.lex, relations);
+puts "SolvPolyRing: " + str(pt) + ", is assoz: " + str(pt.ring.isAssociative);
+puts;
+
+puts "gens = " + pt.gens().join(", ") { |r| r.to_s };
+#is automatic: one,x,y,z,r,s,t = rp.gens();
+
+#exit(0);
+
+#f1 = u**2 + v**2 + w**2;
+#f1 = v;
+f1 = w**2 - t;
+#puts "f1 = " + str(f1);
+
+#f2 = t + u - x**2;
+#f2 = f1 * (t - x**2);
+#f2 = v**2 - v;
+#f2 = v**2 - y;
+#f2 = v**2 - z;
+f2 = v**2 - t;
+#puts "f2 = " + str(f2);
+
+#f3 = (v - u)*f1;
+#f3 = f1*(v - u);
+f3 = v*f1*u;
+#puts "f3 = " + str(f3);
+
+#ff = [ f3, f3 ];
+#ff = [ f1, f2, f3 ];
+#ff = [ f1 ];
+#ff = [ v**2 ]; #isTwosided ideal
+#ff = [ u**2 ]; #isTwosided ideal
+#ff = [ w**2 ]; # twosided == 1
+#ff = [ (v-u)**7 ]; #isTwosided ideal
+#ff = [ (v-u)**5 ]; #isTwosided ideal
+ff = [ (v-u)**3 ]; #isTwosided ideal
+puts "ff = [" + ff.join(", ") { |r| r.to_s } + "]";
+puts
+
+ii = pt.ideal( "", ff );
+puts "SolvableIdeal: " + str(ii);
+puts;
+
+#syl = ii.leftSyzygy().leftGB();
+#puts "left syzygy: " + str(syl);
+#puts;
+
+#syr = ii.rightSyzygy().rightGB();
+#puts "right syzygy: " + str(syr);
+#puts;
+
+
+#exit(0);
+#startLog();
+
+rgl = ii.leftGB();
+puts "seq left GB: " + str(rgl);
+puts "isLeftGB: " + str(rgl.isLeftGB());
+puts;
+
+rgr = ii.rightGB();
+puts "seq right GB: " + str(rgr);
+puts "isRightGB: " + str(rgr.isRightGB());
+puts;
+
+#startLog();
+
+rgt = ii.twosidedGB();
+puts "seq twosided GB: " + str(rgt);
+puts "isTwosidedGB: " + str(rgt.isTwosidedGB());
+puts;
+
+exit(0);
+#startLog();
+
+#rgi = rgl.intersect(rgt);
+#puts "leftGB intersect twosidedGB: " + str(rgi);
+#puts;
+#rgi = rgl.intersect(rgr);
+#puts "leftGB intersect rightGB: " + str(rgi);
+#puts;
+
+#exit(0);
+
+syl = rgt.leftSyzygy().leftGB();
+puts "left syzygy: " + str(syl);
+puts;
+
+syr = rgt.rightSyzygy().rightGB();
+puts "right syzygy: " + str(syr);
+puts;
+
+#terminate();
