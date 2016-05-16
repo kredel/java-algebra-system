7e0795ab9584f59cfc6e494ea8798e407592c698
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2016-05-16 21:54:07 +0000

    update properties
    git-svn-id: file:///home/SUBVERSION/jas/trunk@5515 eafcb37d-f50b-0410-b8f7-d4cb9effa109

---
+++ examples/ncmat.rb
@@ -0,0 +1,33 @@
+#
+# jruby examples for jas.
+# $Id$
+#
+
+require "examples/jas"
+
+# matrix and polynomial examples: 
+# conditions for (non) commuting matrices
+
+r = PolyRing.new(ZZ(), "a,b,c,d,e,f,g,h")
+puts "r = " + r.to_s
+puts
+
+x = Matrix[[a,b],[c,d]]
+y = Matrix[[e,f],[g,h]]
+puts "x = " + x.to_s + ", y = " + y.to_s
+puts
+
+com = x*y - y*x
+puts "commutator = " + com.to_s
+puts
+
+ff = r.ideal("", [ com[0,0], com[0,1], com[1,0], com[1,1] ] )
+puts "ff = " + ff.to_s
+puts
+
+gg = ff.GB();
+puts "gg = " + gg.to_s
+puts
+
+#startLog();
+#terminate();
