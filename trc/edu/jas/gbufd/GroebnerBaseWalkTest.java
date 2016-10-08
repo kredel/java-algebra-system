60c67f3d1239492fc58d56a96c00462f41ee7f2b
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2016-10-08 19:38:15 +0000

    new test case
    git-svn-id: file:///home/SUBVERSION/jas/trunk@5613 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- trc/edu/jas/gbufd/GroebnerBaseWalkTest.java
+++ trc/edu/jas/gbufd/GroebnerBaseWalkTest.java
@@ -187,15 +187,21 @@ public class GroebnerBaseWalkTest extends TestCase {
         }
         //System.out.println("F = " + F);
 
+        long t = System.currentTimeMillis();
         G = bb.GB(F.list);
+        t = System.currentTimeMillis() - t;
         PolynomialList<BigRational> seq = new PolynomialList<BigRational>(F.ring, G);
         //System.out.println("seq G = " + seq);
         assertTrue("isGB( GB(Trinks) )", bb.isGB(G));
         assertTrue("isMinimalGB( GB(Trinks) )", bb.isMinimalGB(G));
         assertEquals("#GB(Trinks) == 6", 6, G.size());
+        long w = System.currentTimeMillis();
         Gp = bbw.GB(F.list);
+        w = System.currentTimeMillis() - w;
         PolynomialList<BigRational> tri = new PolynomialList<BigRational>(F.ring, Gp);
         //System.out.println("walk G = " + tri);
+        //System.out.println("lex = " + t + ", walk = " + w + " in milliseconds");
+        assertTrue("findbugs ", t+w >= 0L);
         assertTrue("isGB( GB(Trinks) )", bb.isGB(Gp));
         assertTrue("isMinimalGB( GB(Trinks) )", bb.isMinimalGB(Gp));
         //assertEquals("#GB(Trinks) == 6", 6, Gp.size());
@@ -203,4 +209,52 @@ public class GroebnerBaseWalkTest extends TestCase {
         assertEquals("G == Gp: ", seq, tri); // ideal.equals
     }
 
+
+    /**
+     * Test ISSAC GBase. Example from the FGLM paper.
+     */
+    @SuppressWarnings({ "unchecked", "cast" })
+    public void testFGLMissacGBase() { // 
+        String exam = "Mod 32003 (w,z,y,x) L " // Mod 9223372036854775783 536870909 32003 (z,y,x,w)
+                        + "( "  
+                        + " (8*w^2 + 5*w*x - 4*w*y + 2*w*z + 3*w + 5*x^2 + 2*x*y - 7*x*z - 7*x + 7*y^2 -8*y*z - 7*y + 7*z^2 - 8*z + 8),"
+                        + "(3*w^2 - 5*w*x - 3*w*y - 6*w*z + 9*w + 4*x^2 + 2*x*y - 2*x*z + 7*x + 9*y^2 + 6*y*z + 5*y + 7*z^2 + 7*z + 5),"
+                        + "(-2*w^2 + 9*w*x + 9*w*y - 7*w*z - 4*w + 8*x^2 + 9*x*y - 3*x*z + 8*x + 6*y^2 - 7*y*z + 4*y - 6*z^2 + 8*z + 2),"
+                        + "(7*w^2 + 5*w*x + 3*w*y - 5*w*z - 5*w + 2*x^2 + 9*x*y - 7*x*z + 4*x -4*y^2 - 5*y*z + 6*y - 4*z^2 - 9*z + 2)"
+                        +" )";
+
+        Reader source = new StringReader(exam);
+        GenPolynomialTokenizer parser = new GenPolynomialTokenizer(source);
+        try {
+            F = (PolynomialList<BigRational>) parser.nextPolynomialSet();
+        } catch (ClassCastException e) {
+            fail("" + e);
+        } catch (IOException e) {
+            fail("" + e);
+        }
+        //System.out.println("F = " + F);
+
+        //G = bb.GB(F.list);
+        long t = System.currentTimeMillis();
+        G = bb.GB(F.list);
+        t = System.currentTimeMillis() - t;
+        PolynomialList<BigRational> seq = new PolynomialList<BigRational>(F.ring, G);
+        //System.out.println("seq G = " + seq);
+        assertTrue("isGB( GB() )", bb.isGB(G));
+        assertTrue("isMinimalGB( GB() )", bb.isMinimalGB(G));
+        //assertEquals("#GB(FJLT) == 2", 2, G.size());
+
+        long w = System.currentTimeMillis();
+        Gp = bbw.GB(F.list);
+        w = System.currentTimeMillis() - w;
+        PolynomialList<BigRational> fjlt = new PolynomialList<BigRational>(F.ring, Gp);
+        //System.out.println("walk G = " + fjlt);
+        //System.out.println("lex = " + t + ", walk = " + w + " in milliseconds");
+        assertTrue("findbugs ", t+w >= 0L);
+        assertTrue("isGB( GB() )", bb.isGB(Gp));
+        assertTrue("isMinimalGB( GB() )", bb.isMinimalGB(Gp));
+        //assertEquals("#GB(FJLT) == 2", 2, Gp.size());
+        //Collections.reverse(G); // now in minimal
+        assertEquals("G == Gp: ", G, Gp);
+    }
 }
