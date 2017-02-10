4a7eb62c6d1142bf6dd1cb694bdcb7eab0a00ee8
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2017-02-10 21:33:32 +0000

    fix classpath option
    git-svn-id: file:///home/SUBVERSION/jas/trunk@5730 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- jruby_tests.sh
+++ jruby_tests.sh
@@ -1,47 +1,50 @@
 #!/bin/sh
 # run some rb files
 
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/all_rings.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/all_rings.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/trinks.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/trinks.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/0dim_primary-decomp.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/0dim_primary-decomp.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/0dim_prime-decomp.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/0dim_prime-decomp.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/0dim_radical.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/0dim_radical.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/cgb_0.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/cgb_0.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/cgb_2.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/cgb_2.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/chebyshev.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/chebyshev.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/e-gb.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/e-gb.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/eliminate.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/eliminate.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_abs.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_abs.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_abs_complex.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_abs_complex.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_abs_mult.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_abs_mult.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_algeb.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/factors_algeb.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/getstart.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/getstart.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/hawes2.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/hawes2.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/module.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/module.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/polynomial.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/polynomial.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/polypower.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/polypower.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/powerseries.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/powerseries.rb
-echo jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/prime-decomp.rb
-time jruby -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/prime-decomp.rb
+#OPTS="-J-cp ../lib/log4j.jar:../lib/junit.jar:."
+OPTS="-J-cp ../lib/log4j-core-2.5.jar:../lib/log4j-api-2.5.jar:../lib/log4j-1.2-api-2.5.jar:../lib/junit.jar-4.12.jar:../lib/hamcrest-core-1.3.jar:."
+
+echo jruby $OPTS -J-verbose:gc examples/all_rings.rb
+time jruby $OPTS -J-verbose:gc examples/all_rings.rb
+echo jruby $OPTS -J-verbose:gc examples/trinks.rb
+time jruby $OPTS -J-verbose:gc examples/trinks.rb
+echo jruby $OPTS -J-verbose:gc examples/0dim_primary-decomp.rb
+time jruby $OPTS -J-verbose:gc examples/0dim_primary-decomp.rb
+echo jruby $OPTS -J-verbose:gc examples/0dim_prime-decomp.rb
+time jruby $OPTS -J-verbose:gc examples/0dim_prime-decomp.rb
+echo jruby $OPTS -J-verbose:gc examples/0dim_radical.rb
+time jruby $OPTS -J-verbose:gc examples/0dim_radical.rb
+echo jruby $OPTS -J-verbose:gc examples/cgb_0.rb
+time jruby $OPTS -J-verbose:gc examples/cgb_0.rb
+echo jruby $OPTS -J-verbose:gc examples/cgb_2.rb
+time jruby $OPTS -J-verbose:gc examples/cgb_2.rb
+echo jruby $OPTS -J-verbose:gc examples/chebyshev.rb
+time jruby $OPTS -J-verbose:gc examples/chebyshev.rb
+echo jruby $OPTS -J-verbose:gc examples/e-gb.rb
+time jruby $OPTS -J-verbose:gc examples/e-gb.rb
+echo jruby $OPTS -J-verbose:gc examples/eliminate.rb
+time jruby $OPTS -J-verbose:gc examples/eliminate.rb
+echo jruby $OPTS -J-verbose:gc examples/factors.rb
+time jruby $OPTS -J-verbose:gc examples/factors.rb
+echo jruby $OPTS -J-verbose:gc examples/factors_abs.rb
+time jruby $OPTS -J-verbose:gc examples/factors_abs.rb
+echo jruby $OPTS -J-verbose:gc examples/factors_abs_complex.rb
+time jruby $OPTS -J-verbose:gc examples/factors_abs_complex.rb
+echo jruby $OPTS -J-verbose:gc examples/factors_abs_mult.rb
+time jruby $OPTS -J-verbose:gc examples/factors_abs_mult.rb
+echo jruby $OPTS -J-verbose:gc examples/factors_algeb.rb
+time jruby $OPTS -J-verbose:gc examples/factors_algeb.rb
+echo jruby $OPTS -J-verbose:gc examples/getstart.rb
+time jruby $OPTS -J-verbose:gc examples/getstart.rb
+echo jruby $OPTS -J-verbose:gc examples/hawes2.rb
+time jruby $OPTS -J-verbose:gc examples/hawes2.rb
+echo jruby $OPTS -J-verbose:gc examples/module.rb
+time jruby $OPTS -J-verbose:gc examples/module.rb
+echo jruby $OPTS -J-verbose:gc examples/polynomial.rb
+time jruby $OPTS -J-verbose:gc examples/polynomial.rb
+echo jruby $OPTS -J-verbose:gc examples/polypower.rb
+time jruby $OPTS -J-verbose:gc examples/polypower.rb
+echo jruby $OPTS -J-verbose:gc examples/powerseries.rb
+time jruby $OPTS -J-verbose:gc examples/powerseries.rb
+echo jruby $OPTS -J-verbose:gc examples/prime-decomp.rb
+time jruby $OPTS -J-verbose:gc examples/prime-decomp.rb
--- jython_tests.sh
+++ jython_tests.sh
@@ -1,55 +1,58 @@
 #!/bin/sh
 # run some py files
 
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/all_rings.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/all_rings.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog2.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/intprog2.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/katsura.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/katsura.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/preimage.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/preimage.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/quantumplane_syz.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/quantumplane_syz.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/rose.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/rose.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/solvablemodule.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/solvablemodule.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/solvmodright.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/solvmodright.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syz.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syz.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syzsolv.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/syzsolv.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/trinks.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/trinks.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/mark.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/mark.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_2_wa_1.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_2_wa_1.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_2.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_2.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_3.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_3.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_3_prod.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_sl_3_prod.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_so_3.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/u_so_3.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_1.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_1.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_32.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_32.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_32_syz.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/wa_32_syz.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/chebyshev.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/chebyshev.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/legendre.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/legendre.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/arith.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/arith.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/powerseries.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/powerseries.py
-echo jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/polynomial.py
-time jython -J-cp ../lib/log4j.jar:../lib/junit.jar:. -J-verbose:gc examples/polynomial.py
+#OPTS="-J-cp ../lib/log4j.jar:../lib/junit.jar:."
+OPTS="-J-cp ../lib/log4j-core-2.5.jar:../lib/log4j-api-2.5.jar:../lib/log4j-1.2-api-2.5.jar:../lib/junit.jar-4.12.jar:../lib/hamcrest-core-1.3.jar:."
+
+echo jython $OPTS -J-verbose:gc examples/all_rings.py
+time jython $OPTS -J-verbose:gc examples/all_rings.py
+echo jython $OPTS -J-verbose:gc examples/intprog.py
+time jython $OPTS -J-verbose:gc examples/intprog.py
+echo jython $OPTS -J-verbose:gc examples/intprog2.py
+time jython $OPTS -J-verbose:gc examples/intprog2.py
+echo jython $OPTS -J-verbose:gc examples/katsura.py
+time jython $OPTS -J-verbose:gc examples/katsura.py
+echo jython $OPTS -J-verbose:gc examples/preimage.py
+time jython $OPTS -J-verbose:gc examples/preimage.py
+echo jython $OPTS -J-verbose:gc examples/quantumplane_syz.py
+time jython $OPTS -J-verbose:gc examples/quantumplane_syz.py
+echo jython $OPTS -J-verbose:gc examples/rose.py
+time jython $OPTS -J-verbose:gc examples/rose.py
+echo jython $OPTS -J-verbose:gc examples/solvablemodule.py
+time jython $OPTS -J-verbose:gc examples/solvablemodule.py
+echo jython $OPTS -J-verbose:gc examples/solvmodright.py
+time jython $OPTS -J-verbose:gc examples/solvmodright.py
+echo jython $OPTS -J-verbose:gc examples/syz.py
+time jython $OPTS -J-verbose:gc examples/syz.py
+echo jython $OPTS -J-verbose:gc examples/syzsolv.py
+time jython $OPTS -J-verbose:gc examples/syzsolv.py
+echo jython $OPTS -J-verbose:gc examples/trinks.py
+time jython $OPTS -J-verbose:gc examples/trinks.py
+echo jython $OPTS -J-verbose:gc examples/mark.py
+time jython $OPTS -J-verbose:gc examples/mark.py
+echo jython $OPTS -J-verbose:gc examples/u_2_wa_1.py
+time jython $OPTS -J-verbose:gc examples/u_2_wa_1.py
+echo jython $OPTS -J-verbose:gc examples/u_sl_2.py
+time jython $OPTS -J-verbose:gc examples/u_sl_2.py
+echo jython $OPTS -J-verbose:gc examples/u_sl_3.py
+time jython $OPTS -J-verbose:gc examples/u_sl_3.py
+echo jython $OPTS -J-verbose:gc examples/u_sl_3_prod.py
+time jython $OPTS -J-verbose:gc examples/u_sl_3_prod.py
+echo jython $OPTS -J-verbose:gc examples/u_so_3.py
+time jython $OPTS -J-verbose:gc examples/u_so_3.py
+echo jython $OPTS -J-verbose:gc examples/wa_1.py
+time jython $OPTS -J-verbose:gc examples/wa_1.py
+echo jython $OPTS -J-verbose:gc examples/wa_32.py
+time jython $OPTS -J-verbose:gc examples/wa_32.py
+echo jython $OPTS -J-verbose:gc examples/wa_32_syz.py
+time jython $OPTS -J-verbose:gc examples/wa_32_syz.py
+echo jython $OPTS -J-verbose:gc examples/chebyshev.py
+time jython $OPTS -J-verbose:gc examples/chebyshev.py
+echo jython $OPTS -J-verbose:gc examples/legendre.py
+time jython $OPTS -J-verbose:gc examples/legendre.py
+echo jython $OPTS -J-verbose:gc examples/arith.py
+time jython $OPTS -J-verbose:gc examples/arith.py
+echo jython $OPTS -J-verbose:gc examples/powerseries.py
+time jython $OPTS -J-verbose:gc examples/powerseries.py
+echo jython $OPTS -J-verbose:gc examples/polynomial.py
+time jython $OPTS -J-verbose:gc examples/polynomial.py
