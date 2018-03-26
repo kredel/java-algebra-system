b97ec8bd7f0ea1b389a939c646055d5f5ff309f7
kredel <kredel@eafcb37d-f50b-0410-b8f7-d4cb9effa109>
2018-03-26 10:20:59 +0000

    add exec method
    git-svn-id: file:///home/SUBVERSION/jas/trunk@5799 eafcb37d-f50b-0410-b8f7-d4cb9effa109

--- meditor/src/edu/jas/editorengine/EditorEngine.java
+++ meditor/src/edu/jas/editorengine/EditorEngine.java
@@ -4,16 +4,21 @@ import jscl.editor.Engine;
 import org.python.util.PythonInterpreter;
 
 public class EditorEngine extends Engine {
-	PythonInterpreter interp=new PythonInterpreter();
+    PythonInterpreter interp=new PythonInterpreter();
 
-	public EditorEngine() {
-                //interp.execfile(EditorEngine.class.getResourceAsStream("/jas.py"));
-	}
+    public EditorEngine() {
+        //interp.execfile(EditorEngine.class.getResourceAsStream("/jas.py"));
+    }
 
-	public String eval(String str) {
-		if(str.lastIndexOf("\n")==str.length()-1) {
-			interp.exec(str);
-			return str;
-		} else return interp.eval(str).__str__().toString();
-	}
+    public String eval(String str) {
+        if(str.lastIndexOf("\n")==str.length()-1) {
+            interp.exec(str);
+            return str;
+        } else return interp.eval(str).__str__().toString();
+    }
+
+    public void exec(String str) {
+        String re = eval(str);
+        return;
+    }
 }
