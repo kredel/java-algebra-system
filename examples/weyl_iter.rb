3f0b6cf0ae4f9435222b446283e664df16b6f355
Heinz Kredel <kredel@rz.uni-mannheim.de>
2021-01-17 15:18:32 +0100

    fix comments

--- examples/weyl_iter.rb
+++ examples/weyl_iter.rb
@@ -8,7 +8,7 @@ require "examples/jas"
 # Weyl coefficient field example 
 
 r = PolyRing.new(QQ(),"p1,q1");
-#is automatic: [one,p1,q1] = r.gens();
+#is automatic: one,p1,q1 = r.gens();
 
 relations = [q1, p1,  p1 * q1 + 1
             ];
@@ -28,7 +28,8 @@ puts "scp = " + str(scp);
 
 
 r2 = PolyRing.new(scp,"p2,q2");
-#is automatic: [one,p1,q1,p2,q2] = r2.gens();
+#is not okay: one,p1,q1,p2,q2 = r2.gens();
+puts "gens r2 =" + r2.gens().join(", ") { |r| r.to_s };
 
 relations2 = [q2, p2,  p2 * q2 + 1
              ];
@@ -40,8 +41,8 @@ rp2 = SolvPolyRing.new(scp, "p2,q2", PolyRing.lex, relations2);
 puts "SolvPolyRing: " + str(rp2);
 puts;
 
-puts "gens =" + rp2.gens().join(", ") { |r| r.to_s };
-#is automatic: one,q1,p1,q2,p2 = rp2.gens();
+puts "gens rp2 =" + rp2.gens().join(", ") { |r| r.to_s };
+#is not okay: one,q1,p1,q2,p2 = rp2.gens();
 
 
 f1 = p2 + p1;
